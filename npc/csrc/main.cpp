8dd2bb37a1cb29e991eeb8816d06e087094a3e25
tracer-ysyx <tracer@ysyx.org>
2024-01-26 00:14:24 +0900

    > sim RTL ysyx_2022280542 温九 Linux wenjiu-virtual-machine 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:14:24 up 16 min,  1 user,  load average: 0.58, 0.38, 0.27

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,63 +1,30 @@
 #include <nvboard.h>
 #include <Vtop.h>
-#include "verilated.h"
-#include "verilated_vcd_c.h"
-#include <stdio.h>
 
-#define WAVE_ON
-
-Vtop* top;
+static TOP_NAME dut;
 
 void nvboard_bind_all_pins(Vtop* top);
 
 static void single_cycle() {
-  top->clk = 0; top->eval();
-  top->clk = 1; top->eval();
+  dut.clk = 0; dut.eval();
+  dut.clk = 1; dut.eval();
 }
 
 static void reset(int n) {
-  top->rst = 1;
+  dut.rst = 1;
   while (n -- > 0) single_cycle();
-  top->rst = 0;
+  dut.rst = 0;
 }
 
-int main(int argc, char** argv) {
-  #ifdef WAVE_ON
-  const std::unique_ptr<VerilatedContext> contextp{new VerilatedContext};
-
-	Verilated::traceEverOn(true);
-
-  #endif
-
-  top = new Vtop;
-  
-  nvboard_bind_all_pins(top);
+int main() {
+  nvboard_bind_all_pins(&dut);
   nvboard_init();
-
-  #ifdef WAVE_ON
-	contextp->commandArgs(argc, argv);
-
-	VerilatedVcdC* tfp = new VerilatedVcdC;
-	top->trace(tfp, 99);
-	tfp->open("wave.vcd");
-  #endif
 
   reset(10);
 
   while(1) {
     nvboard_update();
     single_cycle();
-    top->eval();
-
-    #ifdef WAVE_ON
-    contextp->timeInc(1);
-		tfp->dump(contextp->time());
-		#endif
+    dut.eval();
   }
-
-  #ifdef WAVE_ON
-	tfp->close();
-	#endif
-  nvboard_quit();
-
 }

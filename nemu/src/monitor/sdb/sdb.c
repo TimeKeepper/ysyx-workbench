30554e53a4633ff06def78dc02113e51b0fb53e5
tracer-ysyx <tracer@ysyx.org>
2024-01-27 08:41:06 +0900

    > compile NEMU ysyx_2022280542 温九 Linux wenjiu-virtual-machine 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  08:41:06 up  6:47,  3 users,  load average: 0.43, 0.34, 0.29

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -53,6 +53,12 @@ static int cmd_q(char *args) {
   return -1;
 }
 
+static int cmd_si(char *args) {
+  int exec_num = atoi(strtok(args, " "));
+  cpu_exec(exec_num);
+  return 0;
+}
+
 static int cmd_help(char *args);
 
 static struct {
@@ -63,6 +69,7 @@ static struct {
   { "help", "Display information about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
+  { "si", "Let the program step through N instructions and then pause execution", cmd_si}
 
   /* TODO: Add more commands */
 

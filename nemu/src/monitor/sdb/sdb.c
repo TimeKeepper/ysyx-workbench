78fc5813f7b57cafaff1facb1dcc65ee77e98e89
tracer-ysyx <tracer@ysyx.org>
2024-01-27 15:44:57 +0900

    > compile NEMU ysyx_2022280542 温九 Linux wenjiu-virtual-machine 6.5.0-15-generic #15~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 12 18:54:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:44:57 up 27 min,  2 users,  load average: 1.44, 0.91, 0.58

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -83,9 +83,9 @@ static uint32_t print_Ram(uint32_t bias){
 }
 
 static int cmd_x(char *args){
+  printf("test\n");
   int scan_num = atoi(strtok(args, " "));
   uint32_t base_Addr = expr(strtok(args, " "), NULL);
-  printf("test\n");
   for(int i = 0; i < scan_num; i++){
     print_Ram(base_Addr + 4 * i);
   }

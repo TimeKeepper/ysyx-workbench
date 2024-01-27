8fca6cddc952236910e381036a6972b3ef85da95
tracer-ysyx <tracer@ysyx.org>
2024-01-28 08:17:05 +0900

    > compile NEMU ysyx_2022280542 温九 Linux wenjiu-virtual-machine 6.5.0-15-generic #15~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 12 18:54:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  08:17:05 up  4:23,  4 users,  load average: 0.82, 0.81, 0.58

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -58,7 +58,7 @@ static struct rule {
   {"\\(", TK_LPAREN},         // left parenthesis
   {"\\)", TK_RPAREN},         // right parenthesis
   {"[0-9]+", TK_DECIMAL},         // decimal
-  {"[xX][0-9a-fA-F]+", TK_HEX},         // hex
+  {"[0-9a-fA-F]+[xX]", TK_HEX},         // hex
   {"==", TK_EQ},        // equal
 };
 

6aa94fc5c711ee902bf02aaaa819697f9285aac9
tracer-ysyx <tracer@ysyx.org>
2024-01-29 09:28:10 +0900

    > compile NEMU ysyx_2022280542 温九 Linux wenjiu-virtual-machine 6.5.0-15-generic #15~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 12 18:54:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:28:10 up 18:32,  5 users,  load average: 0.57, 0.54, 0.52

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -221,7 +221,7 @@ static int find_Op(int p, int q){
       }
     }
     else if(tokens[i].type == TK_MULT || tokens[i].type == TK_DIV){
-      if(op == 0 && (op_type == 0 || tokens[op_type].type == TK_PLUS || tokens[op_type].type == TK_MINUS)){
+      if(op == 0 && (op_type == 0 || tokens[op_type].type == TK_MULT || tokens[op_type].type == TK_DIV)){
         op_type = i;
       }
     }

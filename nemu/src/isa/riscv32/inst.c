5eb868f2e55b768080d114751128f8c6fad1ee02
tracer-ysyx <tracer@ysyx.org>
2024-02-19 02:54:31 +0900

    > compile NEMU ysyx_2022280542 温九 Linux wenjiu-virtual-machine 6.5.0-15-generic #15~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 12 18:54:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  02:54:31 up 14:52,  6 users,  load average: 0.65, 0.93, 0.92

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -20,8 +20,8 @@
 #include <pass_include.h>
 
 #define R(i) gpr(store_Regs_Value_cache(i))
-#define Print_rd (rd==0?printf("\n"):printf("rd:%s\n",isa_id2str(rd)))
-#define Print_insut_name(name) printf("insut:%s,",name)
+#define Print_rd (printf("rd:%s,",isa_id2str(rd)))
+#define Print_insut_name(name) printf("insut:%s\n,",name)
 #define Print_DBG_Message(name) (Print_rd,Print_insut_name(name))
 #define Mr vaddr_read
 #define Mw vaddr_write

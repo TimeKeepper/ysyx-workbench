52c4673d12b92951792c2add23ef026ec7569fcd
tracer-ysyx <tracer@ysyx.org>
2024-02-24 10:01:13 +0900

    > compile NEMU ysyx_2022280542 温九 Linux wenjiu-virtual-machine 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:01:13 up  2:08,  3 users,  load average: 1.02, 1.04, 0.93

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -37,12 +37,7 @@ int strcmp(const char *s1, const char *s2) {
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
-  while(n--) {
-    if(*s1 != *s2) return *s1 - *s2;
-    s1++;
-    s2++;
-  }
-  return 0;
+  return memcmp(s1, s2, n);
 }
 
 void *memset(void *s, int c, size_t n) {

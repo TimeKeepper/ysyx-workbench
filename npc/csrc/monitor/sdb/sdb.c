91298e3eeca53b57d4938b398365f73c6c99d6ef
tracer-ysyx <tracer@ysyx.org>
2024-02-29 02:05:09 +0900

    > sim RTL ysyx_2022280542 温九 Linux wenjiu-virtual-machine 6.5.0-21-generic #21~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb  9 13:32:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  02:05:09 up 13:26,  4 users,  load average: 0.61, 0.75, 0.84

--- npc/csrc/monitor/sdb/sdb.c
+++ npc/csrc/monitor/sdb/sdb.c
@@ -89,23 +89,20 @@ void sdb_mainloop() {
 
         /* extract the first token as the command */
         char *cmd = strtok(str, " ");
-        if (cmd == NULL) { continue; }
+        if (cmd == NULL)  continue; 
 
-            /* treat the remaining string as the arguments,
-            * which may need further parsing
-            */
-            char *args = cmd + strlen(cmd) + 1;
-            if (args >= str_end) {
-            args = NULL;
-        }
+        /* treat the remaining string as the arguments,
+        * which may need further parsing
+        */
+        char *args = cmd + strlen(cmd) + 1;
+        if (args >= str_end) args = NULL;
 
         int i;
 
-        for (i = 0; i < NR_CMD; i ++) {
-            if (strcmp(cmd, cmd_table[i].name) == 0) {
-                if (cmd_table[i].handler(args) < 0) { return; }
-                break;
-            }
+        for (i = 0; i < NR_CMD; i++) {
+            if (strcmp(cmd, cmd_table[i].name) != 0) continue;
+            if (cmd_table[i].handler(args) < 0) { return; }
+            break;
         }
 
         if (i == NR_CMD) { printf("Unknown command '%s'\n", cmd); }

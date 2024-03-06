652d1aeb0558c2b636852f9cceaa8ccbab704d6a
tracer-ysyx <tracer@ysyx.org>
2024-03-06 09:58:47 +0900

    > compile NEMU ysyx_23060198 林锦晖 Linux wenjiu-virtual-machine 6.5.0-21-generic #21~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb  9 13:32:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:58:47 up  3:45,  5 users,  load average: 0.28, 0.54, 0.66

--- nemu/scripts/build.mk
+++ nemu/scripts/build.mk
@@ -4,9 +4,9 @@
 ifeq ($(SHARE),1)
 SO = -so
 CFLAGS  += -fPIC -fvisibility=hidden
-CFLAGS := $(CFLAGS)
+CFLAGS := -fsanitize=address $(CFLAGS)
 LDFLAGS += -shared -fPIC
-LDFLAGS := $(LDFLAGS)
+LDFLAGS := -lasan $(LDFLAGS)
 endif
 
 WORK_DIR  = $(shell pwd)

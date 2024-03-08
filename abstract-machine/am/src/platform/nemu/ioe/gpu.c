a68b2c6bbe7e381b9c46f9ebdfcb5125249392e6
202280542-Wen jiu <2784307979@qq.com>
2024-03-08 23:15:40 +0900

    完成PA2

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -1,23 +1,42 @@
 #include <am.h>
 #include <nemu.h>
+#include <stdint.h>
 
+#define SCREEN_HEIGHT inw(VGACTL_ADDR)
+#define SCREEN_WIDTH inw(VGACTL_ADDR + 2)
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
+  // int i;
+  // int w = SCREEN_WIDTH;  // TODO: get the correct width
+  // int h = SCREEN_HEIGHT;  // TODO: get the correct height
+  // uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  // for (i = 0; i < w * h; i ++) fb[i] = i;
+  // outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
+  uint16_t w = SCREEN_WIDTH;
+  uint16_t h = SCREEN_HEIGHT;
+  uint32_t vmemsz = w * h * sizeof(uint32_t);
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
-    .width = 0, .height = 0,
-    .vmemsz = 0
+    .width = w, .height = h,
+    .vmemsz = vmemsz
   };
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-  if (ctl->sync) {
-    outl(SYNC_ADDR, 1);
+  if (!ctl->sync) return;
+
+  size_t *data=ctl->pixels;
+  for(int i = 0; i < ctl->h; i++){
+    for(int j = 0; j < ctl->w; j++){
+      outl((((ctl->x + j) + (ctl->y + i) * SCREEN_WIDTH) * 4) + FB_ADDR, *(data++));
+    }
   }
+  
+  outb(SYNC_ADDR, 1);
 }
 
 void __am_gpu_status(AM_GPU_STATUS_T *status) {

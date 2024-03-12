1d1a8a898819480777f1e1da94f7aafe688ceb0f
tracer-ysyx <tracer@ysyx.org>
2024-03-12 14:34:58 +0900

    > compile NEMU ysyx_23060198 林锦晖 Linux wenjiu-virtual-machine 6.5.0-25-generic #25~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Feb 20 16:09:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:34:58 up 19:46,  4 users,  load average: 0.34, 0.43, 0.44

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -209,7 +209,7 @@ static int decode_exec(Decode *s) {
   bgeu   , B, if (Print_DBG_Message("bgeu"),src1 >= src2) s->dnpc = s->pc + (sword_t)imm);
   
   INSTPAT("0000000 00000 00000 000 00000 11100 11", \
-  ecall  , N, Print_DBG_Message("ecall ") ,               isa_raise_intr(8, s->pc)); 
+  ecall  , N, Print_DBG_Message("ecall ") ,               s->dnpc = isa_raise_intr(8, s->pc)); 
   
   INSTPAT("0000000 00001 00000 000 00000 11100 11", \
   ebreak , N, Print_DBG_Message("ebreak") ,               NEMUTRAP(s->pc, R(10))); // R(10) is $a0

5a90ab338ea5fd8c226fba224859f4f0f666f8e7
tracer-ysyx <tracer@ysyx.org>
2024-03-31 02:01:21 +0900

    > compile NEMU ysyx_23060198 林锦晖 Linux wenjiu-virtual-machine 6.5.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 12 10:22:43 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  02:01:21 up 2 days,  8:05,  5 users,  load average: 1.36, 1.12, 1.00

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -49,7 +49,7 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
     printf("Watchpoint %d: %s\n",wp->NO,wp->expr);
     printf("Old value = 0x%08x\n",wp->last_time_Value);
     printf("New value = 0x%08x\n",wp->value);
-    nemu_state.state = NEMU_STOP;
+    if(nemu_state.state != NEMU_END) nemu_state.state = NEMU_STOP;//如果在nemu停止的情况下修改state，就会导致报错,因为会导致检查trap的时候无法通过NEMU_END的判断
   }
   // while(get_Changed_wp()!=NULL){
   //   printf("Watchpoint %d: %s\n",get_Changed_wp()->NO,get_Changed_wp()->expr);

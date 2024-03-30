acb2e103fb5532ccac2efed93318149045968b87
tracer-ysyx <tracer@ysyx.org>
2024-03-31 04:02:13 +0900

    > compile NEMU ysyx_23060198 林锦晖 Linux wenjiu-virtual-machine 6.5.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 12 10:22:43 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  04:02:13 up 2 days, 10:06,  5 users,  load average: 1.54, 1.38, 1.14

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -63,7 +63,7 @@ void free_wp(WP *wp){
 void wp_display(void){
   WP *p = head;
   while(p != NULL){
-    printf("watchpoint %d: " ANSI_FMT("%s", ANSI_FG_BLUE) " = 0x%08x\n", p->NO, p->expr, p->value);
+    printf("watchpoint %d: (" ANSI_FMT("%s", ANSI_FG_BLUE) ")\t = 0x%08x\n", p->NO, p->expr, p->value);
     p = p->next;
   }
 }

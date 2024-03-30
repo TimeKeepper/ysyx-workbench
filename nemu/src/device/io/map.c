e6f22634b9f84721f16f5e199d65bf0079c80ac5
tracer-ysyx <tracer@ysyx.org>
2024-03-31 06:15:17 +0900

    > compile NEMU ysyx_23060198 林锦晖 Linux wenjiu-virtual-machine 6.5.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 12 10:22:43 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  06:15:17 up 2 days, 12:19,  5 users,  load average: 0.77, 1.04, 1.05

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -58,6 +58,9 @@ word_t map_read(paddr_t addr, int len, IOMap *map) {
   paddr_t offset = addr - map->low;
   invoke_callback(map->callback, offset, len, false); // prepare data to read
   word_t ret = host_read(map->space + offset, len);
+  #ifdef CONFIG_DTRACE
+  printf(ANSI_FMT("map read", ANSI_FG_BLUE) " addr = " FMT_PADDR ", offset = " FMT_PADDR ", device name = %s\n", addr, offset, map->name);
+  #endif
   return ret;
 }
 
@@ -66,5 +69,8 @@ void map_write(paddr_t addr, int len, word_t data, IOMap *map) {
   check_bound(map, addr);
   paddr_t offset = addr - map->low;
   host_write(map->space + offset, len, data);
+  #ifdef CONFIG_DTRACE
+  printf(ANSI_FMT("map write", ANSI_FG_BLUE) " addr = " FMT_PADDR ", offset = " FMT_PADDR ", device name = %s" ", data = %x", addr, offset, map->name, data);
+  #endif
   invoke_callback(map->callback, offset, len, true);
 }

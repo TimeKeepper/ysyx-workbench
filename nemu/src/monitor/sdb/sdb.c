52b4140a791db8e953e24f6ef539e921a547030c
tracer-ysyx <tracer@ysyx.org>
2024-03-31 02:33:42 +0900

    > compile NEMU ysyx_23060198 林锦晖 Linux wenjiu-virtual-machine 6.5.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 12 10:22:43 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  02:33:42 up 2 days,  8:38,  5 users,  load average: 1.68, 1.06, 0.94

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -20,6 +20,7 @@
 #include <memory/paddr.h>
 #include "sdb.h"
 #include "common.h"
+#include "debug.h"
 #include "utils.h"
 
 static int is_batch_mode = false;
@@ -57,149 +58,18 @@ static char* rl_gets() {
   return line_read;
 }
 
-static int cmd_info(char *args) {
-  char* show_type = strtok(args, " ");
-  if(show_type == NULL){
-    Log("You should input the requried info type: r(register) or w(watchpoint).");
-    return 0;
-  }
-  if(strlen(show_type) != 1) {
-    Log("You should only enter an single character.");
-    return 0;
-  }
-  char *specific_info = strtok(NULL, " ");
-  switch(*show_type){
-    case 'r': isa_reg_display(specific_info);break;
-    case 'w': wp_display();break;
-    default:Log("you should input the requried info type: r(register) or w(watchpoint).");break;
-  }
-  return 0;
-}
-
-#define INPUT_BUF_LENGTH 65536
-char input_buf[INPUT_BUF_LENGTH];
-
-static int cmd_test(char *args){
-  bool success = true;
-
-  FILE* fp = fopen("/home/wen-jiu/my_ysyx_project/ysyx-workbench/nemu/tools/gen-expr/input", "r");
-
-  if(fp == NULL){
-    printf("Can not open the file!\n");
-    return 0;
-  }
-
-  while(fgets(input_buf, INPUT_BUF_LENGTH, fp) != NULL){
-    char* result_str = strtok(input_buf, " ");
-    char* expr_str = result_str + strlen(result_str) + 1;
-    expr_str[strlen(expr_str) - 1] = '\0';
-    int result = atoi(result_str);
-    printf("expr: %s, result: %d\n", expr_str, result);
-    if(expr(expr_str, &success) != result){
-      printf("Test failed! The result should be %d, but your result is %d\n", result, expr(expr_str, &success));
-    }
-  }
-
-  return 0;
-}
-
-static int cmd_single_test(char *args){
-  bool success = true;
-  char* expr_str = args;
-  int result = atoi(args + strlen(args) + 1);
-  printf("expr: %s, result: %d\n", expr_str, result);
-  if(expr(expr_str, &success) != result){
-    printf("Test failed! The result should be %d, but your result is %d\n", result, expr(expr_str, &success));
-  }
-  return 0;
-}
-
-static uint32_t print_Ram(uint32_t bias){
-  uint32_t result = paddr_read(bias, 4);
-  printf("0x%08x ", result);
-  return result;
-}
-
-static int cmd_x(char *args){
-  char *scan_num_str = strtok(args, " ");
-  int scan_num = atoi(scan_num_str);
-  bool success = true;
-  uint32_t base_Addr = expr(scan_num_str+strlen(scan_num_str)+1, &success);
-  if(!likely(in_pmem(base_Addr))){
-    printf("The 0x%08x address is out of range!\n", base_Addr);
-    return 0;
-  }
-  for(int i = 0; i < scan_num; i++){
-    print_Ram(base_Addr + 4 * i);
-    for(int j = 0; j < 4; j++){
-      printf("%c", paddr_read(base_Addr + 4 * i + j, 1));
-    }
-    printf("\n");
-  }
-  printf("\n");
-  return 0;
-}
-
-static int cmd_w(char *args){
-  new_wp(args);
-  wp_Value_Update();
-  return 0;
-}
-
-static int cmd_d(char *args){
-  int wpNO = atoi(args);
-  WP* wp = get_head_wp();
-  for(int i = 0; i < wpNO - 1; i++){
-    wp = wp->next;
-  }
-  if(wp!=NULL) free_wp(wp);
-  return 0;
-}
-
-static int cmd_b(char *args){
-  if(args == NULL){
-    printf("You should input the address of the breakpoint!\n");
-    return 0;
-  }
-  bool success = true;
-  word_t addr = expr(args, &success);
-  if(addr < 0x80000000){
-    printf("The address is out of range!\n");
-    return 0;
-  }
-  char expr_str[20] = "$pc == ";
-  strcat(expr_str, args);
-  new_wp(expr_str);
-  wp_Value_Update();
-  return 0;
-}
-
 static int cmd_help(char *args);
 static int cmd_c(char *args);
 static int cmd_q(char *args);
 static int cmd_si(char *args);
-
-void change_register_value(int, word_t);
-
-static int cmd_crv(char *args){
-  char* reg_name = strtok(args, " ");
-  if(reg_name == NULL){
-    Log("You should input the register name!");
-    return 0;
-  }
-  char* reg_value_str = strtok(NULL, " ");
-  if(reg_value_str == NULL){
-    Log("You should input the register value!");
-    return 0;
-  }
-  bool success = true;
-  word_t reg_value = expr(reg_value_str, &success);
-  if(success){
-    int regNO = isa_str2id(reg_name, &success);
-    change_register_value(regNO, reg_value);
-  }
-  return 0;
-}
+static int cmd_info(char *args);
+static int cmd_x(char *args);
+static int cmd_w(char *args);
+static int cmd_d(char *args);
+static int cmd_b(char *args);
+static int cmd_test(char *args);
+static int cmd_single_test(char *args);
+static int cmd_crv(char *args);
 
 static struct {
   const char *name;
@@ -267,6 +137,147 @@ static int cmd_si(char *args) {
   else{
     int parameter = atoi(parameter_str);
     cpu_exec(parameter);
+  }
+  return 0;
+}
+
+static int cmd_info(char *args) {
+  char* show_type = strtok(args, " ");
+  if(show_type == NULL){
+    Log("You should input the requried info type: r(register) or w(watchpoint).");
+    return 0;
+  }
+  if(strlen(show_type) != 1) {
+    Log("You should only enter an single character.");
+    return 0;
+  }
+  char *specific_info = strtok(NULL, " ");
+  switch(*show_type){
+    case 'r': isa_reg_display(specific_info);break;
+    case 'w': wp_display();break;
+    default:Log("you should input the requried info type: r(register) or w(watchpoint).");break;
+  }
+  return 0;
+}
+
+static uint32_t print_Ram(uint32_t bias){
+  uint32_t result = paddr_read(bias, 4);
+  printf("0x%08x ", result);
+  return result;
+}
+
+static int cmd_x(char *args){
+  char *scan_num_str = strtok(args, " ");
+  int scan_num = atoi(scan_num_str);
+  bool success = true;
+  uint32_t base_Addr = expr(scan_num_str+strlen(scan_num_str)+1, &success);
+  if(!likely(in_pmem(base_Addr))){
+    printf("The 0x%08x address is out of range!\n", base_Addr);
+    return 0;
+  }
+  for(int i = 0; i < scan_num; i++){
+    print_Ram(base_Addr + 4 * i);
+    for(int j = 0; j < 4; j++){
+      printf("%c", paddr_read(base_Addr + 4 * i + j, 1));
+    }
+    printf("\n");
+  }
+  printf("\n");
+  return 0;
+}
+
+static int cmd_w(char *args){
+  new_wp(args);
+  wp_Value_Update();
+  return 0;
+}
+
+static int cmd_d(char *args){
+  int wpNO = atoi(args);
+  WP* wp = get_head_wp();
+  for(int i = 0; i < wpNO - 1; i++){
+    wp = wp->next;
+  }
+  if(wp!=NULL) free_wp(wp);
+  return 0;
+}
+
+static int cmd_b(char *args){
+  if(args == NULL){
+    printf("You should input the address of the breakpoint!\n");
+    return 0;
+  }
+  bool success = true;
+  word_t addr = expr(args, &success);
+  if(addr < 0x80000000){
+    printf("The address is out of range!\n");
+    return 0;
+  }
+  char expr_str[20] = "$pc == ";
+  strcat(expr_str, args);
+  new_wp(expr_str);
+  wp_Value_Update();
+  return 0;
+}
+
+// #define INPUT_BUF_LENGTH 65536
+// char input_buf[INPUT_BUF_LENGTH];
+
+static int cmd_test(char *args){
+  TODO();
+  // bool success = true;
+
+  // FILE* fp = fopen("/home/wen-jiu/my_ysyx_project/ysyx-workbench/nemu/tools/gen-expr/input", "r");
+
+  // if(fp == NULL){
+  //   printf("Can not open the file!\n");
+  //   return 0;
+  // }
+
+  // while(fgets(input_buf, INPUT_BUF_LENGTH, fp) != NULL){
+  //   char* result_str = strtok(input_buf, " ");
+  //   char* expr_str = result_str + strlen(result_str) + 1;
+  //   expr_str[strlen(expr_str) - 1] = '\0';
+  //   int result = atoi(result_str);
+  //   printf("expr: %s, result: %d\n", expr_str, result);
+  //   if(expr(expr_str, &success) != result){
+  //     printf("Test failed! The result should be %d, but your result is %d\n", result, expr(expr_str, &success));
+  //   }
+  // }
+
+  // return 0;
+}
+
+static int cmd_single_test(char *args){
+  TODO();
+  // bool success = true;
+  // char* expr_str = args;
+  // int result = atoi(args + strlen(args) + 1);
+  // printf("expr: %s, result: %d\n", expr_str, result);
+  // if(expr(expr_str, &success) != result){
+  //   printf("Test failed! The result should be %d, but your result is %d\n", result, expr(expr_str, &success));
+  // }
+  // return 0;
+}
+
+void change_register_value(int, word_t);
+
+static int cmd_crv(char *args){
+  char* reg_name = strtok(args, " ");
+  if(reg_name == NULL){
+    Log("You should input the register name!");
+    return 0;
+  }
+  char* reg_value_str = strtok(NULL, " ");
+  if(reg_value_str == NULL){
+    Log("You should input the register value!");
+    return 0;
+  }
+  bool success = true;
+  word_t reg_value = expr(reg_value_str, &success);
+  if(success){
+    int regNO = isa_str2id(reg_name, &success);
+    change_register_value(regNO, reg_value);
   }
   return 0;
 }

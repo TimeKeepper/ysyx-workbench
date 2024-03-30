48feb6a711403be5140ef1ca072c56d48cd441ae
tracer-ysyx <tracer@ysyx.org>
2024-03-31 03:40:33 +0900

    > compile NEMU ysyx_23060198 林锦晖 Linux wenjiu-virtual-machine 6.5.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 12 10:22:43 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  03:40:33 up 2 days,  9:44,  5 users,  load average: 0.72, 0.91, 0.88

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -219,10 +219,13 @@ static int cmd_x(char *args){
   int scan_num = atoi(scan_num_str);
   bool success = true;
   uint32_t base_Addr = expr(scan_num_str+strlen(scan_num_str)+1, &success);
+  if(base_Addr == 0) return 0;
+
   if(!likely(in_pmem(base_Addr))){
     printf(ANSI_FMT("The 0x%08x address is out of range!\n", ANSI_FG_RED), base_Addr);
     return 0;
   }
+
   for(int i = 0; i < scan_num; i++){
     print_Ram(base_Addr + 4 * i);
     for(int j = 0; j < 4; j++){
@@ -230,6 +233,7 @@ static int cmd_x(char *args){
     }
     printf("\n");
   }
+  
   printf("\n");
   return 0;
 }

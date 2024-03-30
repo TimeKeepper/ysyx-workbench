119b8a2bb5f46029957d97fa5d1487bacfea9a89
tracer-ysyx <tracer@ysyx.org>
2024-03-31 05:14:34 +0900

    > compile NEMU ysyx_23060198 林锦晖 Linux wenjiu-virtual-machine 6.5.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 12 10:22:43 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  05:14:34 up 2 days, 11:18,  5 users,  load average: 0.78, 0.80, 0.79

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -56,7 +56,7 @@ void init_mem() {
 
 word_t paddr_read(paddr_t addr, int len) {
   #ifdef CONFIG_MTRACE
-  printf("paddr read: addr = " FMT_PADDR ", len = %d\n", addr, len);
+  printf(ANSI_FMT("paddr read", ANSI_FG_BLUE) " addr = " FMT_PADDR ", len = %d\n", addr, len);
   #endif
   if (likely(in_pmem(addr))) return pmem_read(addr, len);
   IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
@@ -66,7 +66,7 @@ word_t paddr_read(paddr_t addr, int len) {
 
 void paddr_write(paddr_t addr, int len, word_t data) {
   #ifdef CONFIG_MTRACE
-  printf("paddr write: addr = " FMT_PADDR ", len = %d, data = " FMT_WORD "\n", addr, len, data);
+  printf(ANSI_FMT("paddr write", ANSI_FG_BLUE) " addr = " FMT_PADDR ", len = %d, data = " FMT_WORD "\n", addr, len, data);
   #endif
   if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);

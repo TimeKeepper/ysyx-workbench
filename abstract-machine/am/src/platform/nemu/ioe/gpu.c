6d8652de0025c4526fadca5e1a8e35938d63e973
tracer-ysyx <tracer@ysyx.org>
2024-03-31 09:09:27 +0900

    > compile NEMU ysyx_23060198 林锦晖 Linux wenjiu-virtual-machine 6.5.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 12 10:22:43 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:09:27 up  1:57,  4 users,  load average: 0.58, 1.01, 0.98

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -29,14 +29,14 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 int printf(const char *fmt, ...);
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-  if (ctl->sync) return;
-
   size_t *data=ctl->pixels;
   for(int i = 0; i < ctl->h; i++){
     for(int j = 0; j < ctl->w; j++){
       outl((((ctl->x + j) + (ctl->y + i) * SCREEN_WIDTH) * 4) + FB_ADDR, *(data++));
     }
   }
+  
+  if (!ctl->sync) return;
   
   outb(SYNC_ADDR, 1);
 }

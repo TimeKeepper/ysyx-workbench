2656c8b743c9d375f57caa0ce42c530ec81696a7
tracer-ysyx <tracer@ysyx.org>
2024-03-31 10:00:26 +0900

    > compile NEMU ysyx_23060198 林锦晖 Linux wenjiu-virtual-machine 6.5.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 12 10:22:43 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:00:26 up  2:48,  4 users,  load average: 1.04, 0.97, 1.00

--- npc/csrc/monitor/sdb/cmd.c
+++ npc/csrc/monitor/sdb/cmd.c
@@ -12,18 +12,19 @@ int cmd_help(char *args) {
     if (arg == NULL) {
         /* no argument given */
         for (i = 0; i < NR_CMD; i ++) {
-        printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
+        printf(ANSI_FMT("%s\t", ANSI_FG_BLUE) " - " ANSI_FMT("%s\n", ANSI_FG_MAGENTA), cmd_table[i].name, cmd_table[i].description);
         }
+        return 0;
     }
-    else {
-        for (i = 0; i < NR_CMD; i ++) {
-            if (strcmp(arg, cmd_table[i].name) != 0) continue;
-            printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
-            return 0;
+    
+    for (i = 0; i < NR_CMD; i ++) {
+        if (strcmp(arg, cmd_table[i].name) == 0) {
+        printf(ANSI_FMT("%s\t", ANSI_FG_BLUE) " - " ANSI_FMT("%s\n", ANSI_FG_MAGENTA) "usage: \n" ANSI_FMT("%s\n", ANSI_FG_CYAN), cmd_table[i].name, cmd_table[i].description, cmd_table[i].usage);
+        return 0;
         }
-        printf("Unknown command '%s'\n", arg);
     }
 
+    printf(ANSI_FMT("Unknown command", ANSI_FG_RED) " '%s'\n", arg);
     return 0;
 }
 
@@ -42,31 +43,43 @@ int cmd_r(char *args){
     return 0;
 }
 
-int cmd_si(char *args){
-    char *arg = strtok(NULL, " ");
-    int n = 1;
-    if (arg != NULL) {
-        sscanf(arg, "%d", &n);
+int cmd_si(char *args) {
+    char* parameter_str = strtok(args, " ");
+
+    if(parameter_str == NULL){
+        cpu_exec(1);
+        return 0;
     }
-    cpu_exec(n);
+
+    int parameter = atoi(parameter_str);
+    if(parameter < 0){
+        printf(ANSI_FMT("You should input a positive value\n", ANSI_FG_RED));
+        return 0;
+    }
+    else if(parameter == 0){
+        printf(ANSI_FMT("What do you mean, Bro?\n", ANSI_FG_RED));
+        return 0;
+    }
+
+    cpu_exec(parameter);
     return 0;
 }
 
-int cmd_info(char *args){
-    char* show_type = strtok(NULL, " ");
+int cmd_info(char *args) {
+    char* show_type = strtok(args, " ");
     if(show_type == NULL){
-        printf("You should input the requried info type: r(register) or w(watchpoint).\n");
+        printf(ANSI_FMT("You should input the requried info type: r(register) or w(watchpoint).\n", ANSI_FG_RED));
         return 0;
     }
     if(strlen(show_type) != 1) {
-        printf("You should only enter an single character.\n");
+        printf(ANSI_FMT("You should only enter an single character.\n", ANSI_FG_RED));
         return 0;
     }
     char *specific_info = strtok(NULL, " ");
     switch(*show_type){
-        case 'r': isa_reg_display(specific_info);break;
-        case 'w': break;
-        default: printf("you should input the requried info type: r(register) or w(watchpoint).\n");
+        case 'r': isa_reg_display(specific_info);                                       break;
+        case 'w': break;                                                                 break;
+        default : printf(ANSI_FMT("you should input the requried info type: r(register) or w(watchpoint).\n", ANSI_FG_RED));  break;
     }
     return 0;
 }
@@ -78,18 +91,25 @@ static uint32_t print_Ram(uint32_t bias){
 }
 
 int cmd_x(char *args){
-    char *scan_num_str = strtok(args, " ");
-    int scan_num = atoi(scan_num_str);
-    bool success = true;
-    uint32_t base_Addr = expr(scan_num_str+strlen(scan_num_str)+1, &success);
-    if(!likely(in_pmem(base_Addr))){
-        printf("The 0x%08x address is out of range!\n", base_Addr);
+    if(args == NULL) {
+        printf(ANSI_FMT("You should input the scan time!\n", ANSI_FG_RED));
         return 0;
     }
+    
+    int scan_num = atoi(strtok(args, " "));
+    
+    bool success = true;
+    uint32_t base_Addr = expr(strtok(NULL, " "), &success);
+
+    if(!likely(in_pmem(base_Addr))){
+        printf(ANSI_FMT("The 0x%08x address is out of range!\n", ANSI_FG_RED), base_Addr);
+        return 0;
+    }
+
     for(int i = 0; i < scan_num; i++){
         print_Ram(base_Addr + 4 * i);
         for(int j = 0; j < 4; j++){
-            printf("%c", paddr_read(base_Addr + 4 * i + j, 1));
+        printf(ANSI_FMT("%c", ANSI_FG_BLUE), paddr_read(base_Addr + 4 * i + j, 1));
         }
         printf("\n");
     }

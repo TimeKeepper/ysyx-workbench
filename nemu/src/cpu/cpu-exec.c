fccc42e91984b58408ae738d81daa97c00034115
tracer-ysyx <tracer@ysyx.org>
2024-04-06 04:14:13 +0900

    > compile NEMU ysyx_23060198 林锦晖 Linux wenjiu-virtual-machine 6.5.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 12 10:22:43 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  04:14:13 up 4 days, 17:04,  4 users,  load average: 0.78, 1.16, 1.46

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -68,7 +68,7 @@ void instr_buf_push(char *instr){
 
 void instr_buf_printf(void){
   for(int i = 0; i < INSTR_BUF_SIZE; i++){
-    i == instr_buf_index ? printf("---> ") : printf("     ");
+    i == instr_buf_index ? printf(ANSI_FMT("---> ", ANSI_BG_BLUE)) : printf("     ");
     printf("%s\n", INST_BUF[i]);
   }
 }

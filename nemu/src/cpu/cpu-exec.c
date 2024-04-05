6bd4fe281536aefa059eea3ce4f997f3001c2dc1
tracer-ysyx <tracer@ysyx.org>
2024-04-06 01:37:59 +0900

    > compile NEMU ysyx_23060198 林锦晖 Linux wenjiu-virtual-machine 6.5.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 12 10:22:43 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:37:59 up 4 days, 14:28,  4 users,  load average: 0.54, 1.10, 1.09

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -158,6 +158,7 @@ void cpu_exec(uint64_t n) {
            (nemu_state.halt_ret == 0 ?  ANSI_FMT("HIT GOOD TRAP", ANSI_FG_GREEN) :
                     (ANSI_FMT("HIT BAD TRAP", ANSI_FG_RED)))),
           nemu_state.halt_pc);
+      IFDEF(CONFIG_ITRACE ,instr_buf_printf());
       // fall through
     case NEMU_QUIT: statistic();
   }

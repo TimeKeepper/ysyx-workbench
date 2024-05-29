44a480984c57a9d81b73d7def24dd54627568b90
tracer-ysyx <tracer@ysyx.org>
2024-05-30 02:24:22 +0900

    > compile NEMU ysyx_23060198 林锦晖 Linux wenjiu-virtual-machine 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  02:24:22 up 2 min,  3 users,  load average: 5.48, 2.35, 0.88

--- npc/src/main/GCD.scala
+++ npc/src/main/GCD.scala
@@ -11,13 +11,13 @@ class GCD extends Module {
   val io = IO(new Bundle {
     val value1        = Input(UInt(16.W))
     val value2        = Input(UInt(16.W))
-    val loadingValues = Input(Bool())
+    val loadingValues = Input(Bool(1.W))
     val outputGCD     = Output(UInt(16.W))
-    val outputValid   = Output(Bool())
+    val outputValid   = Output(Bool(1.W))
   })
 
-  val x = Reg(UInt())
-  val y = Reg(UInt())
+  val x = Reg(UInt(16.W))
+  val y = Reg(UInt(16.W))
 
   when(x > y) { x := x - y }.otherwise { y := y - x }
 

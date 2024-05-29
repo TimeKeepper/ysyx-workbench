5ab64d7b535e0f24d31f036bb06de36298e033f9
tracer-ysyx <tracer@ysyx.org>
2024-05-30 08:31:41 +0900

    > generate verilog ysyx_23060198 林锦晖 Linux wenjiu-virtual-machine 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  08:31:41 up  6:10,  4 users,  load average: 3.47, 1.38, 0.69

--- npc/src/main/test.scala
+++ npc/src/main/test.scala
@@ -2,7 +2,12 @@ package gcd
 
 import chisel3._
 
-class test() extends Module {
+/**
+  * Compute GCD using subtraction method.
+  * Subtracts the smaller from the larger until register y is zero.
+  * value in register x is then the GCD
+  */
+class GCD extends Module {
   val io = IO(new Bundle {
     val value1        = Input(UInt(16.W))
     val value2        = Input(UInt(16.W))
@@ -10,12 +15,25 @@ class test() extends Module {
     val outputGCD     = Output(UInt(16.W))
     val outputValid   = Output(Bool())
   })
-  val gcd = Module(new DecoupledGcd(16))
 
-  gcd.input.bits.value1 := io.value1
-  gcd.input.bits.value2 := io.value2
-  gcd.input.ready := io.loadingValues
+  val x = Reg(UInt())
+  val y = Reg(UInt())
 
-  io.outputGCD := gcd.output.bits.gcd
-  io.outputValid := gcd.output.valid
+  when(x > y) { x := x - y }.otherwise { y := y - x }
+
+  when(io.loadingValues) {
+    x := io.value1
+    y := io.value2
+  }
+
+  io.outputGCD   := x
+  io.outputValid := y === 0.U
+  // val gcd = Module(new DecoupledGcd(16))
+
+  // gcd.input.bits.value1 := io.value1
+  // gcd.input.bits.value2 := io.value2
+  // gcd.input.ready := io.loadingValues
+
+  // io.outputGCD := gcd.output.bits.gcd
+  // io.outputValid := gcd.output.valid
 }

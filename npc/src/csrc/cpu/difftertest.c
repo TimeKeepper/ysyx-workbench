fa2870b3ccdd57dd2a326b87bfbafc06cf615ef8
tracer-ysyx <tracer@ysyx.org>
2024-05-31 00:20:02 +0900

    > generate verilog ysyx_23060198 林锦晖 Linux wenjiu-virtual-machine 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:20:02 up 21:58,  5 users,  load average: 0.35, 0.33, 0.35

---
+++ npc/src/csrc/cpu/difftertest.c
@@ -0,0 +1,139 @@
+#include "Vtop__Dpi.h"
+#include "cpu/cpu.h"
+#include "memory/paddr.h"
+#include <assert.h>
+#include <common.h>
+#include <dlfcn.h>
+#include <utils.h>
+#include <sdb/sdb.h>
+
+enum { DIFFTEST_TO_DUT, DIFFTEST_TO_REF };
+
+void (*ref_difftest_memcpy)(paddr_t addr, void *buf, size_t n, bool direction) = NULL;
+void (*ref_difftest_regcpy)(void *dut, bool direction) = NULL;
+void (*ref_difftest_exec)(uint64_t n) = NULL;
+void (*ref_difftest_raise_intr)(uint64_t NO) = NULL;
+
+static bool is_skip_ref = false;
+static int skip_dut_nr_inst = 0;
+
+// this is used to let ref skip instructions which
+// can not produce consistent behavior with NEMU
+void difftest_skip_ref() {
+  is_skip_ref = true;
+  // If such an instruction is one of the instruction packing in QEMU
+  // (see below), we end the process of catching up with QEMU's pc to
+  // keep the consistent behavior in our best.
+  // Note that this is still not perfect: if the packed instructions
+  // already write some memory, and the incoming instruction in NEMU
+  // will load that memory, we will encounter false negative. But such
+  // situation is infrequent.
+  skip_dut_nr_inst = 0;
+}
+
+// this is used to deal with instruction packing in QEMU.
+// Sometimes letting QEMU step once will execute multiple instructions.
+// We should skip checking until NEMU's pc catches up with QEMU's pc.
+// The semantic is
+//   Let REF run `nr_ref` instructions first.
+//   We expect that DUT will catch up with REF within `nr_dut` instructions.
+void difftest_skip_dut(int nr_ref, int nr_dut) {
+  skip_dut_nr_inst += nr_dut;
+
+  while (nr_ref -- > 0) {
+    ref_difftest_exec(1);
+  }
+}
+
+void init_difftest(char *ref_so_file, long img_size, int port) {
+  #ifdef CONFIG_DIFFTEST
+    assert(ref_so_file != NULL);
+
+    void *handle;
+    handle = dlopen(ref_so_file, RTLD_LAZY);
+    assert(handle);
+
+    ref_difftest_memcpy = (void (*)(paddr_t, void *, size_t, bool))dlsym(handle, "difftest_memcpy");
+    assert(ref_difftest_memcpy);
+
+    ref_difftest_regcpy = (void (*)(void *, bool))dlsym(handle, "difftest_regcpy");
+    assert(ref_difftest_regcpy);
+
+    ref_difftest_exec = (void (*)(uint64_t))dlsym(handle, "difftest_exec");
+    assert(ref_difftest_exec);
+
+    ref_difftest_raise_intr = (void (*)(uint64_t))dlsym(handle, "difftest_raise_intr");
+    assert(ref_difftest_raise_intr);
+
+    void (*ref_difftest_init)(int) = (void (*)(int))dlsym(handle, "difftest_init");
+    assert(ref_difftest_init);
+
+    printf("Differential testing: %s\n", ANSI_FMT("ON", ANSI_FG_GREEN));
+    printf("The result of every instruction will be compared with %s. "
+        "This will help you a lot for debugging, but also significantly reduce the performance. "
+        "If it is not necessary, you can turn it off in menuconfig.\n", ref_so_file);
+
+    ref_difftest_init(port);
+    ref_difftest_memcpy(0x80000000, guest_to_host(0x80000000), img_size, DIFFTEST_TO_REF);
+    ref_difftest_regcpy(&cpu, DIFFTEST_TO_REF);
+  #endif
+}
+
+bool isa_difftest_checkregs(CPU_State *ref_r, vaddr_t pc) {
+  if(ref_r->pc != cpu.pc){
+    printf(ANSI_FG_RED "diffter test has detect an error!\n" ANSI_NONE);
+    printf("reg:" ANSI_FG_YELLOW "%s" ANSI_NONE ", ref_value:" ANSI_FG_YELLOW "0x%08x" ANSI_NONE ", dut_value:" ANSI_FG_YELLOW "0x%08x" ANSI_NONE "\n", "pc", ref_r->pc, cpu.pc);
+    return false;
+  }
+  for(int i = 0; i < 32; i++){
+    if(ref_r->gpr[i] != cpu.gpr[i]){
+        printf(ANSI_FG_RED "diffter test has detect an error!\n" ANSI_NONE);
+        printf("reg:" ANSI_FG_YELLOW "%s" ANSI_NONE ", ref_value:" ANSI_FG_YELLOW "0x%08x" ANSI_NONE ", dut_value:" ANSI_FG_YELLOW "0x%08x" ANSI_NONE "\n", reg_id2name(i), ref_r->gpr[i], cpu.gpr[i]);
+      return false;
+    }
+  }
+  return true;
+}
+
+#ifdef CONFIG_DIFFTEST
+static void checkregs(CPU_State *ref, vaddr_t pc) {
+  if (!isa_difftest_checkregs(ref, pc)) {
+    npc_trap(1);
+    npc_state.state = NPC_ABORT;
+    npc_state.halt_pc = pc;
+  }
+}
+#endif
+
+void difftest_step(vaddr_t pc, vaddr_t npc) {
+  #ifdef CONFIG_DIFFTEST
+  CPU_State ref_r;
+
+  if (skip_dut_nr_inst > 0) {
+    ref_difftest_regcpy(&ref_r, DIFFTEST_TO_DUT);
+    if (ref_r.pc == npc) {
+      skip_dut_nr_inst = 0;
+      checkregs(&ref_r, npc);
+      return;
+    }
+    skip_dut_nr_inst --;
+    if (skip_dut_nr_inst == 0){
+      printf("can not catch up with ref.pc = 0x%08x at pc = 0x%08x", ref_r.pc, pc);
+      assert(0);
+    }
+    return;
+  }
+
+  if (is_skip_ref) {
+    // to skip the checking of an instruction, just copy the reg state to reference design
+    ref_difftest_regcpy(&cpu, DIFFTEST_TO_REF);
+    is_skip_ref = false;
+    return;
+  }
+
+  ref_difftest_exec(1);
+  ref_difftest_regcpy(&ref_r, DIFFTEST_TO_DUT);
+
+  checkregs(&ref_r, pc);
+  #endif
+}
---
+++ npc/src/csrc/monitor/monitor.c
@@ -0,0 +1,181 @@
+#include "cpu/cpu.h"
+#include "utils.h"
+#include <common.h>
+#include <cstdlib>
+#include <sdb/sdb.h>
+#include <fcntl.h>
+#include <gelf.h>
+#include <libelf.h>
+#include <getopt.h>
+#include <memory/paddr.h>
+
+void init_rand();
+void init_mem();
+static void welcome() {
+  printf("Welcome to %s-NPC!\n", ANSI_FMT("riscv32e", ANSI_FG_YELLOW));
+  printf("For help, type \"help\"\n");
+}
+
+static char* elf_file = NULL;
+static char* img_file = NULL;
+static char* diff_so_file = NULL;
+static int difftest_port = 1234;
+
+static struct funtion_info {
+    char *name;
+    long addr;
+    long size;
+}   funtion_info_table[100];
+
+static int funtion_index = 0;
+
+#ifdef FTRACE
+static void funtion_push(char *name, long addr, long size) {
+    funtion_info_table[funtion_index].name = name;
+    funtion_info_table[funtion_index].addr = addr;
+    funtion_info_table[funtion_index].size = size;
+    funtion_index++;
+}
+#endif
+
+static int get_funt_index(long addr){
+    for(int i = 0; i < funtion_index; i++){
+        if(funtion_info_table[i].addr <= addr && addr < funtion_info_table[i].addr + funtion_info_table[i].size){
+            return i;
+        }
+    }
+    return -1;
+}
+
+char* get_func_name(long addr){
+    int index = get_funt_index(addr);
+    if(index == -1){
+        return NULL;
+    }
+    return funtion_info_table[index].name;
+}
+
+static long load_elf() {
+    #ifdef FTRACE
+    Elf *elf;
+    Elf_Scn *scn = NULL;
+    GElf_Shdr shdr;
+
+    if (elf_file == NULL) {
+        printf("No ELF is given. There will no function message.\n");
+        return 0;
+    }
+
+    int fd;
+    if((fd = open(elf_file, O_RDONLY, 0)) < 0){
+        printf("Can not open '%s'\n", elf_file);
+        return 0;
+    }
+    if(elf_version(EV_CURRENT) == EV_NONE){
+        printf("ELF library initialization failed: %s\n", elf_errmsg(-1));
+        return 0;
+    }
+    if ((elf = elf_begin(fd, ELF_C_READ, NULL)) == NULL){
+        printf("elf_begin() failed: %s.\n", elf_errmsg(-1));
+        return 0;
+    }
+
+    int symcount = 0;
+
+    while((scn = elf_nextscn(elf, scn)) != NULL) {
+        gelf_getshdr(scn, &shdr);
+        if(shdr.sh_type == SHT_SYMTAB) {
+            Elf_Data *data = NULL;
+            data = elf_getdata(scn, data);
+            symcount = shdr.sh_size / shdr.sh_entsize;
+            GElf_Sym sym;
+            for(int i = 0; i < symcount; i++) {
+                gelf_getsym(data, i, &sym);
+                if(GELF_ST_TYPE(sym.st_info) == STT_FUNC) {
+                    char *name = elf_strptr(elf, shdr.sh_link, sym.st_name);
+                    if (name != NULL) {
+                        funtion_push(name, sym.st_value, sym.st_size);
+                    }
+                }
+            }
+        }
+    }
+    elf_end(elf);
+    return symcount;
+    #else
+    return 0;
+    #endif
+}
+
+long load_img(char* img_file) {
+    if (img_file == NULL) {
+        printf("No image is given. Use the default build-in image.\n");
+        return DEFAULT_MSIZE; // built-in image size
+    }
+
+    FILE *fp = fopen(img_file, "rb");
+
+    fseek(fp, 0, SEEK_END);
+    long size = ftell(fp);
+
+    printf("The image is %s, size = %ld\n", img_file, size);
+
+    fseek(fp, 0, SEEK_SET);
+    int ret = fread(get_pmem(), 4, size, fp);
+    assert(ret == size/4);
+
+    fclose(fp);
+    return size;
+}
+
+static int parse_args(int argc, char *argv[]) {
+    const struct option table[] = {
+      {"batch"    , no_argument      , NULL, 'b'},
+      {"log"      , required_argument, NULL, 'l'},
+      {"diff"     , required_argument, NULL, 'd'},
+      {"port"     , required_argument, NULL, 'p'},
+      {"elf"      , required_argument, NULL, 'e'},
+      {"help"     , no_argument      , NULL, 'h'},
+      {0          , 0                , NULL,  0 },
+    };
+    int o;
+    while ( (o = getopt_long(argc, argv, "-bhl:d:p:e:", table, NULL)) != -1) {
+      switch (o) {
+        case 'b': sdb_set_batch_mode();     break;
+        case 'p': break;
+        case 'l': break;
+        case 'd': diff_so_file = optarg;    break;
+        case 'e': elf_file = optarg;        break;
+        case 1  : img_file = optarg;        return 0;
+        default:
+          printf("Usage: %s [OPTION...] IMAGE [args]\n\n", argv[0]);
+          printf("\t-b,--batch              run with batch mode\n");
+          printf("\t-l,--log=FILE           output log to FILE\n");
+          printf("\t-d,--diff=REF_SO        run DiffTest with reference REF_SO\n");
+          printf("\t-p,--port=PORT          run DiffTest with port PORT\n");
+          printf("\n");
+          exit(0);
+      }
+    }
+    return 0;
+}
+
+void init_monitor(int argc, char *argv[]) {
+    parse_args(argc, argv);
+
+    init_rand();
+
+    init_mem();
+
+    long img_size = load_img(img_file);
+
+    load_elf();
+
+    init_difftest(diff_so_file, img_size, difftest_port);
+
+    init_sdb();
+
+    init_disasm("riscv32");
+
+    welcome();
+}

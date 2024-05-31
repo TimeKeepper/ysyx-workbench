a2fe42066664ffcc241ea61d17157f2daaf6f182
tracer-ysyx <tracer@ysyx.org>
2024-05-31 16:26:00 +0900

    > generate verilog ysyx_23060198 林锦晖 Linux wenjiu-virtual-machine 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:26:00 up 1 day, 14:04,  7 users,  load average: 0.25, 0.25, 0.46

--- npc/src/ssrc/main/BCU.scala
+++ npc/src/ssrc/main/BCU.scala
@@ -2,8 +2,6 @@ package riscv_cpu
 
 import chisel3._
 import chisel3.util._
-import scala.collection._
-import javax.smartcardio.ATR
 
 // riscv cpu branch control unit
 
--- npc/src/ssrc/main/REG.scala
+++ npc/src/ssrc/main/REG.scala
@@ -1,36 +1,28 @@
 package riscv_cpu
 
 import chisel3._
-import chisel3.util._
+import chisel3.util.U_S
 
 // riscv cpu register file
 
 class REG extends Module {
     val io = IO(new Bundle {
-        val value1        = Input(UInt(16.W))
-        val value2        = Output(UInt(16.W))
-        val raddr         = Input(UInt(5.W))
-        val waddr         = Input(UInt(5.W))
-        // val wdata = Input(UInt(32.W))
-        // val waddr = Input(UInt(5.W))
-        // val wen   = Input(Bool())
+        val wdata = Input(UInt(32.W))
+        val waddr = Input(UInt(5.W))
+        val wen   = Input(Bool())
 
-        // val raddra = Input(UInt(5.W))
-        // val raddrb = Input(UInt(5.W))
-        // val rdataa  = Output(UInt(32.W))
-        // val rdatab  = Output(UInt(32.W))
+        val raddra = Input(UInt(5.W))
+        val raddrb = Input(UInt(5.W))
+        val rdataa  = Output(UInt(32.W))
+        val rdatab  = Output(UInt(32.W))
     })
 
     val regs = RegInit(VecInit(Seq.fill(32)(0.U(32.W))))
 
-    io.value2 := regs(io.raddr)
+    when(io.wen && io.waddr =/= 0.U) {
+        regs(io.waddr) := io.wdata
+    }
 
-    regs(io.waddr) := io.value1
-
-    // when(io.wen && io.waddr =/= 0.U) {
-    //     regs(io.waddr) := io.wdata
-    // }
-
-    // io.rdataa := regs(io.raddra)
-    // io.raddrb := regs(io.raddrb)
+    io.rdataa := regs(io.raddra)
+    io.raddrb := regs(io.raddrb)
 }

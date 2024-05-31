62ca978f62527725fd66139a8f80340a488c5ddd
tracer-ysyx <tracer@ysyx.org>
2024-05-31 15:43:46 +0900

    > generate verilog ysyx_23060198 林锦晖 Linux wenjiu-virtual-machine 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:43:46 up 1 day, 13:22,  7 users,  load average: 0.84, 0.81, 0.62

--- npc/src/ssrc/main/GCD.scala
+++ npc/src/ssrc/main/GCD.scala
@@ -9,9 +9,9 @@ import chisel3._
   */
 class GCD extends Module {
   val io = IO(new Bundle {
-    // val value1        = Input(UInt(16.W))
-    // val value2        = Input(UInt(16.W))
-    // val loadingValues = Input(Bool())
+    val value1        = Input(UInt(16.W))
+    val value2        = Input(UInt(16.W))
+    val loadingValues = Input(Bool())
     val outputGCD     = Output(UInt(16.W))
     val outputValid   = Output(Bool())
   })
@@ -20,6 +20,9 @@ class GCD extends Module {
   val x = Reg(UInt())
   val y = Reg(UInt())
 
+  regs(0) := io.value1
+  regs(1) := io.value2
+  regs(2) := 0.U
   x := regs(0)
   y := regs(1)
 

9fd3fbf8056f37fa30427241e24bac70fa0e8903
tracer-ysyx <tracer@ysyx.org>
2024-05-31 15:52:13 +0900

    > generate verilog ysyx_23060198 林锦晖 Linux wenjiu-virtual-machine 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:52:13 up 1 day, 13:30,  7 users,  load average: 2.91, 1.30, 0.83

--- npc/src/ssrc/main/REG.scala
+++ npc/src/ssrc/main/REG.scala
@@ -9,9 +9,9 @@ import javax.smartcardio.ATR
 
 class REG extends Module {
     val io = IO(new Bundle {
-        val wdata = Input(UInt(32.W))
-        val waddr = Input(UInt(5.W))
-        val wen   = Input(Bool())
+        // val wdata = Input(UInt(32.W))
+        // val waddr = Input(UInt(5.W))
+        // val wen   = Input(Bool())
 
         // val raddra = Input(UInt(5.W))
         // val raddrb = Input(UInt(5.W))
@@ -21,9 +21,9 @@ class REG extends Module {
 
     val regs = RegInit(VecInit(Seq.fill(32)(0.U(32.W))))
 
-    when(io.wen && io.waddr =/= 0.U) {
-        regs(io.waddr) := io.wdata
-    }
+    // when(io.wen && io.waddr =/= 0.U) {
+    //     regs(io.waddr) := io.wdata
+    // }
 
     // io.rdataa := regs(io.raddra)
     // io.raddrb := regs(io.raddrb)

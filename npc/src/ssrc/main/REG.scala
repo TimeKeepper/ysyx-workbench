3d37a42b1f82439b998f9630cf2940d8a46a30f0
tracer-ysyx <tracer@ysyx.org>
2024-05-31 14:27:51 +0900

    > generate verilog ysyx_23060198 林锦晖 Linux wenjiu-virtual-machine 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:27:51 up 1 day, 12:06,  7 users,  load average: 0.43, 0.32, 0.33

--- npc/src/ssrc/main/REG.scala
+++ npc/src/ssrc/main/REG.scala
@@ -19,8 +19,7 @@ class REG extends Module {
         val rdatab  = Output(UInt(32.W))
     })
 
-    val regs = RegInit(VecInit(Seq.fill(32)(0.U(32.W))))
-    // val regs = RegInit(Vec(32, UInt(32.W)), 0.U)
+    val regs = RegInit(Vec(32, UInt(32.W)), 0.U)
     // val regs = Reg(Wire(Vec(32, UInt(32.W))))
 
     when(io.wen && io.waddr =/= 0.U) {

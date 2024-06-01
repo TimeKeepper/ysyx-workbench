777fa9117a4856baf1248f71112aa7ff74708c56
tracer-ysyx <tracer@ysyx.org>
2024-06-02 05:03:41 +0900

    > generate verilog ysyx_23060198 林锦晖 Linux wenjiu-virtual-machine 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  05:03:41 up  6:48,  5 users,  load average: 1.60, 0.99, 0.83

--- npc/src/ssrc/main/Instructions.scala
+++ npc/src/ssrc/main/Instructions.scala
@@ -99,8 +99,8 @@ object signal_value {
     def MemOp_Type = UInt(3.W)
     def M_4BU  = 5.U(3.W)
     def M_2BU  = 2.U(3.W)
-    def M_2BS  = 4.U(3.W)
-    def M_1BU  = 1.U(3.W)
+    def M_2BS  = 1.U(3.W)
+    def M_1BU  = 4.U(3.W)
     def M_1BS  = 0.U(3.W)
 
     // ALUAsrc

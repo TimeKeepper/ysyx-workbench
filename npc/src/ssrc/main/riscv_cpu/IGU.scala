dac0378d4e7fbe2a4e3e983084105c7624ba2d4e
tracer-ysyx <tracer@ysyx.org>
2024-06-03 11:58:20 +0900

    > generate verilog ysyx_23060198 林锦晖 Linux wenjiu-virtual-machine 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:58:20 up 1 day, 13:43,  5 users,  load average: 1.01, 0.75, 0.55

--- npc/src/ssrc/main/riscv_cpu/IGU.scala
+++ npc/src/ssrc/main/riscv_cpu/IGU.scala
@@ -17,11 +17,11 @@ class IGU extends Module {
 
   val imm = MuxLookup(io.Extop, 0.U)(
     Seq(
-      immI -> Cat(Fill(21, io.inst(31)), io.inst(31, 20))
-      immU -> Cat(io.inst(31, 12), Fill(12, 0.U))
-      immS -> Cat(Fill(20, io.inst(31)), io.inst(31, 25), io.inst(11, 7))
-      immB -> Cat(Fill(20, io.inst(31)), io.inst(7), io.inst(30, 25), io.inst(11, 8), 0.U(1.W))
-      immJ -> Cat(Fill(12, io.inst(31)), io.inst(19, 12), io.inst(20), io.inst(30, 21), 0.U(1.W))
+      immI -> Cat(Fill(21, io.inst(31)), io.inst(31, 20)),
+      immU -> Cat(io.inst(31, 12), Fill(12, 0.U)),
+      immS -> Cat(Fill(20, io.inst(31)), io.inst(31, 25), io.inst(11, 7)),
+      immB -> Cat(Fill(20, io.inst(31)), io.inst(7), io.inst(30, 25), io.inst(11, 8), 0.U(1.W)),
+      immJ -> Cat(Fill(12, io.inst(31)), io.inst(19, 12), io.inst(20), io.inst(30, 21), 0.U(1.W)),
     )
   )
 

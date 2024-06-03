85fb5957a104703c982171ad5ab69cd6d76812b6
tracer-ysyx <tracer@ysyx.org>
2024-06-03 16:16:44 +0900

    > generate verilog ysyx_23060198 林锦晖 Linux wenjiu-virtual-machine 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:16:44 up 1 day, 18:01,  5 users,  load average: 0.30, 1.55, 1.19

--- npc/src/ssrc/main/riscv_cpu/IFU.scala
+++ npc/src/ssrc/main/riscv_cpu/IFU.scala
@@ -9,9 +9,11 @@ import signal_value._
 
 class IFU extends Module {
     val io = IO(new Bundle {
-        val inst_input  = Input(UInt(32.W))
+        val inst_input  = Flipped(Decoupled(UInt(32.W)))
         val inst_output = Decoupled(UInt(32.W))
     })
+
+    io.inst_input.ready := true.B
 
     val s_idle :: s_wait_ready :: Nil = Enum(2)
 
@@ -20,6 +22,8 @@ class IFU extends Module {
         s_idle -> Mux(io.inst_output.valid, s_wait_ready, s_idle),
         s_wait_ready -> Mux(io.inst_output.ready, s_idle, s_wait_ready),
     ))
+
+    io.inst_output.valid := io.inst_input.valid && io.inst_input.ready
 
     io.inst_output.bits := io.inst_input
 }

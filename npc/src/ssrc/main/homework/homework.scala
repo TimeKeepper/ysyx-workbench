73910662be1f58b9bdc267914c2fb0b4b4e20874
tracer-ysyx <tracer@ysyx.org>
2024-06-07 01:51:09 +0900

    > generate verilog ysyx_23060198 林锦晖 Linux wenjiu-virtual-machine 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:51:09 up 1 day, 23:41,  3 users,  load average: 0.68, 0.60, 0.77

--- npc/src/ssrc/main/homework/homework.scala
+++ npc/src/ssrc/main/homework/homework.scala
@@ -7,6 +7,39 @@ import chisel3._
 import chisel3.util._
 import chisel3.util.MuxLookup
 
+
+class key(val clk_Mhz: Int) extends Module {
+    val io = IO(new Bundle {
+        val key_in = Input(Bool())
+        val is_key_posedge = Output(Bool())
+    })
+
+    val key_reg = RegInit(false.B)
+    key_reg := io.key_in
+
+    val s_idle :: s_key_wait :: Nil = Enum(2)
+    val state = RegInit(s_idle)
+
+    when(state === s_idle && key_reg === false.B && io.key_in === true.B) {
+        state := s_key_wait
+    }
+
+    val delay_cnt = RegInit(0.U(32.W))
+    when(state === s_key_wait) {
+        delay_cnt := delay_cnt + 1.U
+    }
+
+    def delay_time = clk_Mhz * 1000000 / 1000 * 20 // 20ms
+
+    when(delay_cnt >= delay_time.U) {
+        io.is_key_posedge := true.B
+        state := s_idle
+        delay_cnt := 0.U
+    }.otherwise {
+        io.is_key_posedge := false.B
+    }
+}
+
 class Homework extends Module {
     val io = IO(new Bundle{
         val sw1 = Input(Bool())

c992695b8ccfbd3af837bde66c915ac377943601
tracer-ysyx <tracer@ysyx.org>
2024-06-06 15:09:26 +0900

    > generate verilog ysyx_23060198 林锦晖 Linux wenjiu-virtual-machine 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:09:26 up 1 day, 13:00,  3 users,  load average: 0.51, 0.33, 0.29

---
+++ npc/src/ssrc/main/homework/homework.scala
@@ -0,0 +1,34 @@
+package homework
+
+import timer
+import decoder
+
+import chisel3._
+import chisel3.util._
+import chisel3.util.MuxLookup
+
+class Homework extends Module {
+    val io = IO(new Bundle{
+        val out = Output(Vec(4, UInt(8.W)))
+    })
+
+    val time_seconds = wire(UInt(32.W))
+
+    val timer = Module(new timer.Timer)
+    time_seconds <> timer.io.time_seconds
+
+    val decoder1 = Module(new decoder.BCDDecoder)
+    val decoder2 = Module(new decoder.BCDDecoder)
+    val decoder3 = Module(new decoder.BCDDecoder)
+    val decoder4 = Module(new decoder.BCDDecoder)
+
+    decoder1.io.in := (time_seconds)(4.W)
+    decoder2.io.in := (time_seconds % 10)(4.W)
+    decoder3.io.in := (time_seconds % 100)(4.W)
+    decoder4.io.in := (time_seconds % 1000)(4.W)
+
+    decoder1.io.Output <> io.out(0)
+    decoder2.io.Output <> io.out(1)
+    decoder3.io.Output <> io.out(2)
+    decoder4.io.Output <> io.out(3)
+}
---
+++ npc/src/ssrc/main/other/timer.scala
@@ -0,0 +1,24 @@
+package timer
+
+import chisel3._
+import chisel3.util._
+import chisel3.util.MuxLookup
+
+class Timer(val clk_Mhz: Int) extends Module {
+    val io = IO(new Bundle {
+        val time_seconds = Output(UInt(32.W))
+    })
+
+    val timer_counter = RegInit(0.U(32.W))
+
+    val total_seconds = RegInit(0.U(32.W))
+
+    timer_counter := timer_counter + 1.U
+
+    when(timer_counter === (clk_Mhz * 1000 - 1).U) {
+        timer_counter := 0.U
+        total_seconds := total_seconds + 1.U
+    }
+
+    io.time_seconds := total_seconds
+}

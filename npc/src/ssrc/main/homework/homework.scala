5d309cf37cf2692c47636decefb47029288a5030
tracer-ysyx <tracer@ysyx.org>
2024-06-06 15:11:32 +0900

    > generate verilog ysyx_23060198 林锦晖 Linux wenjiu-virtual-machine 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:11:32 up 1 day, 13:02,  3 users,  load average: 1.41, 0.71, 0.43

--- npc/src/ssrc/main/homework/homework.scala
+++ npc/src/ssrc/main/homework/homework.scala
@@ -22,10 +22,10 @@ class Homework extends Module {
     val decoder3 = Module(new decoder.BCDDecoder)
     val decoder4 = Module(new decoder.BCDDecoder)
 
-    decoder1.io.in := (time_seconds)(4.W)
-    decoder2.io.in := (time_seconds % 10.U)(4.W)
-    decoder3.io.in := (time_seconds % 100.U)(4.W)
-    decoder4.io.in := (time_seconds % 1000.U)(4.W)
+    decoder1.io.in := (time_seconds)(3, 0)
+    decoder2.io.in := (time_seconds % 10.U)(3, 0)
+    decoder3.io.in := (time_seconds % 100.U)(3, 0)
+    decoder4.io.in := (time_seconds % 1000.U)(3, 0)
 
     decoder1.io.out <> io.out(0)
     decoder2.io.out <> io.out(1)

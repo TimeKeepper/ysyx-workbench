6747723dca295acca2e48f6fec1f79838b059231
tracer-ysyx <tracer@ysyx.org>
2024-06-09 06:29:09 +0900

    > sim RTL ysyx_23060198 林锦晖 Linux wenjiu-virtual-machine 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  06:29:09 up 4 days,  4:19,  3 users,  load average: 0.42, 0.42, 0.51

---
+++ npc/src/vsrc/top.v
@@ -0,0 +1,38 @@
+import "DPI-C" function int npc_trap (input int ra);
+
+module top(
+    input clk,
+    input rst,
+    input [31:0] inst_bits,
+    input inst_valid,
+
+    input [31:0] mem_data,
+
+    output [2:0] memop,
+    output [31:0] memdata,
+    output mem_wen,
+    output [31:0] mem_addr
+);
+
+always @(inst_bits) begin
+    if(inst_bits == 32'h00100073)
+        $display("sim has been stop at clk_cnt %d", npc_trap(npc.riscv_cpu.REG.gpr_10));
+end
+
+wire inst_ready;
+
+npc npc (
+    .clock(clk),
+    .reset(rst),
+    .io_inst_ready(inst_ready),
+    .io_inst_bits(inst_bits),
+    .io_inst_valid(inst_valid),
+    .io_mem_rdata(mem_data),
+
+    .io_mem_wop(memop),
+    .io_mem_wdata(memdata),
+    .io_mem_wen(mem_wen),
+    .io_mem_raddr(mem_addr)
+);
+
+endmodule

1dae96f37c945784ae41189b75cb539cd19a0265
tracer-ysyx <tracer@ysyx.org>
2024-06-08 15:43:52 +0900

    > generate verilog ysyx_23060198 林锦晖 Linux wenjiu-virtual-machine 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:43:52 up 3 days, 13:34,  3 users,  load average: 1.25, 0.69, 0.52

--- npc/ps2.v
+++ npc/ps2.v
@@ -1,4 +1,1 @@
 make[1]: Entering directory '/home/wen-jiu/my_ysyx_project/ysyx-workbench'
-make[1]: Leaving directory '/home/wen-jiu/my_ysyx_project/ysyx-workbench'
-# mkdir -p ./build
-mill -i playground.runMain Elaborate --target-dir ./src/vsrc

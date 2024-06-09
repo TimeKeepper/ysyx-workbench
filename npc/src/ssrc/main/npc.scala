a8eb323f3ec82d3638dfcbec62f4227fe946c39e
tracer-ysyx <tracer@ysyx.org>
2024-06-10 03:51:15 +0900

    > generate verilog ysyx_23060198 林锦晖 Linux wenjiu-virtual-machine 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  03:51:15 up 5 days,  1:42,  5 users,  load average: 6.32, 3.08, 1.43

--- npc/src/ssrc/main/npc.scala
+++ npc/src/ssrc/main/npc.scala
@@ -22,7 +22,7 @@ class npc extends Module {
 
   val riscv_cpu = Module(new CPU)
 
-  riscv_cpu.io.inst_input <> io.sram.io.inst_output
+  riscv_cpu.io.inst_input <> sram.io.inst_output
   riscv_cpu.io.mem_rdata  <> io.mem_rdata
   riscv_cpu.io.mem_wraddr <> io.mem_wraddr
 

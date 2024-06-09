36262261881c5f54dedae99fcd2202e7da6d5267
tracer-ysyx <tracer@ysyx.org>
2024-06-09 18:06:13 +0900

    > generate verilog ysyx_23060198 林锦晖 Linux wenjiu-virtual-machine 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:06:13 up 4 days, 15:57,  4 users,  load average: 0.27, 0.41, 0.30

--- npc/src/ssrc/main/npc.scala
+++ npc/src/ssrc/main/npc.scala
@@ -11,27 +11,18 @@ class npc extends Module {
     val mem_rdata = Input(UInt(32.W))
     val mem_raddr = Output(UInt(32.W))
 
-    // val mem_wdata = Output(UInt(32.W))
-    // val mem_wop   = Output(UInt(3.W))
-    // val mem_wen   = Output(Bool())
+    val mem_wdata = Output(UInt(32.W))
+    val mem_wop   = Output(UInt(3.W))
+    val mem_wen   = Output(Bool())
   })
-
-  val I_mem = RegInit(VecInit(Seq.fill(256)(0.U(32.W))))
-  val D_mem = RegInit(VecInit(Seq.fill(256)(0.U(32.W))))
 
   val riscv_cpu = Module(new CPU)
 
-  riscv_cpu.io.inst_input.valid := true.B
-  riscv_cpu.io.inst_input.bits := I_mem(riscv_cpu.io.pc_output(7, 0))
+  riscv_cpu.io.inst_input <> io.inst
+  riscv_cpu.io.mem_rdata  := io.mem_rdata
+  io.mem_raddr            := riscv_cpu.io.mem_raddr
 
-  riscv_cpu.io.mem_rdata := D_mem(riscv_cpu.io.mem_raddr(7, 0))
-  D_mem(riscv_cpu.io.mem_raddr(7, 0)) := riscv_cpu.io.mem_wdata
-
-  // riscv_cpu.io.inst_input <> io.inst
-  // riscv_cpu.io.mem_rdata  := io.mem_rdata
-  // io.mem_raddr            := riscv_cpu.io.mem_raddr
-
-  // io.mem_wdata := riscv_cpu.io.mem_wdata
-  // io.mem_wop   := riscv_cpu.io.mem_wop
-  // io.mem_wen   := riscv_cpu.io.mem_wen
+  io.mem_wdata := riscv_cpu.io.mem_wdata
+  io.mem_wop   := riscv_cpu.io.mem_wop
+  io.mem_wen   := riscv_cpu.io.mem_wen
 }

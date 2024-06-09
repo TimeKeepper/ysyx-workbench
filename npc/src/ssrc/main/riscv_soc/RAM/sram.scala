49354c6dde904619dd55ebb8936000e55d4e30c9
tracer-ysyx <tracer@ysyx.org>
2024-06-10 03:50:01 +0900

    > generate verilog ysyx_23060198 林锦晖 Linux wenjiu-virtual-machine 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  03:50:01 up 5 days,  1:40,  5 users,  load average: 4.06, 2.08, 1.01

---
+++ npc/src/ssrc/main/riscv_soc/RAM/sram.scala
@@ -0,0 +1,35 @@
+package sram
+
+import chisel3._
+import chisel3.util._
+
+import signal_value._
+
+//此模块将32为数据读取并根据memop处理数据，延迟一个周期后发送给cpu
+
+class SRAM extends Module {
+    val io = IO(new Bundle{
+        val inst_input = Flipped(Decoupled(UInt(32.W)))
+        val inst_output = Decoupled(UInt(32.W))
+    })
+
+    val s_idle :: s_wait_ready :: Nil = Enum(2)
+    val state = RegInit(s_idle)
+
+    io.inst_input.ready := state === s_idle
+    io.inst_output.valid := io.inst_input.valid && state === s_wait_ready
+
+    when(state === s_idle) {
+        when(io.inst_output.valid) {
+            state := s_wait_ready
+        }.otherwise {
+            state := s_idle
+        }
+    }.otherwise {
+        when(io.inst_output.ready){
+            state := s_idle
+        }
+    }
+
+    io.inst_output.bits := io.inst_input
+}

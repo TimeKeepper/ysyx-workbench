07a5d6414f9102e5b5dce7f67ef27f85b38abda5
tracer-ysyx <tracer@ysyx.org>
2024-06-13 15:51:25 +0900

    > generate verilog ysyx_23060198 林锦晖 Linux wenjiu-virtual-machine 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:51:25 up 18:15,  3 users,  load average: 0.84, 0.76, 0.53

--- npc/src/ssrc/main/riscv_soc/CPU/IGU/GNU.scala
+++ npc/src/ssrc/main/riscv_soc/CPU/IGU/GNU.scala
@@ -31,37 +31,22 @@ class GNU extends Module{
     val io = IO(new Bundle{
         val in       = Flipped(Decoupled(new GNU_input))
         val out      = new GNU_output
-        // val inst     = Output(UInt(32.W))
-        // val RegWr    = Output(Bool())
-        // val Branch   = Output(Bran_Type)
-        // val MemtoReg = Output(Bool())
-        // val MemWr    = Output(Bool())
-        // val MemOp    = Output(MemOp_Type)
-        // val ALUAsrc  = Output(ALUAsrc_Type)
-        // val ALUBsrc  = Output(ALUBSrc_Type)
-        // val ALUctr   = Output(ALUctr_Type)
-        // val csr_ctr  = Output(CSR_Type)
-        // val Imm      = Output(UInt(32.W))
-        // val PC       = Output(UInt(32.W))
     })
 
     io.in.ready := true.B
-
-    val inst = Wire(UInt(32.W))
-    inst <> io.out.inst
 
     val idu = Module(new IDU)
     val igu = Module(new IGU)
 
     when(io.in.valid) {
-        inst := io.in.bits.inst
-        io.out.Branch := idu.io.Branch
+        io.out.inst     := io.in.bits.inst
+        io.out.Branch   := idu.io.Branch
     }.otherwise {
-        inst := NOP.U(32.W)
-        io.out.Branch := Bran_NoC
+        io.out.inst     := NOP.U(32.W)
+        io.out.Branch   := Bran_NoC
     }
 
-    idu.io.inst <> inst
+    idu.io.inst <> io.out.inst
     idu.io.RegWr <> io.out.RegWr
     idu.io.MemtoReg <> io.out.MemtoReg
     idu.io.MemWr <> io.out.MemWr
@@ -71,7 +56,7 @@ class GNU extends Module{
     idu.io.ALUctr <> io.out.ALUctr
     idu.io.csr_ctr <> io.out.csr_ctr
 
-    igu.io.inst <> inst
+    igu.io.inst <> io.out.inst
     igu.io.ExtOp <> idu.io.ExtOp
     igu.io.imm  <> io.out.Imm
 

924b5296e5003131a9f16da5d8aa55d1392d4809
tracer-ysyx <tracer@ysyx.org>
2024-06-13 20:53:56 +0900

    > generate verilog ysyx_23060198 林锦晖 Linux wenjiu-virtual-machine 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:53:56 up 23:18,  3 users,  load average: 0.37, 0.33, 0.29

--- npc/src/ssrc/main/riscv_soc/CPU/WBU/WBU.scala
+++ npc/src/ssrc/main/riscv_soc/CPU/WBU/WBU.scala
@@ -7,29 +7,51 @@ import signal_value._
 
 // riscv writeback unit
 
+class WBU_input extends Bundle{
+    val RegWr    = Input(Bool())
+    val Branch   = Input(Bran_Type)
+    val MemtoReg = Input(Bool())
+    val MemWr    = Input(Bool())
+    val MemOp    = Input(MemOp_Type)
+    val csr_ctr  = Input(CSR_Type)
+    val Imm      = Input(UInt(32.W))
+    val GPR_Adata= Input(UInt(32.W))
+    val GPR_Bdata= Input(UInt(32.W))
+    val PC       = Input(UInt(32.W))
+    val CSR      = Input(UInt(32.W))
+    val Result   = Input(UInt(32.W))
+    val Zero     = Input(Bool())
+    val Less     = Input(Bool())
+    val GPR_waddr= Input(UInt(5.W))
+
+    val Mem_rdata  = Input(UInt(32.W))
+}
+
+class WBU_output extends Bundle{
+    val Mem_wraddr = Output(UInt(32.W))
+    val Mem_wdata  = Output(UInt(32.W))
+    val MemOp_o    = Output(MemOp_Type)
+    val MemWr_o    = Output(Bool())
+
+    val Next_Pc   = Output(UInt(32.W))
+    val Reg_waddr = Output(UInt(5.W))
+    val Reg_wdata = Output(UInt(32.W))
+    val Reg_wen   = Output(Bool())
+    val CSR_ctr_o = Output(CSR_Type)
+    val CSR_waddra= Output(UInt(12.W))
+    val CSR_waddrb= Output(UInt(12.W))
+    val CSR_wdataa= Output(UInt(32.W))
+    val CSR_wdatab= Output(UInt(32.W))
+}
+
 class WBU extends Module {
     val io = IO(new Bundle{
-        val RegWr    = Input(Bool())
-        val Branch   = Input(Bran_Type)
-        val MemtoReg = Input(Bool())
-        val MemWr    = Input(Bool())
-        val MemOp    = Input(MemOp_Type)
-        val csr_ctr  = Input(CSR_Type)
-        val Imm      = Input(UInt(32.W))
-        val GPR_Adata= Input(UInt(32.W))
-        val GPR_Bdata= Input(UInt(32.W))
-        val PC       = Input(UInt(32.W))
-        val CSR      = Input(UInt(32.W))
-        val Result   = Input(UInt(32.W))
-        val Zero     = Input(Bool())
-        val Less     = Input(Bool())
-        val GPR_waddr= Input(UInt(5.W))
+        val in = new WBU_input 
 
         val Mem_wraddr = Output(UInt(32.W))
-        val Mem_rdata  = Input(UInt(32.W))
         val Mem_wdata  = Output(UInt(32.W))
         val MemOp_o    = Output(MemOp_Type)
-        val MemWr_o    = Input(Bool())
+        val MemWr_o    = Output(Bool())
 
         val Next_Pc   = Output(UInt(32.W))
         val Reg_waddr = Output(UInt(5.W))

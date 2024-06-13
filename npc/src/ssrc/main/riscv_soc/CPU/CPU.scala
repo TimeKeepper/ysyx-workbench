0c74eea603b0cdaff7dfee08595d2d7df212ce3c
tracer-ysyx <tracer@ysyx.org>
2024-06-13 16:40:24 +0900

    > generate verilog ysyx_23060198 林锦晖 Linux wenjiu-virtual-machine 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:40:24 up 19:04,  3 users,  load average: 1.43, 1.09, 0.77

--- npc/src/ssrc/main/riscv_soc/CPU/CPU.scala
+++ npc/src/ssrc/main/riscv_soc/CPU/CPU.scala
@@ -49,8 +49,8 @@ class CPU() extends Module {
   // GNU Connections
   GNU.io.in.bits.inst <> io.inst_input.bits
   GNU.io.in.bits.PC   <> Cur_PC
-  GNU.io.in.bits.GPR_Adata <> GPR_RDATAa
-  GNU.io.in.bits.GPR_Bdata <> GPR_RDATAb
+  GNU.io.in.bits.GPR_Adata <> REG.io.raddra
+  GNU.io.in.bits.GPR_Bdata <> REG.io.raddrb
   GNU.io.in.valid     <> io.inst_input.valid
   GNU.io.in.ready     <> io.inst_input.ready
 

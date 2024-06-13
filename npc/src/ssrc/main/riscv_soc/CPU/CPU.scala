9fc1ed714e40720f19d521180837266cc6b2360f
tracer-ysyx <tracer@ysyx.org>
2024-06-13 16:43:07 +0900

    > generate verilog ysyx_23060198 林锦晖 Linux wenjiu-virtual-machine 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:43:07 up 19:07,  3 users,  load average: 1.15, 1.16, 0.85

--- npc/src/ssrc/main/riscv_soc/CPU/CPU.scala
+++ npc/src/ssrc/main/riscv_soc/CPU/CPU.scala
@@ -31,8 +31,8 @@ class CPU() extends Module {
 
   val GPR_WADDR       = Wire(UInt(5.W))
   val GPR_WDATA       = Wire(UInt(32.W))
-  val GPR_RDATAa      = Wire(UInt(32.W))
-  val GPR_RDATAb      = Wire(UInt(32.W))
+  val GPR_RADDRa      = Wire(UInt(5.W))
+  val GPR_RADDRb      = Wire(UInt(5.W))
 
   val CSR_WADDRa      = Wire(UInt(12.W))
   val CSR_WADDRb      = Wire(UInt(12.W))
@@ -87,6 +87,8 @@ class CPU() extends Module {
   GPR_RADDRb    := GNU.io.out.inst(24, 20)
   REG.io.raddra := GPR_RADDRa
   REG.io.raddrb := GPR_RADDRb
+  GPR_RDATAa    := REG.io.rdataa
+  GPR_RDATAb    := REG.io.rdatab
 
   val PCAval = Wire(UInt(32.W))
   val PCBval = Wire(UInt(32.W))

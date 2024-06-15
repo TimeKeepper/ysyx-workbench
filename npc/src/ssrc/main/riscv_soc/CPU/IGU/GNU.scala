cf0a72759cf2033417177fac5ff02c59d169ccd8
tracer-ysyx <tracer@ysyx.org>
2024-06-15 15:22:19 +0900

    > generate verilog ysyx_23060198 林锦晖 Linux wenjiu-virtual-machine 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:22:19 up 2 days, 17:46,  3 users,  load average: 0.95, 0.51, 0.35

--- npc/src/ssrc/main/riscv_soc/CPU/IGU/GNU.scala
+++ npc/src/ssrc/main/riscv_soc/CPU/IGU/GNU.scala
@@ -48,7 +48,7 @@ class GNU extends Module {
   state := MuxLookup(state, s_wait_valid)(
     Seq(
       s_wait_valid -> Mux(io.in.valid, s_wait_ready, s_wait_valid),
-      s_wait_ready -> Mux(io.in.ready, s_wait_valid, s_wait_ready)
+      s_wait_ready -> Mux(io.out.ready, s_wait_valid, s_wait_ready)
     )
   )
 

bb831b89d4af395e487d83ed3c48dfac07c0befc
tracer-ysyx <tracer@ysyx.org>
2024-06-20 03:54:44 +0900

    > generate verilog ysyx_23060198 林锦晖 Linux wenjiu-virtual-machine 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  03:54:44 up 5 min,  2 users,  load average: 2.67, 3.10, 1.51

--- npc/src/ssrc/main/riscv_soc/CPU/IGU/GNU.scala
+++ npc/src/ssrc/main/riscv_soc/CPU/IGU/GNU.scala
@@ -35,27 +35,14 @@ class GNU_output extends Bundle{
 
 class GNU extends Module{
     val io = IO(new Bundle{
-        val in       = Flipped(Decoupled(new GNU_input))
+        val in       = Input(new GNU_input)
         val out      = new GNU_output
     })
-
-    io.in.ready := true.B
-
-    val inst = Wire(UInt(32.W))
-    inst <> io.out.inst
 
     val idu = Module(new IDU)
     val igu = Module(new IGU)
 
-    when(io.in.valid) {
-        inst            := io.in.bits.inst
-        io.out.Branch   := idu.io.Branch
-    }.otherwise {
-        inst            := NOP.U(32.W)
-        io.out.Branch   := Bran_NoC
-    }
-
-    idu.io.inst     <> inst
+    idu.io.inst     <> io.in.inst
     idu.io.RegWr    <> io.out.RegWr
     idu.io.MemtoReg <> io.out.MemtoReg
     idu.io.MemWr    <> io.out.MemWr
@@ -65,7 +52,7 @@ class GNU extends Module{
     idu.io.ALUctr   <> io.out.ALUctr
     idu.io.csr_ctr  <> io.out.csr_ctr
 
-    igu.io.inst     <> inst
+    igu.io.inst     <> io.in.inst
     igu.io.ExtOp    <> idu.io.ExtOp
     igu.io.imm      <> io.out.Imm
 
@@ -73,7 +60,7 @@ class GNU extends Module{
     io.out.GPR_Bdata <> io.in.bits.GPR_Bdata
     io.out.GPR_waddr <> inst(11, 7)
     io.out.PC       <> io.in.bits.PC
-    io.out.inst     <> inst
+    io.out.inst     <> io.in.inst
  
     io.out.CSR_raddr := MuxLookup(io.out.csr_ctr, io.out.Imm(11, 0))(Seq(
         CSR_R1W0 -> "h341".U,

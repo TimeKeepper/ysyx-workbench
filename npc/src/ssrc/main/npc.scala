1cf1ef434b7d002739c12a1ce47d0c827aa3aa56
tracer-ysyx <tracer@ysyx.org>
2024-06-20 03:57:55 +0900

    > generate verilog ysyx_23060198 林锦晖 Linux wenjiu-virtual-machine 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  03:57:55 up 8 min,  2 users,  load average: 1.46, 2.53, 1.60

--- npc/src/ssrc/main/npc.scala
+++ npc/src/ssrc/main/npc.scala
@@ -8,7 +8,7 @@ import chisel3.util._
 
 class npc extends Module {
   val io = IO(new Bundle {
-    val inst      = Flipped(Decoupled(UInt(32.W)))
+    val inst      = Input(UInt(32.W))
     val mem_rdata = Input(UInt(32.W))
     val mem_wraddr = Output(UInt(32.W))
 
@@ -16,13 +16,10 @@ class npc extends Module {
     val mem_wop   = Output(UInt(3.W))
     val mem_wen   = Output(Bool())
   })
-
-  val sram = Module(new SRAM)
-  sram.io.inst_input <> io.inst
-
+  
   val riscv_cpu = Module(new CPU)
 
-  riscv_cpu.io.inst_input <> sram.io.inst_output
+  riscv_cpu.io.inst_input <> io.inst
   riscv_cpu.io.mem_rdata  <> io.mem_rdata
   riscv_cpu.io.mem_wraddr <> io.mem_wraddr
 

a05731e6bd2ce4e112e3538949186d102b288832
tracer-ysyx <tracer@ysyx.org>
2024-06-27 21:45:43 +0900

    > generate verilog ysyx_23060198 林锦晖 Linux wenjiu-virtual-machine 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:45:43 up 12 min,  2 users,  load average: 0.85, 1.39, 0.96

--- npc_module_test/src/ssrc/main/Elaborate.scala
+++ npc_module_test/src/ssrc/main/Elaborate.scala
@@ -8,5 +8,5 @@ object Elaborate extends App {
       "locationInfoStyle=wrapInAtSquareBracket"
     ).reduce(_ + "," + _)
   )
-  circt.stage.ChiselStage.emitSystemVerilogFile(new decoder.BCDDecoder(), args, firtoolOptions)
+  circt.stage.ChiselStage.emitSystemVerilogFile(new homework.Homework(), args, firtoolOptions)
 }

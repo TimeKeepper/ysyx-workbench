0e9729129bd7eb798bb591e6b547ca1ce4e614a7
202280542-Wen jiu <2784307979@qq.com>
2024-06-28 09:47:12 +0900

    将其他模块的编译验证与npc分离

---
+++ npc_module_test/src/ssrc/Elaborate.scala
@@ -0,0 +1,14 @@
+import homework._
+
+object Elaborate extends App {
+  val firtoolOptions = Array(
+    "--lowering-options=" + List(
+      // make yosys happy
+      // see https://github.com/llvm/circt/blob/main/docs/VerilogGeneration.md
+      "disallowLocalVariables",
+      "disallowPackedArrays",
+      "locationInfoStyle=wrapInAtSquareBracket"
+    ).reduce(_ + "," + _)
+  )
+  circt.stage.ChiselStage.emitSystemVerilogFile(new homework.Homework(), args, firtoolOptions)
+}

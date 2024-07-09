a975a68175a4efb91964555af2725f5c6faa98a5
tracer-ysyx <tracer@ysyx.org>
2024-07-09 21:28:00 +0900

    > generate verilog ysyx_23060198 林锦晖 Linux wenjiu-virtual-machine 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:28:00 up 3 days, 21:35,  4 users,  load average: 0.76, 0.48, 0.38

--- npc/src/ssrc/main/riscv_soc/CPU/GNU/GNU.scala
+++ npc/src/ssrc/main/riscv_soc/CPU/GNU/GNU.scala
@@ -44,7 +44,7 @@ class GNU extends Module{
     val idu = Module(new IDU)
     val igu = Module(new IGU)
 
-    idu.io.inst     <> Mux(io.in.valid, io.in.bits.inst, NOP)
+    idu.io.inst     <> Mux(io.in.valid, io.in.bits.inst, NOP.U)
     idu.io.RegWr    <> io.out.RegWr
     idu.io.Branch   <> io.out.Branch
     idu.io.MemtoReg <> io.out.MemtoReg
@@ -55,13 +55,13 @@ class GNU extends Module{
     idu.io.ALUctr   <> io.out.ALUctr
     idu.io.csr_ctr  <> io.out.csr_ctr
 
-    igu.io.inst     <> Mux(io.in.valid, io.in.bits.inst, NOP)
+    igu.io.inst     <> Mux(io.in.valid, io.in.bits.inst, NOP.U)
     igu.io.ExtOp    <> idu.io.ExtOp
     igu.io.imm      <> io.out.Imm
 
     io.out.GPR_Adata <> io.in.bits.GPR_Adata
     io.out.GPR_Bdata <> io.in.bits.GPR_Bdata
-    io.out.GPR_waddr <> Mux(io.in.valid, io.in.bits.inst(11, 7), NOP(11, 7))
+    io.out.GPR_waddr <> Mux(io.in.valid, io.in.bits.inst(11, 7), NOP.U(11, 7))
     io.out.PC       <> io.in.bits.PC
     io.out.inst     <> io.in.bits.inst
  

64a4237920cb3760a5128bb259f5177b83cf26ec
tracer-ysyx <tracer@ysyx.org>
2024-07-09 18:09:56 +0900

    > generate verilog ysyx_23060198 林锦晖 Linux wenjiu-virtual-machine 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:09:56 up 3 days, 18:17,  4 users,  load average: 0.65, 0.69, 0.64

--- npc/src/ssrc/main/riscv_soc/RAM/sram.scala
+++ npc/src/ssrc/main/riscv_soc/RAM/sram.scala
@@ -10,6 +10,22 @@ class SRAM extends Module {
         val inst_input = Input(UInt(32.W))
         val inst_output = Decoupled(UInt(32.W))
     })
-    io.inst_output.bits := io.inst_input
-    io.inst_output.valid := true.B
+    
+    val inst_cache = RegInit(UInt(32.W), "h0".U)
+
+    val s_idle :: s_wait_ready :: Nil = Enum(2)
+    val state = RegInit(s_idle)
+
+    inst_cache := io.inst_input
+    
+    io.inst_output.bits := inst_cache
+    
+    state := MuxLookup(state, s_idle)(
+        Seq(
+            s_idle       -> Mux(io.inst_output.valid, s_wait_ready, s_idle),
+            s_wait_ready -> Mux(io.inst_output.ready, s_idle, s_wait_ready)
+        )
+    )
+
+    io.inst_output.valid := (state === s_idle)
 }

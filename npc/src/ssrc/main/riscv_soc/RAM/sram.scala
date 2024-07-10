45f47cbd9b3822b5de325dcb4ce1d759b9c1c04f
tracer-ysyx <tracer@ysyx.org>
2024-07-10 21:38:24 +0900

    > generate verilog ysyx_23060198 林锦晖 Linux wenjiu-virtual-machine 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:38:24 up  2:05,  3 users,  load average: 0.19, 0.28, 0.34

--- npc/src/ssrc/main/riscv_soc/RAM/sram.scala
+++ npc/src/ssrc/main/riscv_soc/RAM/sram.scala
@@ -3,11 +3,21 @@ package ram
 import chisel3._
 import chisel3.util._
 
+class Icache_input extends Bundle{
+    val inst = Input(UInt(32.W))
+    val addr = Input(UInt(32.W))
+}
+
+class Icache_output extends Bundle{
+    val inst = Output(UInt(32.W))
+    val addr = Output(UInt(32.W))
+}
+
 //此模块将32为数据读取并根据memop处理数据，延迟一个周期后发送给cpu
 
 class Icache extends Module {
     val io = IO(new Bundle{
-        val inst_input = Flipped(Decoupled(UInt(32.W)))
+        val in = Flipped(Decoupled(Icache_input))
         val inst_output = Decoupled(UInt(32.W))
     })
     
@@ -16,8 +26,8 @@ class Icache extends Module {
     val s_wait_valid :: s_wait_ready :: s_busy :: Nil = Enum(3)
     val state = RegInit(s_wait_valid)
 
-    when(io.inst_input.valid && io.inst_input.ready) {
-        inst_cache := io.inst_input.bits
+    when(io.in.valid && io.in.ready) {
+        inst_cache := io.in.bits.inst
     }
     
     io.inst_output.bits := inst_cache
@@ -29,6 +39,6 @@ class Icache extends Module {
         )
     )
 
-    io.inst_output.valid := io.inst_input.valid
-    io.inst_input.ready  := state === s_wait_valid
+    io.inst_output.valid := io.in.valid
+    io.in.ready  := state === s_wait_valid
 }

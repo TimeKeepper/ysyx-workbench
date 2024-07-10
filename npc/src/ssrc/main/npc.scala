cb4a76e309904c19a6b958b6b8e0634348dc3b17
tracer-ysyx <tracer@ysyx.org>
2024-07-11 00:39:44 +0900

    > generate verilog ysyx_23060198 林锦晖 Linux wenjiu-virtual-machine 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:39:44 up  5:06,  3 users,  load average: 0.77, 1.24, 1.08

--- npc/src/ssrc/main/npc.scala
+++ npc/src/ssrc/main/npc.scala
@@ -24,9 +24,10 @@ class npc extends Module {
   val riscv_cpu = Module(new CPU)
 
   IFU.io.in.bits.inst <> io.Imem_rdata.bits
-  IFU.io.in.valid     <> io.Imem_rdata.valid
+  IFU.io.in.valid     <> riscv_cpu.io.Imem_raddr.valid
+  IFU.io.in.ready     <> riscv_cpu.io.Imem_raddr.ready
   IFU.io.in.ready     <> io.Imem_rdata.ready
-  IFU.io.in.bits.addr <> riscv_cpu.io.Imem_raddr
+  IFU.io.in.bits.addr <> riscv_cpu.io.Imem_raddr.bits
   IFU.io.out.bits.inst  <> riscv_cpu.io.Imem_rdata.bits
   IFU.io.out.bits.addr  <> io.Imem_raddr
   IFU.io.out.valid      <> riscv_cpu.io.Imem_rdata.valid

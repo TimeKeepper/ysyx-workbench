156112067620f520253dcf659226f6a323841f90
TimeKeepper <2784307979@qq.com>
2024-08-17 14:47:53 +0900

    修改链接脚本,实现二级bootloader

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -19,8 +19,10 @@
 #define SERIAL_DLM      (SERIAL_PORT + 1) // divisor latch high
 
 extern char _sheap, _eheap;
+extern char _sssbl, _essbl, _sssbl_load;
 extern char _stext, _etext, _stext_load;
 extern char _srodata, _erodata, _srodata_load;
+extern char _sdata_extra, _edata_extra, _sdata_extra_load;
 extern char _sdata, _edata, _sdata_load;
 extern char _sbss, _ebss, _sbss_load;
 int main(const char *args);
@@ -61,13 +63,23 @@ void halt(int code) {
   while(1);
 }
 
-void boot_memcpy(void *dst, const void *src, size_t n){ // 在bootloader运行的时候,memcpy还没有正常加载
+static void inline boot_memcpy(void *dst, const void *src, size_t n){ // 在bootloader运行的时候,memcpy还没有正常加载
   while(n--){
     *(uint8_t *)dst++ = *(uint8_t *)src++;
   }
 }
 
-int boot_loader(void) {
+int FSBL(void){
+  uint32_t *dst = (uint32_t *)&_sssbl;
+  const uint32_t *src = (uint32_t *)&_sssbl_load;
+  size_t n = (size_t)(&_essbl - &_sssbl);// 确保全部加载
+
+  boot_memcpy(dst, src, n); // 程序加载
+
+  return 0;
+}
+
+int SSBL(void) {
   uint32_t *dst = (uint32_t *)&_stext;
   const uint32_t *src = (uint32_t *)&_stext_load;
   size_t n = (size_t)(&_etext - &_stext);// 确保全部加载
@@ -79,6 +91,12 @@ int boot_loader(void) {
   n = (size_t)(&_erodata - &_srodata);
 
   boot_memcpy(dst, src, n); 
+
+  dst = (uint32_t *)&_sdata_extra;
+  src = (uint32_t *)&_sdata_extra_load;
+  n = (size_t)(&_edata_extra - &_sdata_extra);
+
+  boot_memcpy(dst, src, n);
 
   dst = (uint32_t *)&_sdata;
   src = (uint32_t *)&_sdata_load;
@@ -92,7 +110,6 @@ int boot_loader(void) {
 
   boot_memcpy(dst, src, n); // bss加载
 
-  uart_init();
   return 0;
 }
 
@@ -125,9 +142,11 @@ void _print_creater_info(void){
 
 void _trm_init() {
   int ret;
-  ret = boot_loader();
+  ret = FSBL();
 
-  // printf("Bootloader [%x, %x] load finished\n", (uint32_t)&_stext, (uint32_t)&_stext + (uint32_t)&data_load_size);
+  ret = SSBL();
+
+  uart_init();
 
   _print_creater_info();
 

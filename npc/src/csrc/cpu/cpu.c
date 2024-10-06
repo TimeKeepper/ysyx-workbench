b43264cb1d27135f8b13200051972511d0550bb0
tracer-ysyx <tracer@ysyx.org>
2024-10-06 14:30:21 +0800

    > sim YSYXSOC ysyx_23060198 林锦晖 Linux WIN-H8ULN2I4VKL 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:30:21 up 1 day, 16:01,  6 users,  load average: 0.09, 0.08, 0.13

--- npc/src/csrc/cpu/cpu.c
+++ npc/src/csrc/cpu/cpu.c
@@ -248,8 +248,9 @@ void error_waddr(){
 int npc_trap (int a0){
     npc_state.state = NPC_END;
     npc_state.halt_ret = a0;
+    double ipc = (double)inst_cnt / (double)clk_cnt;
     Log("a0: %d inst: %lu clk: %lu", a0, inst_cnt, clk_cnt);
-    Log("ipc: %lf", (double)inst_cnt / (double)clk_cnt);
+    Log("ipc: %lf", ipc);
     if(a0 == 0) printf("\033[1;32mHit good trap\033[0m\n");
     else printf("\033[1;31mHit bad trap\033[0m\n");
     wave_Trace_once();
--- npc/src/csrc/main.c
+++ npc/src/csrc/main.c
@@ -2,9 +2,6 @@
 #include <main.h>
 
 int main(int argc, char **argv) {
-  #if NAME==rtthread
-  Log("This is a marco test");
-  #endif
   init_monitor(argc, argv);
 
   engine_start(argc, argv);

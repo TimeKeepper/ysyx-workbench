0bcc6ddb1d6cc84e4f352ea01fb767cfc071dcfa
tracer-ysyx <tracer@ysyx.org>
2024-10-09 12:37:45 +0800

    > generate verilog ysyx_23060198 林锦晖 Linux WIN-H8ULN2I4VKL 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  12:37:45 up 4 days, 14:09,  7 users,  load average: 0.17, 0.12, 0.08

--- npc/src/ssrc/main/riscv_soc/CPU/IFU/IFU.scala
+++ npc/src/ssrc/main/riscv_soc/CPU/IFU/IFU.scala
@@ -35,14 +35,16 @@ class IFU_TRACE extends BlackBox with HasBlackBoxInline {
 
 class IFU_PC extends BlackBox with HasBlackBoxInline {
     val io = IO(new Bundle{
+        val clock = Input(Clock())
         val valid = Input(Bool())
     })
     setInline("IFU_PC.v",
     """module IFU_PC(
+    |    input clock,
     |    input valid
     |);
     |  import "DPI-C" function void IFU_finished();
-    |  always @* begin
+    |  always @(posedge clock) begin
     |    if(valid) begin
     |      IFU_finished();
     |    end
@@ -86,6 +88,7 @@ class ysyx_23060198_IFU extends Module {
         trace.io.data := io.out.bits.data
 
         val IFU_PC = Module(new IFU_PC)
+        IFU_PC.io.clock := clock
         IFU_PC.io.valid := io.out.valid && io.out.ready && !reset.asBool
     }
 }

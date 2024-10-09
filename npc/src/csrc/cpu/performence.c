7a64a929a9b233896b93ccf0c1e1c51a5787d417
tracer-ysyx <tracer@ysyx.org>
2024-10-09 19:22:48 +0800

    > Commit: 6d9b96e18a96c504c10527002efafe35ff673867 Instruction count: 709613 Clock count: 9920002 IPC: 0.071534 IFU performence counter: 709614.0 LSU performence counter: 175573.0 ALU performence counter: 534040.0 Freq: 643.613 Chip area: 25263.084000 ysyx_23060198 林锦晖 Linux WIN-H8ULN2I4VKL 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:22:48 up 4 days, 20:54,  5 users,  load average: 0.90, 0.33, 0.15

--- npc/src/csrc/cpu/performence.c
+++ npc/src/csrc/cpu/performence.c
@@ -4,16 +4,13 @@
 uint64_t IFU_pc = 0, LSU_pc = 0, ALU_pc = 0;
 
 extern "C" void IFU_finished() {
-    Log("IFU finished");
     IFU_pc++;
 }
 
 extern "C" void LSU_finished() {
-    Log("LSU finished");
     LSU_pc++;
 }
 
 extern "C" void ALU_finished() {
-    Log("ALU finished");
     ALU_pc++;
 }

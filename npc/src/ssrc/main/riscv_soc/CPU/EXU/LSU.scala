01d73eddb1340ef7fecdf0f176896270bef61563
tracer-ysyx <tracer@ysyx.org>
2024-10-09 12:53:17 +0800

    > generate verilog ysyx_23060198 林锦晖 Linux WIN-H8ULN2I4VKL 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  12:53:17 up 4 days, 14:24,  7 users,  load average: 0.13, 0.17, 0.12

--- npc/src/ssrc/main/riscv_soc/CPU/EXU/LSU.scala
+++ npc/src/ssrc/main/riscv_soc/CPU/EXU/LSU.scala
@@ -191,6 +191,6 @@ class ysyx_23060198_LSU extends Module{
 
         val LSU_PC = Module(new LSU_PC)
         LSU_PC.io.clock := clock
-        LSU_PC.io.valid := io.out.valid && io.out.ready
+        LSU_PC.io.valid := io.AXI.rdata.valid && io.AXI.rdata.ready && !reset.asBool
     }
 }

5a777d1b4f3095a8f09972ea0654564436ad2ae5
tracer-ysyx <tracer@ysyx.org>
2024-10-11 04:56:59 +0800

    > generate verilog ysyx_23060198 林锦晖 Linux WIN-H8ULN2I4VKL 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  04:56:59 up 6 days,  6:28,  3 users,  load average: 0.36, 0.45, 0.38

--- npc/src/ssrc/main/riscv_soc/CPU/WBU/WBU.scala
+++ npc/src/ssrc/main/riscv_soc/CPU/WBU/WBU.scala
@@ -24,7 +24,7 @@ class ysyx_23060198_WBU extends Module {
         )
     )
 
-    io.WBU_2_IFU.bits.Next_PC := io.EXU_2_WBU.bits.PC
+    io.WBU_2_IFU.bits.Next_PC := RegEnable(io.EXU_2_WBU.bits.PC, io.EXU_2_WBU.valid && io.EXU_2_WBU.ready)
     io.WBU_2_IFU.valid := state === s_wait_ready && !reset.asBool // 这是由于soc外设的行为不确定而做出的改动
     io.EXU_2_WBU.ready  := state === s_wait_valid
 

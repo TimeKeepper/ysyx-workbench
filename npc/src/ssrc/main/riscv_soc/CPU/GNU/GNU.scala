63b22dc977bb6ed56824abb7d6ac7cbc12a6c3ba
tracer-ysyx <tracer@ysyx.org>
2024-10-10 18:00:57 +0800

    > generate verilog ysyx_23060198 林锦晖 Linux WIN-H8ULN2I4VKL 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:00:57 up 5 days, 19:32,  3 users,  load average: 0.28, 0.25, 0.15

--- npc/src/ssrc/main/riscv_soc/CPU/GNU/GNU.scala
+++ npc/src/ssrc/main/riscv_soc/CPU/GNU/GNU.scala
@@ -57,9 +57,9 @@ class ysyx_23060198_GNU extends Module{
     io.out.bits.GNU_io.ALUctr       <> RegEnable(idu.io.ALUctr,         comunication_succeed) 
     io.out.bits.GNU_io.csr_ctr      <> RegEnable(idu.io.csr_ctr,        comunication_succeed) 
     io.out.bits.GNU_io.Imm          <> RegEnable(igu.io.imm,            comunication_succeed) 
-    io.out.bits.GNU_io.GPR_Adata    <> RegEnable(io.in.IFU_io.bits.GPR_Adata,  comunication_succeed) 
-    io.out.bits.GNU_io.GPR_Bdata    <> RegEnable(io.in.IFU_io.bits.GPR_Bdata,  comunication_succeed) 
-    io.out.bits.GNU_io.GPR_waddr    <> RegEnable(io.in.IFU_io.bits.IFU_io.data(11, 7), comunication_succeed) 
+    io.out.bits.GNU_io.GPR_Adata    <> RegEnable(io.in.GPR_Adata,  comunication_succeed) 
+    io.out.bits.GNU_io.GPR_Bdata    <> RegEnable(io.in.GPR_Bdata,  comunication_succeed) 
+    io.out.bits.GNU_io.GPR_waddr    <> RegEnable(io.in.IFU_io.bits.data(11, 7), comunication_succeed) 
     io.out.bits.GNU_io.PC           <> RegEnable(io.in.IFU_io.bits.PC,         comunication_succeed) 
     io.out.bits.CSR_raddr           <> RegEnable(MuxLookup(
                                                         idu.io.csr_ctr, igu.io.imm(11, 0))(

2c910f88d275c1fc1ab4c22cf616c3a227973767
tracer-ysyx <tracer@ysyx.org>
2024-10-12 07:51:44 +0800

    > generate verilog ysyx_23060198 林锦晖 Linux WIN-H8ULN2I4VKL 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  07:51:44 up 7 days,  9:23,  4 users,  load average: 0.06, 0.06, 0.09

--- npc/src/ssrc/main/riscv_soc/CPU/EXU/EXU.scala
+++ npc/src/ssrc/main/riscv_soc/CPU/EXU/EXU.scala
@@ -44,7 +44,6 @@ class ysyx_23060198_EXU extends Module {
     lsu.io.GNU_2_EXU.bits := io.GNU_2_EXU.bits
     lsu.io.AXI <> io.AXI
 
-
     io.EXU_2_WBU.bits.RegWr        <> RegEnable(io.GNU_2_EXU.bits.RegWr,        comunication_succeed)     
     io.EXU_2_WBU.bits.Branch       <> RegEnable(io.GNU_2_EXU.bits.Branch,       comunication_succeed)     
     io.EXU_2_WBU.bits.MemtoReg     <> RegEnable(io.GNU_2_EXU.bits.MemtoReg,     comunication_succeed) 
@@ -55,7 +54,7 @@ class ysyx_23060198_EXU extends Module {
     io.EXU_2_WBU.bits.PC           <> RegEnable(io.GNU_2_EXU.bits.PC,           comunication_succeed)   
     io.EXU_2_WBU.bits.CSR          <> RegEnable(io.REG_2_EXU.CSR_rdata,         comunication_succeed)  
       
-    io.EXU_2_WBU.bits.Result       <> RegEnable(alu.io.out.bits.Result,         comunication_succeed) 
+    io.EXU_2_WBU.bits.Result       <> alu.io.out.bits.Result
     io.EXU_2_WBU.bits.Zero         <> alu.io.out.bits.Zero
     io.EXU_2_WBU.bits.Less         <> alu.io.out.bits.Less
     io.EXU_2_WBU.bits.Mem_rdata    <> lsu.io.out.bits.Mem_rdata

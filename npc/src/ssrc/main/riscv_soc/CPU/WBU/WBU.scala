9f07c43176e9dcd979077c562b5a6b9aa119a447
tracer-ysyx <tracer@ysyx.org>
2024-10-12 13:47:15 +0800

    > generate verilog ysyx_23060198 林锦晖 Linux WIN-H8ULN2I4VKL 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  13:47:15 up 7 days, 15:18,  2 users,  load average: 0.24, 0.23, 0.11

--- npc/src/ssrc/main/riscv_soc/CPU/WBU/WBU.scala
+++ npc/src/ssrc/main/riscv_soc/CPU/WBU/WBU.scala
@@ -36,17 +36,21 @@ class ysyx_23060198_WBU extends Module {
     val PCAsrc = Wire(UInt(32.W))
     val PCBsrc = Wire(UInt(32.W))
 
-    PCAsrc := MuxLookup(bcu.io.PCAsrc, 0.U)(Seq(
-        PCAsrc_Imm -> io.EXU_2_WBU.bits.Imm,
-        PCAsrc_0  -> 0.U,
-        PCAsrc_4 -> 4.U,
-        PCAsrc_csr -> io.EXU_2_WBU.bits.CSR,
+    PCAsrc := MuxLookup(io.EXU_2_WBU.bits.Branch, 0.U)(Seq(
+        Bran_Jmp -> io.EXU_2_WBU.bits.Imm,
+        Bran_Jmpr -> io.EXU_2_WBU.bits.Imm,
+        Bran_Jeq -> Mux(io.Zero, io.EXU_2_WBU.bits.Imm, 4.U),
+        Bran_Jne -> Mux(io.Zero, 4.U, io.EXU_2_WBU.bits.Imm),
+        Bran_Jlt -> Mux(io.Less, io.EXU_2_WBU.bits.Imm, 4.U),
+        Bran_Jge -> Mux(io.Less, 4.U, io.EXU_2_WBU.bits.Imm),
+        Bran_Jcsr -> io.EXU_2_WBU.bits.CSR,
+        Bran_NoC -> 0.U,
     ))
 
-    PCBsrc := MuxLookup(bcu.io.PCBsrc, 0.U)(Seq(
-        PCBsrc_gpr -> io.EXU_2_WBU.bits.GPR_Adata,
-        PCBsrc_pc  -> io.EXU_2_WBU.bits.PC,
-        PCBsrc_0   -> 0.U,
+    PCBsrc := MuxLookup(io.EXU_2_WBU.bits.Branch, 0.U)(Seq(
+        Bran_Jmpr -> io.EXU_2_WBU.bits.GPR_Adata,
+        Bran_Jcsr -> 0.U,
+        Bran_NoC  -> io.EXU_2_WBU.bits.PC,
     ))
 
     when(io.EXU_2_WBU.valid && io.EXU_2_WBU.ready){

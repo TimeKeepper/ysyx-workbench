0b48d5440ea27008ae4170d081b42bd2029f6120
tracer-ysyx <tracer@ysyx.org>
2024-10-12 10:02:50 +0800

    > generate verilog ysyx_23060198 林锦晖 Linux WIN-H8ULN2I4VKL 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  10:02:50 up 7 days, 11:34,  2 users,  load average: 0.20, 0.19, 0.14

--- npc/src/ssrc/main/riscv_soc/CPU/EXU/ALU.scala
+++ npc/src/ssrc/main/riscv_soc/CPU/EXU/ALU.scala
@@ -151,14 +151,14 @@ class ysyx_23060198_ALU extends Module {
   val src_B      = Wire(UInt(32.W))
 
   src_A := MuxLookup(io.IDU_2_EXU.bits.ALUAsrc, 0.U)(Seq(
-      ALUAsrc_RS1 -> io.REG_2_EXU.bits.GPR_Adata,
+      ALUAsrc_RS1 -> io.REG_2_EXU.GPR_Adata,
       ALUAsrc_PC  -> io.IDU_2_EXU.bits.PC,
       ALUAsrc_CSR -> io.CSR,
   ))
 
   src_B := MuxLookup(io.IDU_2_EXU.bits.ALUBsrc, 0.U)(Seq(
-      ALUBSrc_RS1 -> io.REG_2_EXU.bits.GPR_Adata,
-      ALUBSrc_RS2 -> io.REG_2_EXU.bits.GPR_Bdata,
+      ALUBSrc_RS1 -> io.REG_2_EXU.GPR_Adata,
+      ALUBSrc_RS2 -> io.REG_2_EXU.GPR_Bdata,
       ALUBSrc_IMM -> io.IDU_2_EXU.bits.Imm,
       ALUBSrc_4   -> 4.U,
   ))

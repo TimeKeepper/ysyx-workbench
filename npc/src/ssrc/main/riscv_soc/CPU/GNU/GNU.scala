1ee053431e6c6b812889fc3c7ef5f1efb2478d06
tracer-ysyx <tracer@ysyx.org>
2024-10-12 08:21:06 +0800

    > generate verilog ysyx_23060198 林锦晖 Linux WIN-H8ULN2I4VKL 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  08:21:06 up 7 days,  9:52,  4 users,  load average: 0.28, 0.16, 0.13

--- npc/src/ssrc/main/riscv_soc/CPU/GNU/GNU.scala
+++ npc/src/ssrc/main/riscv_soc/CPU/GNU/GNU.scala
@@ -8,6 +8,63 @@ import bus_state._
 import Instructions._
 // riscv generating number(all meassge ALU and other thing needs) unit
 
+object Decode {
+  import signal_value._
+
+  import Instructions._
+
+  // format: off
+    val default =
+    //   Extop     RegWr  Branch   MemtoReg  MemWr   MemOp       ALUAsrc    ALUBsrc         ALUctr     csr_ctr 
+    //     |        |       |         |       |        |           |          |               |          |    
+    List(Imm_I,     N,   Bran_NJmp,   N,      N,   MemOp_1BS, ALUAsrc_RS1, ALUBSrc_RS2,  ALUctr_ADD,   CSR_N)
+
+    val map = Array(
+        BitPat(LUI)     -> List(Imm_U, Y, Bran_NJmp, N, N, MemOp_1BS, ALUAsrc_RS1, ALUBSrc_IMM, ALUctr_B,      CSR_N    ),
+        BitPat(AUIPC)   -> List(Imm_U, Y, Bran_NJmp, N, N, MemOp_1BS, ALUAsrc_PC,  ALUBSrc_IMM, ALUctr_ADD,    CSR_N    ),
+        BitPat(ADDI)    -> List(Imm_I, Y, Bran_NJmp, N, N, MemOp_1BS, ALUAsrc_RS1, ALUBSrc_IMM, ALUctr_ADD,    CSR_N    ),
+        BitPat(SLTI)    -> List(Imm_I, Y, Bran_NJmp, N, N, MemOp_1BS, ALUAsrc_RS1, ALUBSrc_IMM, ALUctr_Less_S, CSR_N    ),
+        BitPat(SLTIU)   -> List(Imm_I, Y, Bran_NJmp, N, N, MemOp_1BS, ALUAsrc_RS1, ALUBSrc_IMM, ALUctr_Less_U, CSR_N    ),
+        BitPat(XORI)    -> List(Imm_I, Y, Bran_NJmp, N, N, MemOp_1BS, ALUAsrc_RS1, ALUBSrc_IMM, ALUctr_XOR,    CSR_N    ),
+        BitPat(ORI)     -> List(Imm_I, Y, Bran_NJmp, N, N, MemOp_1BS, ALUAsrc_RS1, ALUBSrc_IMM, ALUctr_OR,     CSR_N    ),
+        BitPat(ANDI)    -> List(Imm_I, Y, Bran_NJmp, N, N, MemOp_1BS, ALUAsrc_RS1, ALUBSrc_IMM, ALUctr_AND,    CSR_N    ),
+        BitPat(SLLI)    -> List(Imm_I, Y, Bran_NJmp, N, N, MemOp_1BS, ALUAsrc_RS1, ALUBSrc_IMM, ALUctr_SLL,    CSR_N    ),
+        BitPat(SRLI)    -> List(Imm_I, Y, Bran_NJmp, N, N, MemOp_1BS, ALUAsrc_RS1, ALUBSrc_IMM, ALUctr_SRL,    CSR_N    ),
+        BitPat(SRAI)    -> List(Imm_I, Y, Bran_NJmp, N, N, MemOp_1BS, ALUAsrc_RS1, ALUBSrc_IMM, ALUctr_SRA,    CSR_N    ),
+        BitPat(ADD)     -> List(Imm_I, Y, Bran_NJmp, N, N, MemOp_1BS, ALUAsrc_RS1, ALUBSrc_RS2, ALUctr_ADD,    CSR_N    ),
+        BitPat(SUB)     -> List(Imm_I, Y, Bran_NJmp, N, N, MemOp_1BS, ALUAsrc_RS1, ALUBSrc_RS2, ALUctr_SUB,    CSR_N    ),
+        BitPat(SLL)     -> List(Imm_I, Y, Bran_NJmp, N, N, MemOp_1BS, ALUAsrc_RS1, ALUBSrc_RS2, ALUctr_SLL,    CSR_N    ),
+        BitPat(SLT)     -> List(Imm_I, Y, Bran_NJmp, N, N, MemOp_1BS, ALUAsrc_RS1, ALUBSrc_RS2, ALUctr_Less_S, CSR_N    ),
+        BitPat(SLTU)    -> List(Imm_I, Y, Bran_NJmp, N, N, MemOp_1BS, ALUAsrc_RS1, ALUBSrc_RS2, ALUctr_Less_U, CSR_N    ),
+        BitPat(XOR)     -> List(Imm_I, Y, Bran_NJmp, N, N, MemOp_1BS, ALUAsrc_RS1, ALUBSrc_RS2, ALUctr_XOR,    CSR_N    ),
+        BitPat(SRL)     -> List(Imm_I, Y, Bran_NJmp, N, N, MemOp_1BS, ALUAsrc_RS1, ALUBSrc_RS2, ALUctr_SRL,    CSR_N    ),
+        BitPat(SRA)     -> List(Imm_I, Y, Bran_NJmp, N, N, MemOp_1BS, ALUAsrc_RS1, ALUBSrc_RS2, ALUctr_SRA,    CSR_N    ),
+        BitPat(OR)      -> List(Imm_I, Y, Bran_NJmp, N, N, MemOp_1BS, ALUAsrc_RS1, ALUBSrc_RS2, ALUctr_OR,     CSR_N    ),
+        BitPat(AND)     -> List(Imm_I, Y, Bran_NJmp, N, N, MemOp_1BS, ALUAsrc_RS1, ALUBSrc_RS2, ALUctr_AND,    CSR_N    ),
+        BitPat(JAL)     -> List(Imm_J, Y, Bran_Jmp,  N, N, MemOp_1BS, ALUAsrc_PC,  ALUBSrc_4,   ALUctr_ADD,    CSR_N    ),
+        BitPat(JALR)    -> List(Imm_I, Y, Bran_Jmpr, N, N, MemOp_1BS, ALUAsrc_PC,  ALUBSrc_4,   ALUctr_ADD,    CSR_N    ),
+        BitPat(BEQ)     -> List(Imm_B, N, Bran_Jeq,  N, N, MemOp_1BS, ALUAsrc_RS1, ALUBSrc_RS2, ALUctr_Less_S, CSR_N    ),
+        BitPat(BNE)     -> List(Imm_B, N, Bran_Jne,  N, N, MemOp_1BS, ALUAsrc_RS1, ALUBSrc_RS2, ALUctr_Less_S, CSR_N    ),
+        BitPat(BLT)     -> List(Imm_B, N, Bran_Jlt,  N, N, MemOp_1BS, ALUAsrc_RS1, ALUBSrc_RS2, ALUctr_Less_S, CSR_N    ),
+        BitPat(BGE)     -> List(Imm_B, N, Bran_Jge,  N, N, MemOp_1BS, ALUAsrc_RS1, ALUBSrc_RS2, ALUctr_Less_S, CSR_N    ),
+        BitPat(BLTU)    -> List(Imm_B, N, Bran_Jlt,  N, N, MemOp_1BS, ALUAsrc_RS1, ALUBSrc_RS2, ALUctr_Less_U, CSR_N    ),
+        BitPat(BGEU)    -> List(Imm_B, N, Bran_Jge,  N, N, MemOp_1BS, ALUAsrc_RS1, ALUBSrc_RS2, ALUctr_Less_U, CSR_N    ),
+        BitPat(LB)      -> List(Imm_I, Y, Bran_NJmp, Y, N, MemOp_1BS, ALUAsrc_RS1, ALUBSrc_IMM, ALUAsrc_RS1,   CSR_N    ),
+        BitPat(LH)      -> List(Imm_I, Y, Bran_NJmp, Y, N, MemOp_2BS, ALUAsrc_RS1, ALUBSrc_IMM, ALUAsrc_RS1,   CSR_N    ),
+        BitPat(LW)      -> List(Imm_I, Y, Bran_NJmp, Y, N, MemOp_4BU, ALUAsrc_RS1, ALUBSrc_IMM, ALUAsrc_RS1,   CSR_N    ),
+        BitPat(LBU)     -> List(Imm_I, Y, Bran_NJmp, Y, N, MemOp_1BU, ALUAsrc_RS1, ALUBSrc_IMM, ALUAsrc_RS1,   CSR_N    ),
+        BitPat(LHU)     -> List(Imm_I, Y, Bran_NJmp, Y, N, MemOp_2BU, ALUAsrc_RS1, ALUBSrc_IMM, ALUAsrc_RS1,   CSR_N    ),
+        BitPat(SB)      -> List(Imm_S, N, Bran_NJmp, N, Y, MemOp_1BS, ALUAsrc_RS1, ALUBSrc_IMM, ALUAsrc_RS1,   CSR_N    ),
+        BitPat(SH)      -> List(Imm_S, N, Bran_NJmp, N, Y, MemOp_2BS, ALUAsrc_RS1, ALUBSrc_IMM, ALUAsrc_RS1,   CSR_N    ),
+        BitPat(SW)      -> List(Imm_S, N, Bran_NJmp, N, Y, MemOp_4BU, ALUAsrc_RS1, ALUBSrc_IMM, ALUAsrc_RS1,   CSR_N    ),
+        BitPat(CSRRW)   -> List(Imm_I, Y, Bran_NJmp, N, N, MemOp_1BS, ALUAsrc_CSR, ALUBSrc_RS1, ALUctr_B,      CSR_R1W1 ),
+        BitPat(CSRRS)   -> List(Imm_I, Y, Bran_NJmp, N, N, MemOp_1BS, ALUAsrc_CSR, ALUBSrc_RS1, ALUctr_OR,     CSR_R1W1 ),
+        BitPat(ECALL)   -> List(Imm_I, N, Bran_Jcsr, N, N, MemOp_1BS, ALUAsrc_CSR, ALUBSrc_RS1, ALUctr_ADD,    CSR_R1W2 ),
+        BitPat(MRET)    -> List(Imm_I, N, Bran_Jcsr, N, N, MemOp_1BS, ALUAsrc_CSR, ALUBSrc_RS1, ALUctr_ADD,    CSR_R1W0 )
+    )
+    // format: on
+}
+
 class ysyx_23060198_GNU extends Module{
     val io = IO(new Bundle{
         val IFU_2_GNU     = Flipped(Decoupled(Input(new BUS_IFU_2_GNU)))
@@ -26,30 +83,38 @@ class ysyx_23060198_GNU extends Module{
         )
     )
 
-    val idu = Module(new ysyx_23060198_IDU)
-
     io.GNU_2_EXU.valid := state === s_wait_ready
     io.IFU_2_GNU.ready := state === s_wait_valid
     val comunication_succeed = (io.IFU_2_GNU.valid && io.IFU_2_GNU.ready)
 
-    idu.io.inst     <> io.IFU_2_GNU.bits.data
+    val ctrlSignals = ListLookup(io.IFU_2_GNU.bits.data, Decode.default, Decode.map)
 
-    io.GNU_2_EXU.bits.RegWr        <> RegEnable(idu.io.RegWr,          comunication_succeed) 
-    io.GNU_2_EXU.bits.Branch       <> RegEnable(idu.io.Branch,         comunication_succeed) 
-    io.GNU_2_EXU.bits.MemtoReg     <> RegEnable(idu.io.MemtoReg,       comunication_succeed) 
-    io.GNU_2_EXU.bits.MemWr        <> RegEnable(idu.io.MemWr,          comunication_succeed) 
-    io.GNU_2_EXU.bits.MemOp        <> RegEnable(idu.io.MemOp,          comunication_succeed) 
-    io.GNU_2_EXU.bits.ALUAsrc      <> RegEnable(idu.io.ALUAsrc,        comunication_succeed) 
-    io.GNU_2_EXU.bits.ALUBsrc      <> RegEnable(idu.io.ALUBsrc,        comunication_succeed) 
-    io.GNU_2_EXU.bits.ALUctr       <> RegEnable(idu.io.ALUctr,         comunication_succeed) 
-    io.GNU_2_EXU.bits.csr_ctr      <> RegEnable(idu.io.csr_ctr,        comunication_succeed) 
-    io.GNU_2_EXU.bits.Imm          <> RegEnable(idu.io.imm,            comunication_succeed) 
+    val imm = MuxLookup(ctrlSignals(0), 0.U)(
+        Seq(
+        Imm_I -> Cat(Fill(21, io.inst(31)), io.inst(31, 20)),
+        Imm_U -> Cat(io.inst(31, 12), Fill(12, 0.U)),
+        Imm_S -> Cat(Fill(20, io.inst(31)), io.inst(31, 25), io.inst(11, 7)),
+        Imm_B -> Cat(Fill(20, io.inst(31)), io.inst(7), io.inst(30, 25), io.inst(11, 8), 0.U(1.W)),
+        Imm_J -> Cat(Fill(12, io.inst(31)), io.inst(19, 12), io.inst(20), io.inst(30, 21), 0.U(1.W)),
+        )
+    )
+
+    io.GNU_2_EXU.bits.RegWr        <> RegEnable(ctrlSignals(1),         comunication_succeed) 
+    io.GNU_2_EXU.bits.Branch       <> RegEnable(ctrlSignals(2),         comunication_succeed) 
+    io.GNU_2_EXU.bits.MemtoReg     <> RegEnable(ctrlSignals(3),         comunication_succeed) 
+    io.GNU_2_EXU.bits.MemWr        <> RegEnable(ctrlSignals(4),         comunication_succeed) 
+    io.GNU_2_EXU.bits.MemOp        <> RegEnable(ctrlSignals(5),         comunication_succeed) 
+    io.GNU_2_EXU.bits.ALUAsrc      <> RegEnable(ctrlSignals(6),         comunication_succeed) 
+    io.GNU_2_EXU.bits.ALUBsrc      <> RegEnable(ctrlSignals(7),         comunication_succeed) 
+    io.GNU_2_EXU.bits.ALUctr       <> RegEnable(ctrlSignals(8),         comunication_succeed) 
+    io.GNU_2_EXU.bits.csr_ctr      <> RegEnable(ctrlSignals(9),         comunication_succeed) 
+    io.GNU_2_EXU.bits.Imm          <> RegEnable(imm,                    comunication_succeed) 
     io.GNU_2_EXU.bits.GPR_Adata    <> RegEnable(io.REG_2_GNU.GPR_Adata,  comunication_succeed) 
     io.GNU_2_EXU.bits.GPR_Bdata    <> RegEnable(io.REG_2_GNU.GPR_Bdata,  comunication_succeed) 
     io.GNU_2_EXU.bits.GPR_waddr    <> RegEnable(io.IFU_2_GNU.bits.data(11, 7), comunication_succeed) 
     io.GNU_2_EXU.bits.PC           <> RegEnable(io.REG_2_GNU.PC,         comunication_succeed) 
     io.GNU_2_REG.CSR_raddr         <> RegEnable(MuxLookup(
-                                                        idu.io.csr_ctr, idu.io.imm(11, 0))(
+                                                    io.GNU_2_EXU.bits.csr_ctr, idu.io.imm(11, 0))(
                                                             Seq(
                                                                 CSR_R1W0 -> "h341".U,
                                                                 CSR_R1W2 -> "h305".U,

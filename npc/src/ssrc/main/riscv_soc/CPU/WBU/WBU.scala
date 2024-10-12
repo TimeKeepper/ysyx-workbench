457a92729c48a0cc14ebeb61cd39cbdeb386f9f4
wenjiu <2784307979@qq.com>
2024-10-12 14:40:16 +0800

    remove redounct save register in WBU

--- npc/src/ssrc/main/riscv_soc/CPU/WBU/WBU.scala
+++ npc/src/ssrc/main/riscv_soc/CPU/WBU/WBU.scala
@@ -6,8 +6,6 @@ import chisel3.util._
 import signal_value._
 import bus_state._
 
-// riscv writeback unit
-
 class ysyx_23060198_WBU extends Module {
     val io = IO(new Bundle{
         val EXU_2_WBU = Flipped(Decoupled(Input(new BUS_EXU_2_WBU)))
@@ -27,52 +25,5 @@ class ysyx_23060198_WBU extends Module {
     io.WBU_2_IFU.valid := state === s_wait_ready && !reset.asBool // 这是由于soc外设的行为不确定而做出的改动
     io.EXU_2_WBU.ready  := state === s_wait_valid
     
-    val PCAsrc = Wire(UInt(32.W))
-    val PCBsrc = Wire(UInt(32.W))
-
-    PCAsrc := MuxLookup(io.EXU_2_WBU.bits.Branch, 4.U)(Seq(
-        Bran_Jmp -> io.EXU_2_WBU.bits.Imm,
-        Bran_Jmpr -> io.EXU_2_WBU.bits.Imm,
-        Bran_Jeq -> Mux(io.EXU_2_WBU.bits.Zero, io.EXU_2_WBU.bits.Imm, 4.U),
-        Bran_Jne -> Mux(io.EXU_2_WBU.bits.Zero, 4.U, io.EXU_2_WBU.bits.Imm),
-        Bran_Jlt -> Mux(io.EXU_2_WBU.bits.Less, io.EXU_2_WBU.bits.Imm, 4.U),
-        Bran_Jge -> Mux(io.EXU_2_WBU.bits.Less, 4.U, io.EXU_2_WBU.bits.Imm),
-        Bran_Jcsr -> io.EXU_2_WBU.bits.CSR,
-        Bran_NoC -> 0.U,
-    ))
-
-    PCBsrc := MuxLookup(io.EXU_2_WBU.bits.Branch, io.EXU_2_WBU.bits.PC)(Seq(
-        Bran_Jmpr -> io.EXU_2_WBU.bits.GPR_Adata,
-        Bran_Jcsr -> 0.U,
-        Bran_NoC  -> io.EXU_2_WBU.bits.PC,
-    ))
-
-    when(io.EXU_2_WBU.valid && io.EXU_2_WBU.ready){
-        io.WBU_2_REG.inst_valid := true.B
-    }.otherwise{
-        io.WBU_2_REG.inst_valid := false.B
-    }
-
-    io.WBU_2_REG.Next_Pc := PCAsrc + PCBsrc
-
-    io.WBU_2_REG.GPR_waddr := io.EXU_2_WBU.bits.GPR_waddr
-    io.WBU_2_REG.GPR_wdata := MuxLookup(io.EXU_2_WBU.bits.MemtoReg, io.EXU_2_WBU.bits.Result)(Seq(
-        Y  -> io.EXU_2_WBU.bits.Mem_rdata,
-        N  -> Mux(io.EXU_2_WBU.bits.csr_ctr === CSR_N, io.EXU_2_WBU.bits.Result, io.EXU_2_WBU.bits.CSR),
-    ))
-    io.WBU_2_REG.GPR_wen <> io.EXU_2_WBU.bits.RegWr
-
-    io.WBU_2_REG.CSR_ctr <> io.EXU_2_WBU.bits.csr_ctr
-
-    io.WBU_2_REG.CSR_waddra := MuxLookup(io.EXU_2_WBU.bits.csr_ctr, io.EXU_2_WBU.bits.Imm(11, 0))(Seq(
-        CSR_R1W2 -> "h341".U
-    ))
-
-    io.WBU_2_REG.CSR_waddrb := "h342".U
-
-    io.WBU_2_REG.CSR_wdataa := MuxLookup(io.EXU_2_WBU.bits.csr_ctr, io.EXU_2_WBU.bits.Result)(Seq(
-        CSR_R1W2 -> io.EXU_2_WBU.bits.PC,
-    ))
-
-    io.WBU_2_REG.CSR_wdatab := 11.U
+    io.EXU_2_WBU.bits <> io.WBU_2_REG
 }

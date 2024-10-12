1f4f2f1a85a870b10713d287de5a1933198165a1
tracer-ysyx <tracer@ysyx.org>
2024-10-12 13:48:08 +0800

    > generate verilog ysyx_23060198 林锦晖 Linux WIN-H8ULN2I4VKL 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  13:48:08 up 7 days, 15:19,  2 users,  load average: 0.27, 0.25, 0.13

--- npc/src/ssrc/main/riscv_soc/CPU/WBU/WBU.scala
+++ npc/src/ssrc/main/riscv_soc/CPU/WBU/WBU.scala
@@ -26,6 +26,9 @@ class ysyx_23060198_WBU extends Module {
 
     io.WBU_2_IFU.valid := state === s_wait_ready && !reset.asBool // 这是由于soc外设的行为不确定而做出的改动
     io.EXU_2_WBU.ready  := state === s_wait_valid
+    
+    val PCAsrc = Wire(UInt(32.W))
+    val PCBsrc = Wire(UInt(32.W))
 
     PCAsrc := MuxLookup(io.EXU_2_WBU.bits.Branch, 0.U)(Seq(
         Bran_Jmp -> io.EXU_2_WBU.bits.Imm,

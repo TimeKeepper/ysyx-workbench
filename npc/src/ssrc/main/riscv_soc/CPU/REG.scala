ef490371fa2f0bd5c8f3579e1ff8f7976d6f83eb
tracer-ysyx <tracer@ysyx.org>
2024-10-13 18:02:40 +0800

    > generate verilog ysyx_23060198 林锦晖 Linux WIN-H8ULN2I4VKL 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:02:40 up 8 days, 19:34,  3 users,  load average: 0.39, 0.37, 0.20

--- npc/src/ssrc/main/riscv_soc/CPU/REG.scala
+++ npc/src/ssrc/main/riscv_soc/CPU/REG.scala
@@ -69,23 +69,14 @@ class ysyx_23060198_REG extends Module {
   val csrb_wen = io.WBU_2_REG.CSR_ctr === CSR_R1W2 && io.WBU_2_REG.inst_valid === true.B
   val gpr_wen = io.WBU_2_REG.GPR_wen && io.WBU_2_REG.GPR_waddr =/= 0.U && io.WBU_2_REG.inst_valid === true.B
 
-  val gpr = RegInit(VecInit(Seq.fill(15)(0.U(32.W))))
+  val gpr = RegInit(VecInit(Seq.fill(16)(0.U(32.W))))
 
   when(gpr_wen) {
-    gpr(io.WBU_2_REG.GPR_waddr - 1.U) := io.WBU_2_REG.GPR_wdata
+    gpr(io.WBU_2_REG.GPR_waddr) := io.WBU_2_REG.GPR_wdata
   }
 
-  when(io.IFU_2_REG.GPR_Aaddr === 0.U){
-    io.REG_2_IDU.GPR_Adata := 0.U
-  }.otherwise{
-    io.REG_2_IDU.GPR_Adata := gpr(io.IFU_2_REG.GPR_Aaddr - 1.U)
-  }
-
-  when(io.IFU_2_REG.GPR_Baddr === 0.U){
-    io.REG_2_IDU.GPR_Bdata := 0.U
-  }.otherwise{
-    io.REG_2_IDU.GPR_Bdata := gpr(io.IFU_2_REG.GPR_Baddr - 1.U)
-  }
+  io.REG_2_IDU.GPR_Adata := gpr(io.IFU_2_REG.GPR_Aaddr)
+  io.REG_2_IDU.GPR_Bdata := gpr(io.IFU_2_REG.GPR_Baddr)
 
   val pc = RegEnable(io.WBU_2_REG.Next_Pc, main_val.Reset_Vector, io.WBU_2_REG.inst_valid)
 

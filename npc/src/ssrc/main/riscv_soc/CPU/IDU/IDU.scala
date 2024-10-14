a54d3bc15c0fb2f82285cfd21ba4078882aeabef
tracer-ysyx <tracer@ysyx.org>
2024-10-14 15:39:37 +0800

    > generate verilog ysyx_23060198 林锦晖 Linux WIN-H8ULN2I4VKL 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:39:37 up 9 days, 17:11,  3 users,  load average: 0.33, 0.21, 0.13

--- npc/src/ssrc/main/riscv_soc/CPU/IDU/IDU.scala
+++ npc/src/ssrc/main/riscv_soc/CPU/IDU/IDU.scala
@@ -99,6 +99,13 @@ class ysyx_23060198_IDU extends Module{
         )
     )
 
+    val csr_raddr = MuxLookup(ctrlSignals(9), imm(11, 0))(
+        Seq(
+            CSR_R1W0 -> "h341".U,
+            CSR_R1W2 -> "h305".U,
+        )
+    )
+
     io.IDU_2_EXU.bits.RegWr        <> RegEnable(ctrlSignals(1),         comunication_succeed) 
     io.IDU_2_EXU.bits.Branch       <> RegEnable(ctrlSignals(2),         comunication_succeed) 
     io.IDU_2_EXU.bits.MemtoReg     <> RegEnable(ctrlSignals(3),         comunication_succeed) 
@@ -113,12 +120,5 @@ class ysyx_23060198_IDU extends Module{
     io.IDU_2_EXU.bits.GPR_Bdata    <> RegEnable(io.REG_2_IDU.GPR_Bdata,  comunication_succeed) 
     io.IDU_2_EXU.bits.GPR_waddr    <> RegEnable(io.IFU_2_IDU.bits.data(11, 7), comunication_succeed) 
     io.IDU_2_EXU.bits.PC           <> RegEnable(io.REG_2_IDU.PC,         comunication_succeed) 
-    io.IDU_2_REG.CSR_raddr         <> RegEnable(MuxLookup(
-                                                    ctrlSignals(9), imm(11, 0))(
-                                                            Seq(
-                                                                CSR_R1W0 -> "h341".U,
-                                                                CSR_R1W2 -> "h305".U,
-                                                            )
-                                                    ), comunication_succeed
-                                        )
+    io.IDU_2_REG.CSR_raddr         <> RegEnable(csr_raddr, comunication_succeed)
 }

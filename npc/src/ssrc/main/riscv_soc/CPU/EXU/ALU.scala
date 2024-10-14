b704c65dc85e097620d90a0f63b19bb59b30d0cb
tracer-ysyx <tracer@ysyx.org>
2024-10-15 04:06:09 +0800

    > generate verilog ysyx_23060198 林锦晖 Linux WIN-H8ULN2I4VKL 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  04:06:09 up 10 days,  5:37,  3 users,  load average: 0.21, 0.27, 0.17

--- npc/src/ssrc/main/riscv_soc/CPU/EXU/ALU.scala
+++ npc/src/ssrc/main/riscv_soc/CPU/EXU/ALU.scala
@@ -30,31 +30,6 @@ class ALU_PC extends BlackBox with HasBlackBoxInline {
     """.stripMargin)
 }
 
-class ysyx_23060198_ALU_Adder extends Module {
-  val io = IO(new Bundle {
-    val A   = Input(UInt(32.W))
-    val B   = Input(UInt(32.W))
-    val Cin = Input(Bool())
-
-    val Carry    = Output(Bool())
-    val Zero     = Output(Bool())
-    val Overflow = Output(Bool())
-    val Result   = Output(UInt(32.W))
-  })
-
-  val R_B = Wire(UInt(32.W))
-  R_B := io.B +% io.Cin
-
-  val add_result = Wire(UInt(33.W))
-  add_result := io.A +& io.B +& io.Cin
-
-  io.Carry  := add_result(32)
-  io.Result := add_result(31, 0)
-  io.Zero   := io.Result === 0.U
-
-  io.Overflow := (io.A(31) & R_B(31) & !io.Result(31)) | (!io.A(31) & !R_B(31) & io.Result(31))
-}
-
 class ysyx_23060198_ALU_BarrelShifter extends Module {
   val io = IO(new Bundle {
     val Din   = Input(UInt(32.W))
@@ -155,11 +130,6 @@ class ysyx_23060198_ALU extends Module {
 
   Sub_Add_ex := Sub_Add.asSInt
 
-  val alu_adder = Module(new ysyx_23060198_ALU_Adder)
-  alu_adder.io.A   := src_A
-  alu_adder.io.B   := src_B ^ Sub_Add_ex.asUInt
-  alu_adder.io.Cin := Sub_Add
-
   val add_result = Wire(UInt(33.W))
   add_result := src_A +& (src_B ^ Sub_Add_ex.asUInt) +& Sub_Add
 
@@ -182,6 +152,21 @@ class ysyx_23060198_ALU extends Module {
   alu_barrel_shifter.io.L_R   := L_R
   alu_barrel_shifter.io.A_L   := A_L
 
+  when(L_R) {
+    when(A_L) {
+      shifter_result := (src_A.asSInt << src_B(4, 0))(31, 0)
+    }.otherwise {
+      shifter_result := (src_A << src_B(4, 0))(31, 0)
+    }
+  }.otherwise {
+    when(A_L) {
+      shifter_result := (src_A.asSInt >> src_B(4, 0))(31, 0)
+    }.otherwise {
+      shifter_result := (src_A >> src_B(4, 0))(31, 0)
+    }
+  }
+  val shifter_result = Wire(UInt(32.W))
+
   // other ALU outputs
   val Less = Wire(Bool())
   when(U_S) {
@@ -201,9 +186,9 @@ class ysyx_23060198_ALU extends Module {
       ALUctr_Less_S -> Cat(0.U(31.W), Less),
       ALUctr_A -> src_A,
       ALUctr_B -> src_B,
-      ALUctr_SLL -> alu_barrel_shifter.io.Dout,
-      ALUctr_SRL -> alu_barrel_shifter.io.Dout,
-      ALUctr_SRA -> alu_barrel_shifter.io.Dout,
+      ALUctr_SLL -> shifter_result,
+      ALUctr_SRL -> shifter_result,
+      ALUctr_SRA -> shifter_result,
       ALUctr_XOR -> (src_A ^ src_B),
       ALUctr_OR -> (src_A | src_B),
       ALUctr_AND -> (src_A & src_B)

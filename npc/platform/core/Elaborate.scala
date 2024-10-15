49ab5a0f9def8a0433c95d3f7227238aeace9567
tracer-ysyx <tracer@ysyx.org>
2024-10-16 00:11:12 +0800

    > generate verilog ysyx_23060198 林锦晖 Linux WIN-H8ULN2I4VKL 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  00:11:12 up 11 days,  1:42,  4 users,  load average: 0.82, 0.34, 0.17

--- npc/platform/core/Elaborate.scala
+++ npc/platform/core/Elaborate.scala
@@ -8,5 +8,5 @@ object Elaborate extends App {
       "locationInfoStyle=wrapInAtSquareBracket"
     ).reduce(_ + "," + _)
   )
-  circt.stage.ChiselStage.emitSystemVerilogFile(new riscv_cpu.ysyx_23060198_IDU(), args, firtoolOptions)
+  circt.stage.ChiselStage.emitSystemVerilogFile(new riscv_cpu.ysyx_23060198(), args, firtoolOptions)
 }

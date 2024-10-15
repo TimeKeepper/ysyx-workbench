92e3f8e9f3606f558b8e0811f5b0a2390690a142
tracer-ysyx <tracer@ysyx.org>
2024-10-16 00:29:48 +0800

    > generate verilog ysyx_23060198 林锦晖 Linux WIN-H8ULN2I4VKL 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  00:29:48 up 11 days,  2:01,  4 users,  load average: 0.34, 0.23, 0.14

--- npc/src/ssrc/main/riscv_soc/CPU/WBU/WBU.scala
+++ npc/src/ssrc/main/riscv_soc/CPU/WBU/WBU.scala
@@ -48,7 +48,7 @@ class ysyx_23060198_WBU extends Module {
 
     val Next_Pc = MuxLookup(io.EXU_2_WBU.bits.Branch, io.EXU_2_WBU.bits.PC + 4.U)(Seq(
         Bran_Jmp -> (io.EXU_2_WBU.bits.PC + io.EXU_2_WBU.bits.Imm),
-        Bran_Jmpr -> io.EXU_2_WBU.bits.GPR_Adata + io.EXU_2_WBU.bits.Imm,
+        Bran_Jmpr -> (io.EXU_2_WBU.bits.GPR_Adata + io.EXU_2_WBU.bits.Imm),
         Bran_Jeq -> Mux(io.EXU_2_WBU.bits.Zero, io.EXU_2_WBU.bits.PC + io.EXU_2_WBU.bits.Imm, io.EXU_2_WBU.bits.PC + 4.U),
         Bran_Jne -> Mux(io.EXU_2_WBU.bits.Zero, io.EXU_2_WBU.bits.PC + 4.U, io.EXU_2_WBU.bits.PC + io.EXU_2_WBU.bits.Imm),
         Bran_Jlt -> Mux(io.EXU_2_WBU.bits.Less, io.EXU_2_WBU.bits.PC + io.EXU_2_WBU.bits.Imm, io.EXU_2_WBU.bits.PC + 4.U),

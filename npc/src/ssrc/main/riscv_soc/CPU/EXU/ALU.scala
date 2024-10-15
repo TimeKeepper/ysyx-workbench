7468862abd8895311f9431f9e7040ca29cafbf53
tracer-ysyx <tracer@ysyx.org>
2024-10-16 00:52:19 +0800

    > generate verilog ysyx_23060198 林锦晖 Linux WIN-H8ULN2I4VKL 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  00:52:19 up 11 days,  2:23,  4 users,  load average: 0.68, 0.25, 0.17

--- npc/src/ssrc/main/riscv_soc/CPU/EXU/ALU.scala
+++ npc/src/ssrc/main/riscv_soc/CPU/EXU/ALU.scala
@@ -125,7 +125,7 @@ class ysyx_23060198_ALU extends Module {
 
   when(io.IDU_2_EXU.bits.ALUctr === ALUctr_SLL) {
     shifter_result := (src_A << src_B(4, 0))(31, 0)
-  }.elsewhen(io.IDU_2_EXU.bits.ALUctr === ALUctr_Less_U || io.IDU_2_EXU.bits.ALUctr === ALUctr_SRL){
+  }.elsewhen(io.IDU_2_EXU.bits.ALUctr === ALUctr_SRL){
     shifter_result := (src_A >> src_B(4, 0))(31, 0)
   }.otherwise {
     shifter_result := (src_A.asSInt >> src_B(4, 0))(31, 0)

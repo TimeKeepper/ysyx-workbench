4d094bd95fdabe2610c59b5ce2e2a5f2a87b636d
tracer-ysyx <tracer@ysyx.org>
2024-10-17 10:25:47 +0800

    > generate verilog ysyx_23060198 林锦晖 Linux WIN-H8ULN2I4VKL 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  10:25:47 up 12 days, 11:57,  5 users,  load average: 0.05, 0.22, 0.62

--- npc/src/ssrc/main/riscv_soc/CPU/EXU/LSU.scala
+++ npc/src/ssrc/main/riscv_soc/CPU/EXU/LSU.scala
@@ -183,7 +183,7 @@ class ysyx_23060198_LSU extends Module{
         MemOp_1BU -> (AXI_rdata(7,0).asUInt),
         MemOp_1BS -> (AXI_rdata(7,0).asSInt).asUInt,
         MemOp_2BU -> (AXI_rdata(15,0).asUInt),
-        MemOp_2BS -> (AXI_rdata(15,0).asSInt),
+        MemOp_2BS -> (AXI_rdata(15,0).asSInt).asUInt,
         MemOp_4BU -> (AXI_rdata(31,0).asUInt),
     ))
 

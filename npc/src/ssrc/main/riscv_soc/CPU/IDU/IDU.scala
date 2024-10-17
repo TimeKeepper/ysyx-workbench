d0dcc89359e3927b0b4695663329776361c39fd4
tracer-ysyx <tracer@ysyx.org>
2024-10-17 12:43:19 +0800

    > generate verilog ysyx_23060198 林锦晖 Linux WIN-H8ULN2I4VKL 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  12:43:19 up 12 days, 14:14,  5 users,  load average: 0.10, 0.22, 0.18

--- npc/src/ssrc/main/riscv_soc/CPU/IDU/IDU.scala
+++ npc/src/ssrc/main/riscv_soc/CPU/IDU/IDU.scala
@@ -70,7 +70,7 @@ object Decode {
 case class InstructionPattern(
     val func7: BitPat = BitPat.dontCare(7),
     val func3: BitPat = BitPat.dontCare(3),
-    val opcode: BitPat
+    val opcode: BitPat = BitPat.dontCare(7),
 ) extends DecodePattern {
     def bitPat: BitPat = func7 ## BitPat.dontCare(10) ## func3 ## BitPat.dontCare(5) ## opcode
 }

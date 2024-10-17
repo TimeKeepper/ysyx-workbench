15c142cd6c01b6d216d47c366d215922b6203d46
tracer-ysyx <tracer@ysyx.org>
2024-10-17 14:32:52 +0800

    > generate verilog ysyx_23060198 林锦晖 Linux WIN-H8ULN2I4VKL 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:32:52 up 12 days, 16:04,  3 users,  load average: 1.56, 0.54, 0.25

--- npc/src/ssrc/main/riscv_soc/CPU/IDU/IDU.scala
+++ npc/src/ssrc/main/riscv_soc/CPU/IDU/IDU.scala
@@ -201,7 +201,7 @@ class ysyx_23060198_IDU extends Module{
     io.IDU_2_REG.CSR_raddr         <> csr_raddr
 
     io.IDU_2_EXU.bits.RegWr        <> RegEnable(decodeResult(RegWrFiled),         comunication_succeed) 
-    io.IDU_2_EXU.bits.Branch       <> RegEnable(ctrlSignals(0),         comunication_succeed) 
+    io.IDU_2_EXU.bits.Branch       <> RegEnable(decodeResult(BranchField),         comunication_succeed) 
     io.IDU_2_EXU.bits.MemtoReg     <> RegEnable(ctrlSignals(1),         comunication_succeed) 
     io.IDU_2_EXU.bits.MemWr        <> RegEnable(ctrlSignals(2),         comunication_succeed) 
     io.IDU_2_EXU.bits.MemOp        <> RegEnable(ctrlSignals(3),         comunication_succeed) 

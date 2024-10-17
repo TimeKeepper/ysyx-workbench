d75a9591480ee54d84d20d310ab6be813100663a
tracer-ysyx <tracer@ysyx.org>
2024-10-17 14:51:41 +0800

    > generate verilog ysyx_23060198 林锦晖 Linux WIN-H8ULN2I4VKL 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:51:41 up 12 days, 16:23,  4 users,  load average: 0.38, 0.23, 0.30

--- npc/src/ssrc/main/riscv_soc/CPU/IDU/IDU.scala
+++ npc/src/ssrc/main/riscv_soc/CPU/IDU/IDU.scala
@@ -116,17 +116,22 @@ object RegWrFiled extends BoolDecodeField[InstructionPattern] {
 object BranchField extends DecodeField[InstructionPattern, UInt] {
     def name: String = "Branch"
     def chiselType = Bran_Type
+
+    def Branch_sub: BitPat = {
+        op.func3.rawString match {
+            case "000" => BitPat(Bran_Jeq)
+            case "001" => BitPat(Bran_Jne)
+            case "100" => BitPat(Bran_Jlt)
+            case "101" => BitPat(Bran_Jge)
+            case "110" => BitPat(Bran_Jlt)
+            case "111" => BitPat(Bran_Jge)
+            case _ => BitPat(Bran_NJmp)
+        }
+    }
+    
     def genTable(op: InstructionPattern): BitPat = {
         op.opcode.rawString match {
-            case "1100011" => op.func3.rawString match {
-                case "000" => BitPat(Bran_Jeq)
-                case "001" => BitPat(Bran_Jne)
-                case "100" => BitPat(Bran_Jlt)
-                case "101" => BitPat(Bran_Jge)
-                case "110" => BitPat(Bran_Jlt)
-                case "111" => BitPat(Bran_Jge)
-                case _ => BitPat(Bran_NJmp)
-            }
+            case "1100011" => Branch_sub
             case "1101111" => BitPat(Bran_Jmp)
             case "1100111" => BitPat(Bran_Jmpr)
             case "1110011" => if (op.func3 == BitPat("b000")) BitPat(Bran_Jcsr) else BitPat(Bran_NJmp)

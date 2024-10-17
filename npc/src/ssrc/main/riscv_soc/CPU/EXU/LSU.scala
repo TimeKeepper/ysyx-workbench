6a840bb8f077ff63f96bb5e404ca6575e27c6487
tracer-ysyx <tracer@ysyx.org>
2024-10-17 10:32:06 +0800

    > generate verilog ysyx_23060198 林锦晖 Linux WIN-H8ULN2I4VKL 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  10:32:06 up 12 days, 12:03,  5 users,  load average: 0.34, 0.27, 0.50

--- npc/src/ssrc/main/riscv_soc/CPU/EXU/LSU.scala
+++ npc/src/ssrc/main/riscv_soc/CPU/EXU/LSU.scala
@@ -182,11 +182,11 @@ class ysyx_23060198_LSU extends Module{
     val mem_rd = Wire(Bits(32.W))
 
     mem_rd := MuxLookup(io.IDU_2_EXU.bits.MemOp, 0.U)(Seq(
-        MemOp_1BU -> (AXI_rdata(7,0).asUInt),
-        MemOp_1BS -> (AXI_rdata(7,0).asSInt),
-        MemOp_2BU -> (AXI_rdata(15,0).asUInt),
-        MemOp_2BS -> (AXI_rdata(15,0).asSInt),
-        MemOp_4BU -> (AXI_rdata(31,0).asUInt),
+        MemOp_1BU -> (AXI_rdata(7,0).asUInt(32.W)),
+        MemOp_1BS -> (AXI_rdata(7,0).asSInt(32.W)).asUInt,
+        MemOp_2BU -> (AXI_rdata(15,0).asUInt(32.W)),
+        MemOp_2BS -> (AXI_rdata(15,0).asSInt(32.W)).asUInt,
+        MemOp_4BU -> (AXI_rdata(31,0).asUInt(32.W)),
     ))
 
     io.out.bits.Mem_rdata := mem_rd

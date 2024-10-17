fdff495bb6419b81b59fca34fb369618a57c87c7
tracer-ysyx <tracer@ysyx.org>
2024-10-17 13:42:59 +0800

    > generate verilog ysyx_23060198 林锦晖 Linux WIN-H8ULN2I4VKL 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  13:42:59 up 12 days, 15:14,  2 users,  load average: 0.03, 0.21, 0.21

--- npc/src/ssrc/main/riscv_soc/CPU/IDU/IDU.scala
+++ npc/src/ssrc/main/riscv_soc/CPU/IDU/IDU.scala
@@ -107,7 +107,7 @@ object RegWrFiled extends BoolDecodeField[InstructionPattern] {
             case "1100011" => BitPat(N)
             case "1101111" => BitPat(Y)
             case "1100111" => BitPat(Y)
-            case "1110011" => if (op.func3 == BitPat("b001") || op.func3 == BitPat("b010")) BitPat(Y) else BitPat(N)
+            case "1110011" => if (op.func3 != BitPat("b000")) BitPat(Y) else BitPat(N)
             case _ => BitPat.dontCare(1)
         }
     }

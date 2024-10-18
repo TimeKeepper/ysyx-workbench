1e4dec8acaff0878f528f6cea8ce4914771d770a
tracer-ysyx <tracer@ysyx.org>
2024-10-18 14:41:54 +0800

    > generate verilog ysyx_23060198 林锦晖 Linux WIN-H8ULN2I4VKL 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:41:54 up 13 days, 16:13,  4 users,  load average: 0.11, 0.23, 0.20

--- npc/src/ssrc/main/riscv_soc/CPU/IDU/decodedb.scala
+++ npc/src/ssrc/main/riscv_soc/CPU/IDU/decodedb.scala
@@ -165,9 +165,9 @@ object ALUctrField extends DecodeField[InstructionPattern, UInt] {
             case (_, "111", "0010011") => BitPat(ALUctr_AND) // ANDI
             case (_, "010", "0010011") => BitPat(ALUctr_Less_S) // SLTI
             case (_, "011", "0010011") => BitPat(ALUctr_Less_U) // SLTUI
-            case (_, "001", "0010011") => BitPat(ALUctr_SLL) // SLLI
-            case (_, "101", "0010011") => BitPat(ALUctr_SRL) // SRLI
-            case (_, "101", "0010011") => BitPat(ALUctr_SRA) // SRAI // logical I
+            case ("0000000", "001", "0010011") => BitPat(ALUctr_SLL) // SLLI
+            case ("0000000", "101", "0010011") => BitPat(ALUctr_SRL) // SRLI
+            case ("0100000", "101", "0010011") => BitPat(ALUctr_SRA) // SRAI // logical I
 
             case (_, "000", "1100011") => BitPat(ALUctr_SUB)    // BEQ 
             case (_, "001", "1100011") => BitPat(ALUctr_SUB)    // BNE 

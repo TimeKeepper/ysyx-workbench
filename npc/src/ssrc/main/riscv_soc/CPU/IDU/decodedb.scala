58914d9524303e892117d5fd08f077101c5ba331
tracer-ysyx <tracer@ysyx.org>
2024-10-18 21:27:39 +0800

    > generate verilog ysyx_23060198 林锦晖 Linux WIN-H8ULN2I4VKL 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:27:39 up 13 days, 22:59,  4 users,  load average: 0.10, 0.18, 0.16

--- npc/src/ssrc/main/riscv_soc/CPU/IDU/decodedb.scala
+++ npc/src/ssrc/main/riscv_soc/CPU/IDU/decodedb.scala
@@ -20,16 +20,16 @@ object ImmField extends DecodeField[InstructionPattern, Imm_TypeEnum.Type] {
     def chiselType = Imm_TypeEnum()
     def genTable(op: InstructionPattern): BitPat = {
         op.opcode.rawString match {
-            case "0000011" => BitPat(Imm_TypeEnum.Imm_I) // Loadxx
-            case "0100011" => BitPat(Imm_TypeEnum.Imm_S) // Storexx
-            case "0010011" => BitPat(Imm_TypeEnum.Imm_I) // xxI
-            case "0110111" => BitPat(Imm_TypeEnum.Imm_U) // LUI
-            case "0010111" => BitPat(Imm_TypeEnum.Imm_U) // AUIPC
-            case "1100011" => BitPat(Imm_TypeEnum.Imm_B) // Branchxx
-            case "1101111" => BitPat(Imm_TypeEnum.Imm_J) // JAL
-            case "1100111" => BitPat(Imm_TypeEnum.Imm_I) // JALR
-            case "1110011" => BitPat(Imm_TypeEnum.Imm_I) // CSRRx
-            case _ => BitPat.dontCare(Imm_width)
+            case "0000011" => BitPat(Imm_TypeEnum.toBitPat(Imm_TypeEnum.Imm_I)) // Loadxx
+            case "0100011" => BitPat(Imm_TypeEnum.toBitPat(Imm_TypeEnum.Imm_S)) // Storexx
+            case "0010011" => BitPat(Imm_TypeEnum.toBitPat(Imm_TypeEnum.Imm_I)) // xxI
+            case "0110111" => BitPat(Imm_TypeEnum.toBitPat(Imm_TypeEnum.Imm_U)) // LUI
+            case "0010111" => BitPat(Imm_TypeEnum.toBitPat(Imm_TypeEnum.Imm_U)) // AUIPC
+            case "1100011" => BitPat(Imm_TypeEnum.toBitPat(Imm_TypeEnum.Imm_B)) // Branchxx
+            case "1101111" => BitPat(Imm_TypeEnum.toBitPat(Imm_TypeEnum.Imm_J)) // JAL
+            case "1100111" => BitPat(Imm_TypeEnum.toBitPat(Imm_TypeEnum.Imm_I)) // JALR
+            case "1110011" => BitPat(Imm_TypeEnum.toBitPat(Imm_TypeEnum.Imm_I)) // CSRRx
+            case _ => BitPat.dontCare(Imm_TypeEnum.getWidth.W)
         }
     }
 }

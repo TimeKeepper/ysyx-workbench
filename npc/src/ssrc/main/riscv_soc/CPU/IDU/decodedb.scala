54a87791deede0999e9ec76314a9ef5b3d1112d0
tracer-ysyx <tracer@ysyx.org>
2024-10-18 21:34:34 +0800

    > generate verilog ysyx_23060198 林锦晖 Linux WIN-H8ULN2I4VKL 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:34:34 up 13 days, 23:06,  4 users,  load average: 1.03, 0.44, 0.26

--- npc/src/ssrc/main/riscv_soc/CPU/IDU/decodedb.scala
+++ npc/src/ssrc/main/riscv_soc/CPU/IDU/decodedb.scala
@@ -19,7 +19,7 @@ object ImmField extends DecodeField[InstructionPattern, Imm_TypeEnum.Type] {
     def name: String = "imm"
     def chiselType = Imm_TypeEnum()
     def genTable(op: InstructionPattern): BitPat = {
-        val Imm_Type = op.opcode.rawString match {
+        val Imm_Type = map(op.opcode.rawString match {
             case "0000011" => Imm_TypeEnum.Imm_I // Loadxx
             case "0100011" => Imm_TypeEnum.Imm_S // Storexx
             case "0010011" => Imm_TypeEnum.Imm_I // xxI
@@ -29,7 +29,7 @@ object ImmField extends DecodeField[InstructionPattern, Imm_TypeEnum.Type] {
             case "1101111" => Imm_TypeEnum.Imm_J // JAL
             case "1100111" => Imm_TypeEnum.Imm_I // JALR
             case "1110011" => Imm_TypeEnum.Imm_I // CSRRx
-        }
+        })
         BitPat(Imm_Type.litValue.U((Imm_Type.getWidth).W))
         // op.opcode.rawString match {
         //     case "0000011" => BitPat(Imm_TypeEnum.toBitPat(Imm_TypeEnum.Imm_I)) // Loadxx

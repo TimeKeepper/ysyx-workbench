b7550c60421fdf03d5b626046e6bdefd19eae77d
tracer-ysyx <tracer@ysyx.org>
2024-10-18 12:08:12 +0800

    > generate verilog ysyx_23060198 林锦晖 Linux WIN-H8ULN2I4VKL 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  12:08:12 up 13 days, 13:39,  4 users,  load average: 0.33, 0.26, 0.17

--- npc/src/ssrc/main/riscv_soc/CPU/IDU/decodedb.scala
+++ npc/src/ssrc/main/riscv_soc/CPU/IDU/decodedb.scala
@@ -121,218 +121,218 @@ object my_fooldecodedb {
     val possiblePattern = Seq(
         // Loads
         InstructionPattern(
-            func3 = BitPat("b000")
+            func3 = BitPat("b000"),
             opcode = BitPat("b0000011"), 
         ), // LB
         InstructionPattern(
-            func3 = BitPat("b001")
+            func3 = BitPat("b001"),
             opcode = BitPat("b0000011"), 
         ), // LH
         InstructionPattern(
-            func3 = BitPat("b010")
+            func3 = BitPat("b010"),
             opcode = BitPat("b0000011"), 
         ), // LW
         InstructionPattern(
-            func3 = BitPat("b100")
+            func3 = BitPat("b100"),
             opcode = BitPat("b0000011"), 
         ), // LBU
         InstructionPattern(
-            func3 = BitPat("b101")
+            func3 = BitPat("b101"),
             opcode = BitPat("b0000011"), 
         ), // LHU
 
         // Stores
         InstructionPattern(
-            func3 = BitPat("b000")
+            func3 = BitPat("b000"),
             opcode = BitPat("b0100011"), 
         ), // SB
         InstructionPattern(
-            func3 = BitPat("b001")
+            func3 = BitPat("b001"),
             opcode = BitPat("b0100011"), 
         ), // SH
         InstructionPattern(
-            func3 = BitPat("b010")
+            func3 = BitPat("b010"),
             opcode = BitPat("b0100011"), 
         ), // SW
         
         // Shifts
         InstructionPattern(
-            func7 = BitPat("b0000000")
-            func3 = BitPat("b001")
+            func7 = BitPat("b0000000"),
+            func3 = BitPat("b001"),
             opcode = BitPat("b0110011"), 
         ), // SLL 
         InstructionPattern(
-            func7 = BitPat("b0000000")
-            func3 = BitPat("b001")
+            func7 = BitPat("b0000000"),
+            func3 = BitPat("b001"),
             opcode = BitPat("b0010011"), 
         ), // SLLI
         InstructionPattern(
-            func7 = BitPat("b0000000")
-            func3 = BitPat("b101")
+            func7 = BitPat("b0000000"),
+            func3 = BitPat("b101"),
             opcode = BitPat("b0110011"), 
         ), // SRL 
         InstructionPattern(
-            func7 = BitPat("b0000000")
-            func3 = BitPat("b101")
+            func7 = BitPat("b0000000"),
+            func3 = BitPat("b101"),
             opcode = BitPat("b0010011"), 
         ), // SRLI
         InstructionPattern(
-            func7 = BitPat("b0100000")
-            func3 = BitPat("b101")
+            func7 = BitPat("b0100000"),
+            func3 = BitPat("b101"),
             opcode = BitPat("b0110011"), 
         ), // SRA 
         InstructionPattern(
-            func7 = BitPat("b0100000")
-            func3 = BitPat("b101")
+            func7 = BitPat("b0100000"),
+            func3 = BitPat("b101"),
             opcode = BitPat("b0010011"), 
         ), // SRAI
 
         // Arithmetic
         InstructionPattern(
-            func7 = BitPat("b0000000")
-            func3 = BitPat("b000")
+            func7 = BitPat("b0000000"),
+            func3 = BitPat("b000"),
             opcode = BitPat("b0110011"), 
         ), // ADD  
         InstructionPattern(
-            func7 = BitPat("b???????")
-            func3 = BitPat("b000")
+            func7 = BitPat("b???????"),
+            func3 = BitPat("b000"),
             opcode = BitPat("b0010011"), 
         ), // ADDI 
         InstructionPattern(
-            func7 = BitPat("b0100000")
-            func3 = BitPat("b000")
+            func7 = BitPat("b0100000"),
+            func3 = BitPat("b000"),
             opcode = BitPat("b0110011"), 
         ), // SUB  
         InstructionPattern(
-            func7 = BitPat("b???????")
-            func3 = BitPat("b???")
+            func7 = BitPat("b???????"),
+            func3 = BitPat("b???"),
             opcode = BitPat("b0110111"), 
         ), // LUI  
         InstructionPattern(
-            func7 = BitPat("b???????")
-            func3 = BitPat("b???")
+            func7 = BitPat("b???????"),
+            func3 = BitPat("b???"),
             opcode = BitPat("b0010111"), 
         ), // AUIPC
         
         // Logical
         InstructionPattern(
-            func7 = BitPat("b0000000")
-            func3 = BitPat("b100")
+            func7 = BitPat("b0000000"),
+            func3 = BitPat("b100"),
             opcode = BitPat("b0110011"), 
         ), // XOR 
         InstructionPattern(
-            func7 = BitPat("b???????")
-            func3 = BitPat("b100")
+            func7 = BitPat("b???????"),
+            func3 = BitPat("b100"),
             opcode = BitPat("b0010011"), 
         ), // XORI
         InstructionPattern(
-            func7 = BitPat("b0000000")
-            func3 = BitPat("b110")
+            func7 = BitPat("b0000000"),
+            func3 = BitPat("b110"),
             opcode = BitPat("b0110011"), 
         ), // OR  
         InstructionPattern(
-            func7 = BitPat("b???????")
-            func3 = BitPat("b110")
+            func7 = BitPat("b???????"),
+            func3 = BitPat("b110"),
             opcode = BitPat("b0010011"), 
         ), // ORI 
         InstructionPattern(
-            func7 = BitPat("b0000000")
-            func3 = BitPat("b111")
+            func7 = BitPat("b0000000"),
+            func3 = BitPat("b111"),
             opcode = BitPat("b0110011"), 
         ), // AND 
         InstructionPattern(
-            func7 = BitPat("b???????")
-            func3 = BitPat("b111")
+            func7 = BitPat("b???????"),
+            func3 = BitPat("b111"),
             opcode = BitPat("b0010011"), 
         ), // ANDI
 
         // Compare
         InstructionPattern(
-            func7 = BitPat("b0000000")
-            func3 = BitPat("b010")
+            func7 = BitPat("b0000000"),
+            func3 = BitPat("b010"),
             opcode = BitPat("b0110011"), 
         ), // SLT  
         InstructionPattern(
-            func7 = BitPat("b???????")
-            func3 = BitPat("b010")
+            func7 = BitPat("b???????"),
+            func3 = BitPat("b010"),
             opcode = BitPat("b0010011"), 
         ), // SLTI 
         InstructionPattern(
-            func7 = BitPat("b0000000")
-            func3 = BitPat("b011")
+            func7 = BitPat("b0000000"),
+            func3 = BitPat("b011"),
             opcode = BitPat("b0110011"), 
         ), // SLTU 
         InstructionPattern(
-            func7 = BitPat("b???????")
-            func3 = BitPat("b011")
+            func7 = BitPat("b???????"),
+            func3 = BitPat("b011"),
             opcode = BitPat("b0010011"), 
         ), // SLTIU
 
         // Branches
         InstructionPattern(
-            func7 = BitPat("b???????")
-            func3 = BitPat("b000")
+            func7 = BitPat("b???????"),
+            func3 = BitPat("b000"),
             opcode = BitPat("b1100011"), 
         ), // BEQ 
         InstructionPattern(
-            func7 = BitPat("b???????")
-            func3 = BitPat("b001")
+            func7 = BitPat("b???????"),
+            func3 = BitPat("b001"),
             opcode = BitPat("b1100011"), 
         ), // BNE 
         InstructionPattern(
-            func7 = BitPat("b???????")
-            func3 = BitPat("b100")
+            func7 = BitPat("b???????"),
+            func3 = BitPat("b100"),
             opcode = BitPat("b1100011"), 
         ), // BLT 
         InstructionPattern(
-            func7 = BitPat("b???????")
-            func3 = BitPat("b101")
+            func7 = BitPat("b???????"),
+            func3 = BitPat("b101"),
             opcode = BitPat("b1100011"), 
         ), // BGE 
         InstructionPattern(
-            func7 = BitPat("b???????")
-            func3 = BitPat("b110")
+            func7 = BitPat("b???????"),
+            func3 = BitPat("b110"),
             opcode = BitPat("b1100011"), 
         ), // BLTU
         InstructionPattern(
-            func7 = BitPat("b???????")
-            func3 = BitPat("b111")
+            func7 = BitPat("b???????"),
+            func3 = BitPat("b111"),
             opcode = BitPat("b1100011"), 
         ), // BGEU
 
         // Jump & Link
         InstructionPattern(
-            func7 = BitPat("b???????")
-            func3 = BitPat("b???")
+            func7 = BitPat("b???????"),
+            func3 = BitPat("b???"),
             opcode = BitPat("b1101111"), 
         ), // JAL 
         InstructionPattern(
-            func7 = BitPat("b???????")
-            func3 = BitPat("b000")
+            func7 = BitPat("b???????"),
+            func3 = BitPat("b000"),
             opcode = BitPat("b1100111"), 
         ), // JALR
 
         // CSR Access
         InstructionPattern(
-            func7 = BitPat("b???????")
-            func3 = BitPat("b001")
+            func7 = BitPat("b???????"),
+            func3 = BitPat("b001"),
             opcode = BitPat("b1110011"), 
         ), // CSRRW
         InstructionPattern(
-            func7 = BitPat("b???????")
-            func3 = BitPat("b010")
+            func7 = BitPat("b???????"),
+            func3 = BitPat("b010"),
             opcode = BitPat("b1110011"), 
         ), // CSRRS
 
         // Change Level
         InstructionPattern(
-            func7 = BitPat("b0000000")
-            func3 = BitPat("b000")
+            func7 = BitPat("b0000000"),
+            func3 = BitPat("b000"),
             opcode = BitPat("b1110011"), 
         ), // ECALL
         InstructionPattern(
-            func7 = BitPat("b0011000")
-            func3 = BitPat("b000")
+            func7 = BitPat("b0011000"),
+            func3 = BitPat("b000"),
             opcode = BitPat("b1110011"), 
         ), // MRET
     )

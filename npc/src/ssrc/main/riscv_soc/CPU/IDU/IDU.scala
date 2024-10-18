4fa76efd9748530a6be05015e676effdf0a3dc8e
tracer-ysyx <tracer@ysyx.org>
2024-10-18 09:52:24 +0800

    > generate verilog ysyx_23060198 林锦晖 Linux WIN-H8ULN2I4VKL 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  09:52:24 up 13 days, 11:23,  4 users,  load average: 1.03, 0.62, 0.63

--- npc/src/ssrc/main/riscv_soc/CPU/IDU/IDU.scala
+++ npc/src/ssrc/main/riscv_soc/CPU/IDU/IDU.scala
@@ -240,7 +240,7 @@ class ysyx_23060198_IDU extends Module{
     io.IDU_2_EXU.bits.Branch       <> RegEnable(ctrlSignals(0), comunication_succeed) 
     io.IDU_2_EXU.bits.MemtoReg     <> RegEnable(decodeResult(MemtoRegField),         comunication_succeed) 
     io.IDU_2_EXU.bits.MemWr        <> RegEnable(decodeResult(MemWrField),         comunication_succeed) 
-    io.IDU_2_EXU.bits.MemOp        <> RegEnable(decodeResult(MemOpField),         comunication_succeed) 
+    io.IDU_2_EXU.bits.MemOp        <> RegEnable(ctrlSignals(1),         comunication_succeed) 
     io.IDU_2_EXU.bits.ALUAsrc      <> RegEnable(ctrlSignals(2),         comunication_succeed) 
     io.IDU_2_EXU.bits.ALUBsrc      <> RegEnable(ctrlSignals(3),         comunication_succeed) 
     io.IDU_2_EXU.bits.ALUctr       <> RegEnable(ctrlSignals(4),         comunication_succeed) 

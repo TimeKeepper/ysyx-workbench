ba17e14a5aa4e90c4a2a6e9fef8c0f6a1cc25378
tracer-ysyx <tracer@ysyx.org>
2024-10-21 00:15:11 +0800

    > generate verilog ysyx_23060198 林锦晖 Linux WIN-H8ULN2I4VKL 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  00:15:11 up 16 days,  1:46,  4 users,  load average: 0.05, 0.21, 0.16

--- npc/src/ssrc/main/riscv_soc/CPU/IDU/IDU.scala
+++ npc/src/ssrc/main/riscv_soc/CPU/IDU/IDU.scala
@@ -20,14 +20,16 @@ object Imm_Field extends DecodeField[rvInstructionPattern, Imm_TypeEnum.Type] {
     override def genTable(i: rvInstructionPattern): BitPat = {
         val immType = i.inst.args
             .map(_.name match{
-                case "imm12"                 => Imm_TypeEnum.Imm_I
-                case "shamtw"                => Imm_TypeEnum.Imm_I
-                case "imm12hi" | "imm12lo"   => Imm_TypeEnum.Imm_S
-                case "bimm12hi" | "bimm12lo" => Imm_TypeEnum.Imm_B
-                case "imm20"                 => Imm_TypeEnum.Imm_U
-                case "jimm20"                => Imm_TypeEnum.Imm_J
+                case "imm12" | "shamtw" | "csr"     => Imm_TypeEnum.Imm_I
+                case "imm12hi" | "imm12lo"          => Imm_TypeEnum.Imm_S
+                case "bimm12hi" | "bimm12lo"        => Imm_TypeEnum.Imm_B
+                case "imm20"                        => Imm_TypeEnum.Imm_U
+                case "jimm20"                       => Imm_TypeEnum.Imm_J
+                case _                              => Imm_TypeEnum.Imm_None
             })
+            .filterNot(_ == Imm_TypeEnum.Imm_None)
             .headOption
+            .getOrElse(Imm_TypeEnum.Imm_None)
 
         BitPat(immType.litValue.U((immType.getWidth).W))
     }

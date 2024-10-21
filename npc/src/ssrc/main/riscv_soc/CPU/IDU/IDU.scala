80e9b6f77b23794c8ba1b74bb136cbe07f528832
tracer-ysyx <tracer@ysyx.org>
2024-10-21 19:16:56 +0800

    > generate verilog ysyx_23060198 林锦晖 Linux WIN-H8ULN2I4VKL 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:16:56 up 16 days, 20:48,  4 users,  load average: 0.25, 0.10, 0.09

--- npc/src/ssrc/main/riscv_soc/CPU/IDU/IDU.scala
+++ npc/src/ssrc/main/riscv_soc/CPU/IDU/IDU.scala
@@ -18,6 +18,18 @@ object Imm_Field extends DecodeField[rvInstructionPattern, Imm_TypeEnum.Type] {
     override def name: String = "imm"
     override def chiselType = Imm_TypeEnum()
     override def genTable(i: rvInstructionPattern): BitPat = {
+        val immType_test  = i.inst.args
+            .map(_.name match{
+                case "imm12" | "shamtw" | "csr"     => Imm_TypeEnum.Imm_I
+                case "imm12hi" | "imm12lo"          => Imm_TypeEnum.Imm_S
+                case "bimm12hi" | "bimm12lo"        => Imm_TypeEnum.Imm_B
+                case "imm20"                        => Imm_TypeEnum.Imm_U
+                case "jimm20"                       => Imm_TypeEnum.Imm_J
+                case _                              => Imm_TypeEnum.Imm_None
+            })
+
+        print(immType_test)
+
         val immType = i.inst.args
             .map(_.name match{
                 case "imm12" | "shamtw" | "csr"     => Imm_TypeEnum.Imm_I

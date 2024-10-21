b44851393a5b39f5ca6c4a5d0a8a49eace1b2ec7
tracer-ysyx <tracer@ysyx.org>
2024-10-22 01:12:59 +0800

    > generate verilog ysyx_23060198 林锦晖 Linux WIN-H8ULN2I4VKL 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  01:12:59 up 28 min,  2 users,  load average: 0.33, 0.18, 0.19

--- npc/src/ssrc/main/riscv_soc/CPU/IDU/IDU.scala
+++ npc/src/ssrc/main/riscv_soc/CPU/IDU/IDU.scala
@@ -11,6 +11,7 @@ import bus_state._
 // riscv generating number(all meassge ALU and other thing needs) unit
 
 case class rvInstructionPattern(val inst: rvdecoderdb.Instruction) extends DecodePattern {
+    println(inst.toString())
     override def bitPat: BitPat = BitPat("b" + inst.encoding.toString())
 }
 
@@ -18,26 +19,18 @@ object Imm_Field extends DecodeField[rvInstructionPattern, Imm_TypeEnum.Type] wi
     override def name: String = "imm"
     override def chiselType = Imm_TypeEnum()
     override def genTable(i: rvInstructionPattern): BitPat = {
-        // i.inst.args
-        //     .map(_.name match{
-        //         case "imm12" | "shamtw" | "csr"     => Get_BitPat(Imm_TypeEnum.Imm_I)
-        //         case "imm12hi" | "imm12lo"          => Get_BitPat(Imm_TypeEnum.Imm_S)
-        //         case "bimm12hi" | "bimm12lo"        => Get_BitPat(Imm_TypeEnum.Imm_B)
-        //         case "imm20"                        => Get_BitPat(Imm_TypeEnum.Imm_U)
-        //         case "jimm20"                       => Get_BitPat(Imm_TypeEnum.Imm_J)
-        //         case _                              => BitPat.dontCare(Imm_TypeEnum.getWidth)
-        //     })
-        //     .filterNot(_ == BitPat.dontCare(Imm_TypeEnum.getWidth))
-        //     .headOption
-        //     .getOrElse(BitPat.dontCare(Imm_TypeEnum.getWidth))
-        i.inst match {
-            case _ if i.inst.is_I => Get_BitPat(Imm_TypeEnum.Imm_I)
-            case _ if i.inst.is_S => Get_BitPat(Imm_TypeEnum.Imm_S)
-            case _ if i.inst.is_B => Get_BitPat(Imm_TypeEnum.Imm_B)
-            case _ if i.inst.is_U => Get_BitPat(Imm_TypeEnum.Imm_U)
-            case _ if i.inst.is_J => Get_BitPat(Imm_TypeEnum.Imm_J)
-            case _ => BitPat.dontCare(Imm_TypeEnum.getWidth)
-        }
+        i.inst.args
+            .map(_.name match{
+                case "imm12" | "shamtw" | "csr"     => Get_BitPat(Imm_TypeEnum.Imm_I)
+                case "imm12hi" | "imm12lo"          => Get_BitPat(Imm_TypeEnum.Imm_S)
+                case "bimm12hi" | "bimm12lo"        => Get_BitPat(Imm_TypeEnum.Imm_B)
+                case "imm20"                        => Get_BitPat(Imm_TypeEnum.Imm_U)
+                case "jimm20"                       => Get_BitPat(Imm_TypeEnum.Imm_J)
+                case _                              => BitPat.dontCare(Imm_TypeEnum.getWidth)
+            })
+            .filterNot(_ == BitPat.dontCare(Imm_TypeEnum.getWidth))
+            .headOption
+            .getOrElse(BitPat.dontCare(Imm_TypeEnum.getWidth))
     }
 }
 

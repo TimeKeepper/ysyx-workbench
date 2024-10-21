59fe7eab05e47b19b5493f586283bab93a7d597d
tracer-ysyx <tracer@ysyx.org>
2024-10-22 00:55:54 +0800

    > generate verilog ysyx_23060198 林锦晖 Linux WIN-H8ULN2I4VKL 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  00:55:54 up 11 min,  2 users,  load average: 0.40, 0.48, 0.33

--- npc/src/ssrc/main/riscv_soc/CPU/IDU/IDU.scala
+++ npc/src/ssrc/main/riscv_soc/CPU/IDU/IDU.scala
@@ -38,17 +38,14 @@ object Bran_Field extends DecodeField[rvInstructionPattern, Bran_TypeEnum.Type] 
     override def chiselType = Bran_TypeEnum()
     override def genTable(i: rvInstructionPattern): BitPat = {
         i.inst.name match {
-            case "beq"      => Get_BitPat(Bran_TypeEnum.Bran_Jeq)
-            case "bne"      => Get_BitPat(Bran_TypeEnum.Bran_Jne)
-            case "blt"      => Get_BitPat(Bran_TypeEnum.Bran_Jlt)
-            case "bge"      => Get_BitPat(Bran_TypeEnum.Bran_Jge)
-            case "bltu"     => Get_BitPat(Bran_TypeEnum.Bran_Jlt)
-            case "bgeu"     => Get_BitPat(Bran_TypeEnum.Bran_Jge)
-            case "jal"      => Get_BitPat(Bran_TypeEnum.Bran_Jmp)
-            case "jalr"     => Get_BitPat(Bran_TypeEnum.Bran_Jmpr)
-            case "ecall"    => Get_BitPat(Bran_TypeEnum.Bran_Jcsr)
-            case "mret"     => Get_BitPat(Bran_TypeEnum.Bran_Jcsr)
-            case _          => Get_BitPat(Bran_TypeEnum.Bran_NJmp)
+            case "beq"              => Get_BitPat(Bran_TypeEnum.Bran_Jeq)
+            case "bne"              => Get_BitPat(Bran_TypeEnum.Bran_Jne)
+            case "blt" | "bltu"     => Get_BitPat(Bran_TypeEnum.Bran_Jlt)
+            case "bge" | "bgeu"     => Get_BitPat(Bran_TypeEnum.Bran_Jge)
+            case "jal"              => Get_BitPat(Bran_TypeEnum.Bran_Jmp)
+            case "jalr"             => Get_BitPat(Bran_TypeEnum.Bran_Jmpr)
+            case "ecall" | "mret"   => Get_BitPat(Bran_TypeEnum.Bran_Jcsr)
+            case _                  => Get_BitPat(Bran_TypeEnum.Bran_NJmp)
         }
     }
 }

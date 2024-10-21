74039f1fdcb1473cf47b0fd5ac9e428932893b4b
tracer-ysyx <tracer@ysyx.org>
2024-10-22 05:31:53 +0800

    > generate verilog ysyx_23060198 林锦晖 Linux WIN-H8ULN2I4VKL 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  05:31:53 up  4:49,  4 users,  load average: 0.38, 0.24, 0.14

--- npc/src/ssrc/main/riscv_soc/CPU/IDU/IDU.scala
+++ npc/src/ssrc/main/riscv_soc/CPU/IDU/IDU.scala
@@ -190,9 +190,6 @@ class ysyx_23060198_IDU extends Module{
     io.IFU_2_IDU.ready := state === bus_state.s_wait_valid
     val comunication_succeed = (io.IFU_2_IDU.valid && io.IFU_2_IDU.ready)
 
-    val decodeTable = new DecodeTable(my_fooldecodedb.possiblePattern, my_fooldecodedb.allFields)
-    val decodeResult = decodeTable.decode(io.IFU_2_IDU.bits.data)
-
     val instTable = rvdecoderdb.fromFile.instructions(os.pwd / os.up / os.up / "src" / "ssrc" / "main" /  "rvdecoderdb" / "rvdecoderdbtest" / "jvm" / "riscv-opcodes")
 
     val rv32iExceptInstructions = 

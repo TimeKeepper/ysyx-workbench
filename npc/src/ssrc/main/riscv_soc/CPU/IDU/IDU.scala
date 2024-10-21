b370ac2644d11b43ced5fea3ab4bd5f2ff13dc3a
tracer-ysyx <tracer@ysyx.org>
2024-10-21 19:24:24 +0800

    > generate verilog ysyx_23060198 林锦晖 Linux WIN-H8ULN2I4VKL 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:24:24 up 16 days, 20:55,  4 users,  load average: 0.06, 0.24, 0.17

--- npc/src/ssrc/main/riscv_soc/CPU/IDU/IDU.scala
+++ npc/src/ssrc/main/riscv_soc/CPU/IDU/IDU.scala
@@ -29,7 +29,6 @@ object Imm_Field extends DecodeField[rvInstructionPattern, Imm_TypeEnum.Type] {
             })
             .filterNot(_ == None)
             .headOption
-            .getOrElse(Imm_TypeEnum.Imm_None)
 
         print(immType_test)
 

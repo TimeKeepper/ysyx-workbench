f8899d8bd614db980e32f8e9d90ddc088c39614f
tracer-ysyx <tracer@ysyx.org>
2024-10-22 10:51:09 +0800

    > generate verilog ysyx_23060198 林锦晖 Linux WIN-H8ULN2I4VKL 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  10:51:09 up 10:10,  5 users,  load average: 0.71, 0.44, 0.28

--- npc/src/ssrc/main/riscv_soc/CPU/EXU/LSU.scala
+++ npc/src/ssrc/main/riscv_soc/CPU/EXU/LSU.scala
@@ -169,7 +169,7 @@ class ysyx_23060198_LSU extends Module{
     if(Config.DPIC_on){
         val LS_DPIC = Module(new LSU_DPIC)
         LS_DPIC.io.LS_begin  := io.AXI.araddr.valid || io.AXI.awaddr.valid
-        LS_DPIC.io.addr      := io.IDU_2_EXU.bits.EXU_A + io.IDU_2_EXU.bits.EXU_B
+        LS_DPIC.io.addr      := io.IDU_2_EXU.bits.GPR_Adata + io.IDU_2_EXU.bits.Imm
 
         val LSU_PC = Module(new LSU_PC)
         LSU_PC.io.clock := clock

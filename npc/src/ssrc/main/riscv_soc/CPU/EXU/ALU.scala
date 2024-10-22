85160cbdfe4189337bd33bba9c94dc9dbda562f2
tracer-ysyx <tracer@ysyx.org>
2024-10-22 08:26:19 +0800

    > generate verilog ysyx_23060198 林锦晖 Linux WIN-H8ULN2I4VKL 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  08:26:19 up  7:45,  5 users,  load average: 0.18, 0.26, 0.17

--- npc/src/ssrc/main/riscv_soc/CPU/EXU/ALU.scala
+++ npc/src/ssrc/main/riscv_soc/CPU/EXU/ALU.scala
@@ -120,7 +120,10 @@ class ysyx_23060198_ALU extends Module {
     Bran_TypeEnum.Bran_Jeq -> Mux(src_A === src_B, true.B, false.B),
     Bran_TypeEnum.Bran_Jne -> Mux(src_A =/= src_B, true.B, false.B),
     Bran_TypeEnum.Bran_Jlt -> Mux(Less, true.B, false.B),
-    Bran_TypeEnum.Bran_Jge -> Mux(!Less, true.B, false.B)
+    Bran_TypeEnum.Bran_Jge -> Mux(!Less, true.B, false.B),
+    Bran_TypeEnum.Bran_Jmp -> true.B,
+    Bran_TypeEnum.Bran_Jmpr -> true.B,
+    Bran_TypeEnum.Bran_jcsr -> true.B
   ))
 
   val Jmp_Pc = MuxLookup(io.IDU_2_EXU.bits.Branch, io.IDU_2_EXU.bits.PC + io.IDU_2_EXU.bits.Imm)(Seq(

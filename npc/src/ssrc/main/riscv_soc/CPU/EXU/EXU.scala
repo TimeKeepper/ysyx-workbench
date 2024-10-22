0c744f18d24a2757c47d1b252a651dbc648db5cc
tracer-ysyx <tracer@ysyx.org>
2024-10-22 08:50:27 +0800

    > generate verilog ysyx_23060198 林锦晖 Linux WIN-H8ULN2I4VKL 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  08:50:27 up  8:09,  5 users,  load average: 1.78, 1.57, 0.80

--- npc/src/ssrc/main/riscv_soc/CPU/EXU/EXU.scala
+++ npc/src/ssrc/main/riscv_soc/CPU/EXU/EXU.scala
@@ -49,11 +49,7 @@ class ysyx_23060198_EXU extends Module {
     val default_NextPC = io.IDU_2_EXU.bits.PC + 4.U
     val bran_NextPC = io.IDU_2_EXU.bits.PC + io.IDU_2_EXU.bits.Imm
 
-    val Next_PC = MuxLookup(io.IDU_2_EXU.bits.Branch, default_NextPC)(Seq(
-        Bran_TypeEnum.Bran_Jeq -> Mux(is_jmp, bran_NextPC, default_NextPC),
-        Bran_TypeEnum.Bran_Jne -> Mux(is_jmp, default_NextPC, bran_NextPC),
-        Bran_TypeEnum.Bran_Jlt -> Mux(is_jmp, bran_NextPC, default_NextPC),
-        Bran_TypeEnum.Bran_Jge -> Mux(is_jmp, default_NextPC, bran_NextPC),
+    val Next_PC = MuxLookup(io.IDU_2_EXU.bits.Branch, Mux(is_jmp, default_NextPC, bran_NextPC))(Seq(
         Bran_TypeEnum.Bran_Jmp -> (io.IDU_2_EXU.bits.PC + io.IDU_2_EXU.bits.Imm),
         Bran_TypeEnum.Bran_Jmpr -> (io.IDU_2_EXU.bits.GPR_Adata + io.IDU_2_EXU.bits.Imm),
         Bran_TypeEnum.Bran_Jcsr -> (io.IDU_2_EXU.bits.CSR_rdata)

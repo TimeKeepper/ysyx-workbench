df9edc71043d9c8fe7c149fb40cfd712a4fa4ff9
tracer-ysyx <tracer@ysyx.org>
2024-10-26 15:51:24 +0800

    > generate verilog ysyx_23060198 林锦晖 Linux WIN-H8ULN2I4VKL 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:51:24 up 1 day, 14:01,  4 users,  load average: 0.39, 0.43, 0.31

--- npc/src/ssrc/main/riscv_soc/CPU/BUSio/io.scala
+++ npc/src/ssrc/main/riscv_soc/CPU/BUSio/io.scala
@@ -28,8 +28,6 @@ class BUS_REG_2_IDU extends Bundle{
 
 class BUS_IDU_2_EXU extends Bundle{
     val Branch   = Bran_TypeEnum()
-    val MemtoReg = Bool()
-    val MemWr    = Bool()
     val MemOp    = MemOp_TypeEnum()
     val EXU_A    = UInt(32.W)
     val EXU_B    = UInt(32.W)

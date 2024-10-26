e6bb93844f1b973560bd8a04614cd9c07e222036
tracer-ysyx <tracer@ysyx.org>
2024-10-26 14:05:10 +0800

    > generate verilog ysyx_23060198 林锦晖 Linux WIN-H8ULN2I4VKL 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:05:10 up 1 day, 12:10,  4 users,  load average: 0.42, 0.27, 0.22

--- npc/src/ssrc/main/riscv_soc/CPU/IDU/IDU.scala
+++ npc/src/ssrc/main/riscv_soc/CPU/IDU/IDU.scala
@@ -220,7 +220,7 @@ class ysyx_23060198_IDU extends Module{
         .toSeq
     val instList = rviInstList ++ rv32iInstList ++ rvsysInstList ++ rvzicsrInstList
 
-    val rvdecoderTable = new DecodeTable(instList, Seq(Imm_Field, Bran_Field, MemOp_Field, ALUAsrc_Field, ALUBsrc_Field, ALUctr_Field, csr_ctr_Field, RegWr_Field, MemtoReg_Field, MemWr_Field))
+    val rvdecoderTable = new DecodeTable(instList, Seq(Imm_Field, Bran_Field, MemOp_Field, ALUAsrc_Field, ALUBsrc_Field, EXUctr_Field, csr_ctr_Field, RegWr_Field, MemtoReg_Field, MemWr_Field))
     val rvdecoderResult = rvdecoderTable.decode(io.IFU_2_IDU.bits.data)
 
     val imm = MuxLookup(rvdecoderResult(Imm_Field), 0.U)(

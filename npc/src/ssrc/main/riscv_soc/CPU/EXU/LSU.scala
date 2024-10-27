39e87718b585809c8ab6460d111ba8a26464bbe6
tracer-ysyx <tracer@ysyx.org>
2024-10-27 10:11:59 +0800

    > generate verilog ysyx_23060198 林锦晖 Linux WIN-H8ULN2I4VKL 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  10:11:59 up 2 days,  8:53,  4 users,  load average: 0.19, 0.27, 0.19

--- npc/src/ssrc/main/riscv_soc/CPU/EXU/LSU.scala
+++ npc/src/ssrc/main/riscv_soc/CPU/EXU/LSU.scala
@@ -117,7 +117,7 @@ class ysyx_23060198_LSU extends Module{
 
     io.AXI.araddr.bits.addr  <> io.IDU_2_EXU.bits.EXU_A + io.IDU_2_EXU.bits.Imm
     io.AXI.awaddr.bits.addr  <> io.IDU_2_EXU.bits.EXU_A + io.IDU_2_EXU.bits.Imm
-    io.AXI.wdata.bits.data   <> (io.IDU_2_EXU.bits.GPR_Bdata << (io.AXI.awaddr.bits.addr(1,0) << 3.U))(31, 0)
+    io.AXI.wdata.bits.data   <> (io.IDU_2_EXU.bits.EXU_B << (io.AXI.awaddr.bits.addr(1,0) << 3.U))(31, 0)
     
     when(io.IDU_2_EXU.bits.MemOp === MemOp_TypeEnum.MemOp_1BU || io.IDU_2_EXU.bits.MemOp === MemOp_TypeEnum.MemOp_1BS){
         io.AXI.wdata.bits.strb   := MuxLookup(io.AXI.awaddr.bits.addr(1,0), "b0001".U)(Seq(

e5f9f8478a4ebecda4b0fa44a77e614c1123e193
tracer-ysyx <tracer@ysyx.org>
2024-11-01 08:34:19 +0800

    > sim npc ysyx_23060198 林锦晖 Linux WIN-H8ULN2I4VKL 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  08:34:19 up 12:48,  3 users,  load average: 1.69, 1.02, 0.82

--- npc/src/ssrc/main/riscv_soc/CPU/CPU.scala
+++ npc/src/ssrc/main/riscv_soc/CPU/CPU.scala
@@ -64,6 +64,7 @@ object CPUAXI4BundleParameters {
 class CPU(idBits: Int)(implicit p: Parameters) extends LazyModule {
   ElaborationArtefacts.add("graphml", graphML)
 
+  val LazyIFU = LazyModule(new ysyx_23060198_IFU(idBits))
   val LazyEXU = LazyModule(new ysyx_23060198_EXU(idBits))
   // val beatBytes = 4
   // val node = AXI4SlaveNode(Seq(AXI4SlavePortParameters(
@@ -84,7 +85,7 @@ class CPU(idBits: Int)(implicit p: Parameters) extends LazyModule {
       val interrupt = Input(Bool())
     })
     
-    val IFU             = Module(new ysyx_23060198_IFU)
+    val IFU             = LazyIFU.module
     val IDU             = Module(new ysyx_23060198_IDU)
     val EXU             = LazyEXU.module
     val WBU             = Module(new ysyx_23060198_WBU)

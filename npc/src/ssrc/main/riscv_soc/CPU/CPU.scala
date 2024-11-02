8424bdbc9226fbc1e3e914e32f7090233fbf5816
tracer-ysyx <tracer@ysyx.org>
2024-11-02 19:03:45 +0800

    > sim npc ysyx_23060198 林锦晖 Linux WIN-H8ULN2I4VKL 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:03:45 up 2 days, 42 min,  6 users,  load average: 1.96, 0.86, 0.62

--- npc/src/ssrc/main/riscv_soc/CPU/CPU.scala
+++ npc/src/ssrc/main/riscv_soc/CPU/CPU.scala
@@ -173,14 +173,14 @@ class npc(idBits: Int)(implicit p: Parameters) extends LazyModule {
   val LazyIFU = LazyModule(new ysyx_23060198_IFU(idBits = idBits))
   val LazyEXU = LazyModule(new ysyx_23060198_EXU(idBits = idBits))
 
-  val xbar = AXI4Xbar(maxFlightPerId = 1, awQueueDepth = 1)
+  val xbar = AXI4Xbar(maxFlightPerId = 3, awQueueDepth = 2)
   xbar := LazyIFU.masterNode
   xbar := LazyEXU.masterNode
   val beatBytes = 4
 
   val luart = LazyModule(new UART(AddressSet.misaligned(0x10000000, 0x1000)))
   val lclint = LazyModule(new CLINT(AddressSet.misaligned(0xa0000048L, 0x10)))
-  val lsram = LazyModule(new SRAM(AddressSet.misaligned(0x80000000L, 0x20000000), 1.U))
+  val lsram = LazyModule(new SRAM(AddressSet.misaligned(0x80000000L, 0x2000000), 1.U))
 
   luart.node := xbar
   lclint.node := xbar

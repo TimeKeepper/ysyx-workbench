8fd59a9a85341aef974e807306d6a0bdd83b9903
tracer-ysyx <tracer@ysyx.org>
2024-11-02 20:45:08 +0800

    > sim npc ysyx_23060198 林锦晖 Linux WIN-H8ULN2I4VKL 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:45:08 up 2 days,  2:26,  6 users,  load average: 2.04, 1.20, 0.74

--- npc/src/csrc/cpu/difftertest.c
+++ npc/src/csrc/cpu/difftertest.c
@@ -151,7 +151,7 @@ void difftest_step(vaddr_t pc, vaddr_t npc) {
   ref_difftest_regcpy(&ref_r, DIFFTEST_TO_DUT);
 
   checkregs(&ref_r, pc);
-  checkmems();
+  // checkmems();
   #endif
 }
 // word_t paddr_read(paddr_t addr, int len);

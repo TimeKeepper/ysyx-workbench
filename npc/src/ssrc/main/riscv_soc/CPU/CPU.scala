ab5fea65d15bc372bb1b5ac326a46634fd37c417
tracer-ysyx <tracer@ysyx.org>
2024-11-03 19:49:52 +0800

    > sim npc ysyx_23060198 林锦晖 Linux WIN-H8ULN2I4VKL 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:49:52 up  7:35,  3 users,  load average: 1.29, 0.67, 0.47

--- npc/src/ssrc/main/riscv_soc/CPU/CPU.scala
+++ npc/src/ssrc/main/riscv_soc/CPU/CPU.scala
@@ -175,7 +175,7 @@ class npc(idBits: Int)(implicit p: Parameters) extends LazyModule {
   val LazyIFU = LazyModule(new ysyx_23060198_IFU(idBits = idBits))
   val LazyEXU = LazyModule(new ysyx_23060198_EXU(idBits = idBits))
 
-  val xbar = AXI4Xbar(maxFlightPerId = 3, awQueueDepth = 2)
+  val xbar = AXI4Xbar()
   xbar := LazyIFU.masterNode
   xbar := LazyEXU.masterNode
   val beatBytes = 4

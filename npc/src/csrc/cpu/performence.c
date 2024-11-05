7e4dfef249d53d516a64b6a99e266b5262f24ca9
tracer-ysyx <tracer@ysyx.org>
2024-11-06 03:12:34 +0800

    > sim npc ysyx_23060198 林锦晖 Linux WIN-H8ULN2I4VKL 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  03:12:34 up 2 days, 15:52,  5 users,  load average: 0.13, 0.28, 0.25

--- npc/src/csrc/cpu/performence.c
+++ npc/src/csrc/cpu/performence.c
@@ -66,13 +66,13 @@ void watchpoint_catch(void){
 static uint32_t npc = RESET_VECTOR;
 
 extern "C" void pc_update(uint32_t n_npc){
-    Log("npc: %x", n_npc);
     npc = n_npc;
 }
 
 extern "C" void inst_comp_update(){
     inst_cnt++;
     num_of_inst_to_end = num_of_inst_to_end == 0 ? 0 : num_of_inst_to_end - 1;
+    Log("cpu.pc = 0x%08x, npc = 0x%08x", cpu.pc, npc);
     difftest_step(cpu.pc, npc);
     
     watchpoint_catch();          //检查watchpoint

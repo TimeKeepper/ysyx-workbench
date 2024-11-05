64a80f397a2588f21d6c6b77fb350c239e05df35
tracer-ysyx <tracer@ysyx.org>
2024-11-05 19:54:06 +0800

    > sim npc ysyx_23060198 林锦晖 Linux WIN-H8ULN2I4VKL 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:54:06 up 2 days,  8:23,  5 users,  load average: 3.88, 2.29, 2.04

--- npc/src/ssrc/main/riscv_soc/CPU/IFU/IFU.scala
+++ npc/src/ssrc/main/riscv_soc/CPU/IFU/IFU.scala
@@ -103,8 +103,8 @@ class ysyx_23060198_IFU(idBits: Int)(implicit p: Parameters) extends LazyModule 
         io.IFU_2_IDU.valid := state === bus_state.s_wait_ready
         io.IFU_2_IDU.bits.data := RegEnable(AXI.r.bits.data, AXI.r.fire)
 
-        io.IFU_2_REG.GPR_Aaddr <> AXI.r.bits.data(19, 15)
-        io.IFU_2_REG.GPR_Baddr <> AXI.r.bits.data(24, 20)
+        io.IFU_2_REG.GPR_Aaddr <> io.IFU_2_IDU.bits.data(19, 15)
+        io.IFU_2_REG.GPR_Baddr <> io.IFU_2_IDU.bits.data(24, 20)
 
         AXI.aw.valid := false.B
         AXI.aw.bits.addr := 0.U

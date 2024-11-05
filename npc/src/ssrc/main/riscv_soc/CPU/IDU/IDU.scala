d08ec44f2a390bd895ad6b1002c47de2e7cf51e8
tracer-ysyx <tracer@ysyx.org>
2024-11-05 21:47:03 +0800

    > sim npc ysyx_23060198 林锦晖 Linux WIN-H8ULN2I4VKL 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:47:03 up 2 days, 10:19,  5 users,  load average: 1.10, 0.75, 0.82

--- npc/src/ssrc/main/riscv_soc/CPU/IDU/IDU.scala
+++ npc/src/ssrc/main/riscv_soc/CPU/IDU/IDU.scala
@@ -240,10 +240,7 @@ class ysyx_23060198_IDU extends Module{
         .toSeq
     val instList = rviInstList ++ rv32iInstList ++ rvsysInstList ++ rvzicsrInstList
 
-    val allField = Seq(Imm_Field, Bran_Field, EXUAsrc_Field, EXUBsrc_Field, EXUctr_Field, csr_ctr_Field, RegWr_Field, MemOp_Field)
-
-    // if(Config.DPIC_on) allField ++ Seq(PC_Field)
-    print(allField)
+    var allField = Seq(Imm_Field, Bran_Field, EXUAsrc_Field, EXUBsrc_Field, EXUctr_Field, csr_ctr_Field, RegWr_Field, MemOp_Field)
 
     val rvdecoderTable = new DecodeTable(instList, allField)
     val rvdecoderResult = rvdecoderTable.decode(io.IFU_2_IDU.bits.data)

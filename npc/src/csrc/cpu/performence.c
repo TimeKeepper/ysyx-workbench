4ceb86472ea8aa0a8e6188d11685b2161f6a3052
tracer-ysyx <tracer@ysyx.org>
2024-11-10 04:29:43 +0800

    > sim YSYXSOC ysyx_23060198 林锦晖 Linux WIN-H8ULN2I4VKL 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  04:29:43 up 1 day, 14:06,  5 users,  load average: 2.36, 1.02, 0.61

--- npc/src/csrc/cpu/performence.c
+++ npc/src/csrc/cpu/performence.c
@@ -13,7 +13,7 @@ uint64_t inst_cnt = 0;
 uint64_t num_of_inst_to_end = 0;
 
 extern "C" void IFU_finished(uint32_t cache_hit, uint32_t tag, uint32_t cache_tag) {
-    // if(cache_hit) Log("IFU cache hit");
+    if(cache_hit) Log("IFU cache hit");
     // Log("tag: 0x%08x, cache_tag: 0x%08x", tag, cache_tag);
     IFU_pc++;
 }

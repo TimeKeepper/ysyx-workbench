4c2e4b5cbd0d29c16009064bba80cb73b7c9bc32
tracer-ysyx <tracer@ysyx.org>
2024-11-10 06:24:02 +0800

    > sim npc ysyx_23060198 林锦晖 Linux WIN-H8ULN2I4VKL 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  06:24:02 up 1 day, 16:00,  6 users,  load average: 1.83, 1.47, 1.23

--- npc/src/ssrc/main/riscv_soc/CPU/IFU/IFU.scala
+++ npc/src/ssrc/main/riscv_soc/CPU/IFU/IFU.scala
@@ -90,11 +90,18 @@ class ysyx_23060198_IFU(idBits: Int)(implicit p: Parameters) extends LazyModule 
 
         val icache = Mem(16, UInt(52.W))
 
-        val index = io.REG_2_IFU.Next_PC(5, 2)
-        val tag = io.REG_2_IFU.Next_PC(31, 6)
-        val cache_tag = icache(index)(50, 32)
-        val data = icache(index)(31, 0)
-        val tag_hit = tag === Cat("b1010000".U(7.W), cache_tag)
+        def offsetWidth = 2
+        def indexWidth = 4
+        def dataWidth = Math.pow(2, offsetWidth).toInt * 8
+        def offsetPos = offsetWidth - 1
+        def indexPos = offsetWidth + indexWidth - 1
+        def dataPos = dataWidth - 1
+
+        val index = io.REG_2_IFU.Next_PC(indexPos, offsetPos + 1)
+        val tag = io.REG_2_IFU.Next_PC(31, indexPos + 1)
+        val cache_tag = icache(index)(50, dataPos + 1)
+        val data = icache(index)(dataPos, 0)
+        val tag_hit = tag === Cat("b1000000".U(7.W), cache_tag)
         val valid = icache(index)(51)
         val cache_hit = valid && tag_hit
         

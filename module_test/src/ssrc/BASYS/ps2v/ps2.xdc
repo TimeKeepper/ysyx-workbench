2c455c1812240674d1401a450d1c7ab9c87a2eff
tracer-ysyx <tracer@ysyx.org>
2024-11-11 16:51:16 +0800

    > sim YSYXSOC ysyx_23060198 林锦晖 Linux WIN-H8ULN2I4VKL 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:51:16 up 3 days,  3:17,  6 users,  load average: 0.35, 0.37, 0.45

--- module_test/src/ssrc/BASYS/ps2v/ps2.xdc
+++
@@ -1,4 +0,0 @@
-set_property IOSTANDARD LVCMOS33 [get_ports ps2_clk]
-set_property PACKAGE_PIN C17 [get_ports ps2_clk]
-set_property IOSTANDARD LVCMOS33 [get_ports ps2_data]
-set_property PACKAGE_PIN B17 [get_ports ps2_data]
--- module_test/src/ssrc/Elaborate.scala
+++ module_test/src/ssrc/Elaborate.scala
@@ -3,7 +3,7 @@ import homework._
 object Elaborate extends App {
   val firtoolOptions = Array(
     "--lowering-options=" + List(
-      // make yosys happy
+      // make vivado happy
       // see https://github.com/llvm/circt/blob/main/docs/VerilogGeneration.md
       "mitigateVivadoArrayIndexConstPropBug",
       "locationInfoStyle=wrapInAtSquareBracket"

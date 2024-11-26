53c1936090fbff5c69beb0ed1fd6aabea9471bf4
tracer-ysyx <tracer@ysyx.org>
2024-11-27 02:55:43 +0800

    > sim npc ysyx_23060198 林锦晖 Linux WIN-H8ULN2I4VKL 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  02:55:43 up 5 days,  4:58,  5 users,  load average: 0.48, 0.76, 0.64

--- npc/src/csrc/main.cpp
+++ npc/src/csrc/main.cpp
@@ -2,9 +2,12 @@
 #include <main.hpp>
 
 int main(int argc, char **argv) {
-  init_monitor(argc, argv);
 
-  engine_start(argc, argv);
+  Log("REBUILD");
 
-  return is_exit_status_bad();
+  // init_monitor(argc, argv);
+
+  // engine_start(argc, argv);
+
+  // return is_exit_status_bad();
 }

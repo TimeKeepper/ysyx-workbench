74e5233321e48a4a29150cf1f17ead8e7bb48c26
tracer-ysyx <tracer@ysyx.org>
2024-11-27 08:56:19 +0800

    > sim YSYXSOC ysyx_23060198 林锦晖 Linux WIN-H8ULN2I4VKL 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  08:56:19 up 5 days, 11:30,  6 users,  load average: 0.57, 0.43, 0.58

--- npc/include/utils.hpp
+++ npc/include/utils.hpp
@@ -37,15 +37,15 @@ typedef struct {
 
 #include <iostream>
 #include <cstdio>
-
 #define _Log(...) \
   do { \
     printf(__VA_ARGS__); \
   } while (0)
 
 #define Log(format, ...) \
-  _Log(ANSI_FMT(format, ANSI_FG_BLUE) "\n", ## __VA_ARGS__)
-
+    _Log(ANSI_FMT("[%s:%d %s] " format, ANSI_FG_BLUE) "\n", \
+        __FILE__, __LINE__, __func__, ## __VA_ARGS__)
+        
 #include <cassert>
 #define Assert(cond, format, ...) \
   do { \

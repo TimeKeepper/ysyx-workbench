5d295d122cfa9c5196c7d531117681c85e2c578c
tracer-ysyx <tracer@ysyx.org>
2024-11-27 09:06:45 +0800

    > sim YSYXSOC ysyx_23060198 林锦晖 Linux WIN-H8ULN2I4VKL 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  09:06:45 up 5 days, 11:42,  6 users,  load average: 0.73, 0.47, 0.51

--- npc/src/csrc/emulator.cpp
+++ npc/src/csrc/emulator.cpp
@@ -138,7 +138,7 @@ void Emulator::load_image() {
 }
 
 static void welcome() {
-    std::cout << "Welcome to" << ANSI_FMT("riscv32e", ANSI_FG_YELLOW) << "npc" << std::endl;
+    std::cout << "Welcome to " << ANSI_FMT("riscv32e", ANSI_FG_YELLOW) << "-npc" << std::endl;
 }
 
 Emulator::Emulator(int argc, char **argv) : argc(argc), argv{argv} {

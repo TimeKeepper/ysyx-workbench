a175ee8d8e5cd6841a59a7af220b4c5d641eee05
tracer-ysyx <tracer@ysyx.org>
2024-11-28 12:42:03 +0800

    > sim YSYXSOC ysyx_23060198 林锦晖 Linux WIN-H8ULN2I4VKL 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  12:42:03 up 6 days, 16:51,  6 users,  load average: 1.17, 0.68, 0.41

--- npc/src/csrc/main.cpp
+++ npc/src/csrc/main.cpp
@@ -8,7 +8,6 @@
 std::unique_ptr<Emulator> emulator;
 
 void SDL_handle(int SIGNAL){
-  Log("state: %d", emulator->npc_state.state);
   if(SIGNAL == SIGINT && emulator->npc_state.state == NPC_RUNNING){
     std::cout << ANSI_FG_CYAN << "Ctrl+C detected, stopping Emulator..." << ANSI_NONE << std::endl;
     emulator->npc_state.state = NPC_STOP;
@@ -19,13 +18,11 @@ int main(int argc, char **argv) {
   Log(ANSI_FMT("REBUILD", ANSI_FG_GREEN));
 
   emulator = std::make_unique<Emulator>(argc, argv);
-  Log("state: %d", emulator->npc_state.state);
 
   signal(SIGINT, SDL_handle);
 
   std::unique_ptr<simple_debugger> sdb = std::make_unique<simple_debugger>(emulator.get());
 
-  Log("state: %d", emulator->npc_state.state);
   sdb->main_loop();
 
   // init_monitor(argc, argv);

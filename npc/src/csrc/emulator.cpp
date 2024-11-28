92f538ddb1a3c91d7cd299dd7785a12b7c08d58d
tracer-ysyx <tracer@ysyx.org>
2024-11-28 13:14:03 +0800

    > sim YSYXSOC ysyx_23060198 林锦晖 Linux WIN-H8ULN2I4VKL 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  13:14:03 up 6 days, 17:25,  6 users,  load average: 1.00, 0.83, 0.61

--- npc/src/csrc/emulator.cpp
+++ npc/src/csrc/emulator.cpp
@@ -207,6 +207,7 @@ void Emulator::cycle(uint64_t n) {
         if(this->wave_trace_on) wave_trace_once();  
 
         #ifdef CONFIG_NVBOARD
+        Log("nvboard update");
         nvboard_update();
         #endif
 

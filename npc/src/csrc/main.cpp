3a4af51786c2cf3d0e946f36a9ce5c4c6e2b79b9
tracer-ysyx <tracer@ysyx.org>
2024-11-28 12:38:04 +0800

    > sim YSYXSOC ysyx_23060198 林锦晖 Linux WIN-H8ULN2I4VKL 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  12:38:04 up 6 days, 16:47,  6 users,  load average: 0.63, 0.40, 0.28

--- npc/src/csrc/main.cpp
+++ npc/src/csrc/main.cpp
@@ -19,11 +19,13 @@ int main(int argc, char **argv) {
   Log(ANSI_FMT("REBUILD", ANSI_FG_GREEN));
 
   emulator = std::make_unique<Emulator>(argc, argv);
+  Log("state: %d", emulator->npc_state.state);
 
   signal(SIGINT, SDL_handle);
 
   std::unique_ptr<simple_debugger> sdb = std::make_unique<simple_debugger>(emulator.get());
 
+  Log("state: %d", emulator->npc_state.state);
   sdb->main_loop();
 
   // init_monitor(argc, argv);

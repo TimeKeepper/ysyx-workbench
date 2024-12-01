fcd3dbf65df599c4d94b8e4d5a25e7fb70ac958a
tracer-ysyx <tracer@ysyx.org>
2024-12-01 13:36:39 +0800

    > sim YSYXSOC ysyx_23060198 林锦晖 Linux WIN-H8ULN2I4VKL 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  13:36:39 up  4:20,  5 users,  load average: 0.80, 0.77, 0.74

--- npc/src/csrc/disasm.cpp
+++ npc/src/csrc/disasm.cpp
@@ -89,8 +89,8 @@ void init_disasm(const char *triple) {
   #endif
 }
 
-#ifdef CONFIG_ITRACE
 void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte) {
+#ifdef CONFIG_ITRACE
   MCInst inst;
   llvm::ArrayRef<uint8_t> arr(code, nbyte);
   uint64_t dummy_size = 0;
@@ -104,5 +104,7 @@ void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte) {
   const char *p = s.c_str() + skip;
   assert((int)s.length() - skip < size);
   strcpy(str, p);
-}
+#else
+  Log("Instruction Trace Not Available");
 #endif
+}
--- npc/src/csrc/emulator.cpp
+++ npc/src/csrc/emulator.cpp
@@ -248,8 +248,6 @@ void Emulator::Emulator_trap(uint32_t a0) {
         ANSI_FMT("Hit bad trap",  ANSI_FG_RED)) << std::endl;
 }
 
-void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte);
-
 void Emulator::IFU_catch(uint32_t inst){
     std::stringstream ss;
     ss << ANSI_FG_CYAN << "0x" << std::hex << std::nouppercase << cpu.pc << ANSI_NONE;
@@ -257,9 +255,14 @@ void Emulator::IFU_catch(uint32_t inst){
 
     char inst_str[64];
 
-    disassemble(inst_str, 64, this->cpu.pc, (uint8_t*)&inst, 4);
+    #ifdef CONFIG_ITRACE
     
+    void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte);
+
+    disassemble(inst_str, 64, this->cpu.pc, (uint8_t*)&inst, 4);
     std::cout << ss.str() << '\t' << ANSI_FG_BLUE << inst_str << ANSI_NONE << std::endl;
+    
+    #endif
 }
 
 void Emulator::WBU_catch(uint32_t next_pc, \

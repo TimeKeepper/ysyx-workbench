3b7e0194429e260709d9f8a05b385e3f5dbd3aa2
tracer-ysyx <tracer@ysyx.org>
2024-12-01 18:56:13 +0800

    > sim YSYXSOC ysyx_23060198 林锦晖 Linux WIN-H8ULN2I4VKL 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:56:13 up  9:40,  5 users,  load average: 1.27, 0.89, 0.66

--- npc/src/csrc/simple_debugger.cpp
+++ npc/src/csrc/simple_debugger.cpp
@@ -1,3 +1,4 @@
+#include <numeric>
 #include <simple_debugger.hpp>
 
 #include <readline/readline.h>
@@ -184,7 +185,9 @@ simple_debugger::simple_debugger(Emulator* emulator) : emulator(emulator) {
     cmds.push_back(
         {"expr", "Evaluate an expression", "expr <expr>", \
         [&](std::vector<std::string> args) {
-            std::cout << ANSI_FG_CYAN << "Result" << ANSI_NONE << "\t:" << ANSI_FG_BLUE << expr(args[0]) << ANSI_NONE << std::endl;
+            // 将所有args合成一个string传入函数
+            std::string expr_str = std::accumulate(args.begin(), args.end(), std::string(""));
+            std::cout << ANSI_FG_CYAN << "Result" << ANSI_NONE << "\t:" << ANSI_FG_BLUE << expr(expr_str) << ANSI_NONE << std::endl;
             return 0;
         }});
 

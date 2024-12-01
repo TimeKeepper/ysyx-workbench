40b2c0f963d76fc10df231d0a4083377c17b18df
tracer-ysyx <tracer@ysyx.org>
2024-12-01 19:36:46 +0800

    > sim YSYXSOC ysyx_23060198 林锦晖 Linux WIN-H8ULN2I4VKL 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:36:46 up 10:21,  5 users,  load average: 1.35, 1.14, 1.81

--- npc/src/csrc/expr.cpp
+++ npc/src/csrc/expr.cpp
@@ -1,8 +1,14 @@
 #include <muParser.h>
+#include <muParserDLL.h>
 #include <utils.hpp>
 
 std::string expr(std::string expr) {
     mu::Parser p;
+
+    p.DefineInfixOprt("0x", [](double tar) { 
+        return static_cast<double>(std::stoi(std::to_string(static_cast<int>(tar)), nullptr, 16)); 
+    });
+
     p.SetExpr(expr);
     return std::to_string(static_cast<int>(p.Eval()));
 }

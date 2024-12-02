05fe8d9c8b1774e8c9af135283da6256b1367e83
tracer-ysyx <tracer@ysyx.org>
2024-12-02 09:00:42 +0800

    > sim YSYXSOC ysyx_23060198 林锦晖 Linux WIN-H8ULN2I4VKL 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  09:00:42 up 23:44,  5 users,  load average: 0.58, 0.36, 0.45

--- npc/src/csrc/expr.cpp
+++ npc/src/csrc/expr.cpp
@@ -1,5 +1,20 @@
 #include <exprtk.hpp>
 
-std::string expr(std::string expr){
-    return expr;
+std::string expr(std::string expr){    
+    typedef exprtk::symbol_table<double> symbol_table_t;
+    typedef exprtk::expression<double> expression_t;
+    typedef exprtk::parser<double> parser_t;
+    
+    symbol_table_t symbol_table;
+    expression_t expression;
+    parser_t parser;
+    
+    expression.register_symbol_table(symbol_table);
+    
+    if (!parser.compile(expr, expression)) {
+        return "Error: " + std::string(parser.error().c_str());
+    }
+    
+    double result = expression.value();
+    return std::to_string(result);
 }

8dc7b71fc50a87de980ab3971f29094d5676a765
tracer-ysyx <tracer@ysyx.org>
2024-12-02 08:47:30 +0800

    > sim YSYXSOC ysyx_23060198 林锦晖 Linux WIN-H8ULN2I4VKL 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  08:47:30 up 23:31,  5 users,  load average: 0.64, 0.62, 0.66

--- npc/src/csrc/expr.cpp
+++ npc/src/csrc/expr.cpp
@@ -1,16 +1,5 @@
-#include <muParser.h>
-#include <muParserDLL.h>
-#include <utils.hpp>
+#include <exprtk.hpp>
 
-double prefix_hex(double tar) {
-    return static_cast<double>(std::stoi(std::to_string(static_cast<int>(tar)), nullptr, 16));
-}
-
-std::string expr(std::string expr) {
-    mu::Parser p;
-
-    p.DefineFun("0x", prefix_hex);
-
-    p.SetExpr(expr);
-    return std::to_string(static_cast<int>(p.Eval()));
+std::string expr(std::string expr){
+    return expr;
 }

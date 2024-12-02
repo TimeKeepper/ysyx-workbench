9d20144f8a42d4eb2e764367198cd7d0c620ac45
tracer-ysyx <tracer@ysyx.org>
2024-12-02 14:01:34 +0800

    > sim YSYXSOC ysyx_23060198 林锦晖 Linux WIN-H8ULN2I4VKL 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:01:34 up 1 day,  4:45,  5 users,  load average: 0.88, 0.52, 0.41

--- npc/src/csrc/expr.cpp
+++ npc/src/csrc/expr.cpp
@@ -11,8 +11,6 @@ std::vector<Expr::Token> Expr::get_tokens(std::string expr){
         for(auto pattern : token_patterns){
             std::smatch match;
             if(std::regex_search(expr, match, pattern.second)){
-                std::cout << match.str() << std::endl;
-                std::cout << pattern.first << std::endl;
                 tokens.push_back(Token(pattern.first, match.str()));
                 expr = match.suffix();
                 break;

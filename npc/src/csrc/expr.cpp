c17f74466815ce629ff1ffe7dafcd92cf207466a
tracer-ysyx <tracer@ysyx.org>
2024-12-04 04:05:14 +0800

    > sim YSYXSOC ysyx_23060198 林锦晖 Linux WIN-H8ULN2I4VKL 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  04:05:14 up 21 min,  3 users,  load average: 0.08, 0.07, 0.08

--- npc/src/csrc/expr.cpp
+++ npc/src/csrc/expr.cpp
@@ -117,8 +117,6 @@ std::string Expr::eval(std::string expr){
     std::vector<Token> tokens = get_tokens(expr);
     if(!expr_valid(tokens)) return "Invalid expression";
 
-    Log("Valid expression");
-
     std::queue<Token> Rpn = RPN(tokens);
 
     if(Rpn.empty()) return "";

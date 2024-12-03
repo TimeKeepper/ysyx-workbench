fa279dfd3b778f8a711ab6884ab06b440dd0f681
tracer-ysyx <tracer@ysyx.org>
2024-12-04 07:12:57 +0800

    > sim YSYXSOC ysyx_23060198 林锦晖 Linux WIN-H8ULN2I4VKL 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  07:12:57 up  3:45,  4 users,  load average: 0.92, 0.47, 0.46

--- npc/src/csrc/simple_debugger.cpp
+++ npc/src/csrc/simple_debugger.cpp
@@ -216,6 +216,13 @@ simple_debugger::simple_debugger(Emulator* emulator) : emulator(emulator) {
         }
     });
 
+    cmds.push_back(
+        {"test", "Test the expr", "test", \
+        [&](std::vector<std::string> args){
+            this->expr->test();
+            return 0;
+        }});
+
     cmds.push_back({
         "func", "Control Debug Function ON/OFF", "func <func> on/off", \
         [&](std::vector<std::string> args){
@@ -249,13 +256,6 @@ simple_debugger::simple_debugger(Emulator* emulator) : emulator(emulator) {
             return 0;
         }
     });
-
-    cmds.push_back(
-        {"test", "Test the expr", "test", \
-        [&](std::vector<std::string> args){
-            this->expr->test();
-            return 0;
-        }});
 
     this->expr = std::make_unique<Expr>();
     this->wpm = std::make_unique<Watch_Point_Manager>(this->expr.get());

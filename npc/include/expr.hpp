10f11d9dc3901e518029d2bc706349810a71c02b
tracer-ysyx <tracer@ysyx.org>
2024-12-04 04:56:13 +0800

    > sim YSYXSOC ysyx_23060198 林锦晖 Linux WIN-H8ULN2I4VKL 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  04:56:13 up  1:17,  4 users,  load average: 0.47, 0.31, 0.32

--- npc/include/expr.hpp
+++ npc/include/expr.hpp
@@ -55,7 +55,8 @@ class Expr {
         Token(TokenType type, const std::string& value) : type(type), value(value) {};
         uint32_t get_val() {
             switch(this->type) {
-                case TokenType::REGISTER:   return emulator->cpu.gpr[gpr_name2id(this->value.substr(1))];
+                case TokenType::REGISTER:   if(this->value.substr(1) == "pc") return emulator->cpu.pc;
+                                            return emulator->cpu.gpr[gpr_name2id(this->value.substr(1))];
                 case TokenType::HEX:        return std::stoul(this->value, nullptr, 16);
                 case TokenType::DECIMAL:    return std::stoul(this->value);
                 default: return 0;

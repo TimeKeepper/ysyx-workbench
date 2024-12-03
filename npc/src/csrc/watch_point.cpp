314917d58b023316330a573224103d7221ed4a04
tracer-ysyx <tracer@ysyx.org>
2024-12-04 07:02:36 +0800

    > sim YSYXSOC ysyx_23060198 林锦晖 Linux WIN-H8ULN2I4VKL 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  07:02:36 up  3:33,  4 users,  load average: 0.19, 0.40, 0.44

--- npc/src/csrc/watch_point.cpp
+++ npc/src/csrc/watch_point.cpp
@@ -21,10 +21,10 @@ bool Watch_Point::check(){
 Watch_Point_Manager::Watch_Point_Manager(Expr* expr_parser) : expr_parser(expr_parser) {}
 
 bool Watch_Point_Manager::add_watch_point(std::string expr){
-    Log("expr %s", expr.c_str());
     if(this->expr_parser->eval(expr) == "Invalid expression") return false;
 
     this->watch_points.emplace_back(expr, this->expr_parser);
+    Log("expr %s", expr.c_str());
 
     return true;
 }

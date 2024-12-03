edd0a78bc2c24a20127c39d4abed033f7c0b3522
tracer-ysyx <tracer@ysyx.org>
2024-12-03 15:50:26 +0800

    > sim YSYXSOC ysyx_23060198 林锦晖 Linux WIN-H8ULN2I4VKL 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:50:26 up  1:15,  3 users,  load average: 0.25, 0.20, 0.21

--- npc/src/csrc/expr.cpp
+++ npc/src/csrc/expr.cpp
@@ -41,11 +41,6 @@ std::vector<Expr::Token> Expr::RPN(std::vector<Token> tokens){
 std::string Expr::eval(std::string expr){
     std::vector<Token> tokens = RPN(get_tokens(expr));
 
-    std::cout << "TokensType: ";
-    for(auto token : tokens){
-        std::cout << std::to_string(static_cast<int>(token.type)) << std::endl;
-    }
-
     std::vector<uint32_t> stack;
     for(auto token : tokens){
         if(token.type == TokenType::DECIMAL || token.type == TokenType::HEX){
@@ -60,5 +55,7 @@ std::string Expr::eval(std::string expr){
         }
     }
 
-    return std::to_string(stack.back());
+    return std::to_string(std::accumulate(stack.begin(), stack.end(), 0));
+
+    // return std::to_string(stack.back());
 }

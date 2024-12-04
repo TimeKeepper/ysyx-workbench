8e0acbf5e71da0d05d38f88d52049653106b4bd7
tracer-ysyx <tracer@ysyx.org>
2024-12-04 11:20:10 +0800

    > sim YSYXSOC ysyx_23060198 林锦晖 Linux WIN-H8ULN2I4VKL 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:20:10 up 6 min,  3 users,  load average: 0.62, 0.60, 0.31

--- npc/src/csrc/watch_point.cpp
+++ npc/src/csrc/watch_point.cpp
@@ -12,6 +12,7 @@ Watch_Point::Watch_Point(std::string expr, Expr* expr_parser) : expr(expr), expr
 bool Watch_Point::check(){
     Log("watch_point check");
     Log("%d, %lu", this->value, std::stoul(expr_parser->eval(expr)));
+    std::cout << "Expr:" << expr << std::endl;
     if(this->value != std::stoul(expr_parser->eval(expr))){
         this->value = std::stoul(expr_parser->eval(expr));
         return true;

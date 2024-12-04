7b2a6f5f3eca3d8fe5ace5b580af14aed84fa47c
tracer-ysyx <tracer@ysyx.org>
2024-12-04 13:03:28 +0800

    > sim YSYXSOC ysyx_23060198 林锦晖 Linux WIN-H8ULN2I4VKL 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  13:03:28 up  1:52,  3 users,  load average: 0.30, 0.16, 0.17

--- npc/src/csrc/simple_debugger.cpp
+++ npc/src/csrc/simple_debugger.cpp
@@ -330,7 +330,7 @@ void simple_debugger::main_loop() {
 }
 
 void simple_debugger::inst_comp(void) {
-    if(this->wpm->check_watch_points()){
+    if(this->wpm->check_watch_points() || this->wpm->check_break_points()){
         this->emulator->npc_state.state = NPC_STOP;
     }
 }
--- npc/src/csrc/watch_point.cpp
+++ npc/src/csrc/watch_point.cpp
@@ -18,6 +18,19 @@ bool Watch_Point::check(){
     return false;
 }
 
+Break_Point::Break_Point(std::string addr, Expr* expr_parser) : addr(addr), expr_parser(expr_parser) {
+    if(this->expr_parser->eval(addr) == "Invalid expression") {
+        this->value = 0;
+        return;
+    }
+
+    this->value = std::stoul(expr_parser->eval(addr), 0, 16);
+}
+
+bool Break_Point::check(){
+    return this->value == std::stoul(this->expr_parser->eval(this->addr));
+}
+
 Watch_Point_Manager::Watch_Point_Manager(Expr* expr_parser) : expr_parser(expr_parser) {}
 
 bool Watch_Point_Manager::add_watch_point(std::string expr){
@@ -28,10 +41,26 @@ bool Watch_Point_Manager::add_watch_point(std::string expr){
     return true;
 }
 
+bool Watch_Point_Manager::add_break_point(std::string addr){
+    if(this->expr_parser->eval(addr) == "Invalid expression") return false;
+
+    this->break_points.emplace_back(addr, this->expr_parser);
+
+    return true;
+}
+
 bool Watch_Point_Manager::delete_watch_point(uint32_t index){
     if(index >= this->watch_points.size()) return false;
 
     this->watch_points.erase(this->watch_points.begin() + index);
+
+    return true;
+}
+
+bool Watch_Point_Manager::delete_break_point(uint32_t index){
+    if(index >= this->break_points.size()) return false;
+
+    this->break_points.erase(this->break_points.begin() + index);
 
     return true;
 }
@@ -48,8 +77,26 @@ uint32_t Watch_Point_Manager::check_watch_points(){
     return hit;
 }
 
+uint32_t Watch_Point_Manager::check_break_points(){
+    uint32_t hit = 0;
+    for(auto &bp : this->break_points){
+        if(bp.check()){
+            std::cout << ANSI_FG_CYAN << "Breakpoint triggered: " << ANSI_NONE << bp.addr << std::endl;
+            hit++;
+        }
+    }
+
+    return hit;
+}
+
 void Watch_Point_Manager::print_watch_points(){
     for(int i = 0; i < this->watch_points.size(); i++){
         std::cout << ANSI_FG_CYAN << "Watchpoint " << i << ANSI_NONE << "\t: " << ANSI_FG_BLUE << this->watch_points[i].expr << ANSI_NONE << std::endl;
+    }
+}
+
+void Watch_Point_Manager::print_break_points(){
+    for(int i = 0; i < this->break_points.size(); i++){
+        std::cout << ANSI_FG_CYAN << "Breakpoint " << i << ANSI_NONE << "\t: " << ANSI_FG_BLUE << this->break_points[i].addr << ANSI_NONE << std::endl;
     }
 }

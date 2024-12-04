abca5d0b07ed379bf0544687c3c413b16b87e1ca
wenjiu <2784307979@qq.com>
2024-12-04 13:18:08 +0800

    watch point

--- npc/include/watch_point.hpp
+++ npc/include/watch_point.hpp
@@ -14,17 +14,32 @@ class Watch_Point {
         bool check();
 };
 
+class Break_Point {
+    public:
+        uint32_t addr;
+        Expr* expr_parser;
+
+        Break_Point(std::string addr_str, Expr* expr_parser);
+        bool check();
+};
+
 class Watch_Point_Manager {
     public:
         std::vector<Watch_Point> watch_points;
+        std::vector<Break_Point> break_points;
         Expr* expr_parser;
 
         Watch_Point_Manager(Expr* expr_parser);
+
         bool add_watch_point(std::string expr);
+        bool add_break_point(std::string addr);
         bool delete_watch_point(uint32_t index);
+        bool delete_break_point(uint32_t index);
         uint32_t check_watch_points();
+        uint32_t check_break_points();
 
         void print_watch_points();
+        void print_break_points();
 };
 
 #endif
--- npc/src/csrc/watch_point.cpp
+++ npc/src/csrc/watch_point.cpp
@@ -1,4 +1,5 @@
 #include <watch_point.hpp>
+#include <iomanip>
 
 Watch_Point::Watch_Point(std::string expr, Expr* expr_parser) : expr(expr), expr_parser(expr_parser) {
     if(this->expr_parser->eval(expr) == "Invalid expression") {
@@ -6,16 +7,29 @@ Watch_Point::Watch_Point(std::string expr, Expr* expr_parser) : expr(expr), expr
         return;
     }
 
-    this->value = std::stoul(expr_parser->eval(expr));
+    this->value = std::stoul(expr_parser->eval(expr), 0, 16);
 }
 
 bool Watch_Point::check(){
-    if(this->value != std::stoul(expr_parser->eval(expr))){
-        this->value = std::stoul(expr_parser->eval(expr));
+    if(this->value != std::stoul(expr_parser->eval(expr), 0, 16)){
+        this->value = std::stoul(expr_parser->eval(expr), 0, 16);
         return true;
     }
 
     return false;
+}
+
+Break_Point::Break_Point(std::string addr_str, Expr* expr_parser) : expr_parser(expr_parser) {
+    if(this->expr_parser->eval(addr_str) == "Invalid expression") {
+        this->addr = 0;
+        return;
+    }
+
+    this->addr = std::stoul(expr_parser->eval(addr_str), 0, 16);
+}
+
+bool Break_Point::check(){
+    return this->addr == std::stoul(this->expr_parser->eval("$pc"), 0, 16);
 }
 
 Watch_Point_Manager::Watch_Point_Manager(Expr* expr_parser) : expr_parser(expr_parser) {}
@@ -28,10 +42,26 @@ bool Watch_Point_Manager::add_watch_point(std::string expr){
     return true;
 }
 
+bool Watch_Point_Manager::add_break_point(std::string addr){
+    if(this->expr_parser->eval(addr) == "Invalid expression") return false;
+
+    this->break_points.emplace_back(addr, this->expr_parser);
+
+    return true;
+}
+
 bool Watch_Point_Manager::delete_watch_point(uint32_t index){
     if(index >= this->watch_points.size()) return false;
 
     this->watch_points.erase(this->watch_points.begin() + index);
+
+    return true;
+}
+
+bool Watch_Point_Manager::delete_break_point(uint32_t index){
+    if(index >= this->break_points.size()) return false;
+
+    this->break_points.erase(this->break_points.begin() + index);
 
     return true;
 }
@@ -48,8 +78,26 @@ uint32_t Watch_Point_Manager::check_watch_points(){
     return hit;
 }
 
+uint32_t Watch_Point_Manager::check_break_points(){
+    uint32_t hit = 0;
+    for(auto &bp : this->break_points){
+        if(bp.check()){
+            std::cout << ANSI_FG_CYAN << "Breakpoint triggered: " << ANSI_NONE << std::hex << "0x" << std::setw(8) << std::setfill('0') << bp.addr << std::endl;
+            hit++;
+        }
+    }
+
+    return hit;
+}
+
 void Watch_Point_Manager::print_watch_points(){
     for(int i = 0; i < this->watch_points.size(); i++){
         std::cout << ANSI_FG_CYAN << "Watchpoint " << i << ANSI_NONE << "\t: " << ANSI_FG_BLUE << this->watch_points[i].expr << ANSI_NONE << std::endl;
+    }
+}
+
+void Watch_Point_Manager::print_break_points(){
+    for(int i = 0; i < this->break_points.size(); i++){
+        std::cout << ANSI_FG_CYAN << "Breakpoint " << i << ANSI_NONE << "\t: " << ANSI_FG_BLUE << this->break_points[i].addr << ANSI_NONE << std::endl;
     }
 }

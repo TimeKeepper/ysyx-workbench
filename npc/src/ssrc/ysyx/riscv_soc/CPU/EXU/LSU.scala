0a9e0fd6b3b4e834d627445e55892b3ff25bf65f
tracer-ysyx <tracer@ysyx.org>
2024-12-05 12:07:15 +0800

    > sim YSYXSOC ysyx_23060198 林锦晖 Linux WIN-H8ULN2I4VKL 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  12:07:15 up 1 day,  2:02,  3 users,  load average: 1.55, 0.67, 0.46

--- npc/src/ssrc/ysyx/riscv_soc/CPU/EXU/LSU.scala
+++ npc/src/ssrc/ysyx/riscv_soc/CPU/EXU/LSU.scala
@@ -208,9 +208,11 @@ class LSU extends Module{
     }
 
     if(Config.Simulate){
-        val diff_mis_map =  AddressSet.misaligned(0x10002000, 0x10)
+        val diff_mis_map =  AddressSet.misaligned(0x10000000, 0x1000) ++
+                            AddressSet.misaligned(0x10002000, 0x10) ++
+                            AddressSet.misaligned(0x10011000, 0x8)
         val Catch = Module(new LSU_catch)
         Catch.io.LS := io.AXI.ar.valid || io.AXI.aw.valid
-        Catch.io.diff_skip := true.B
+        Catch.io.diff_skip := diff_mis_map.map(_.contains(RegEnable(addr, io.IDU_2_EXU.fire))).reduce(_ || _)
     }
 }

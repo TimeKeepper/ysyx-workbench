516fa4cafcfaff46f6bead497cd07219d155ee11
tracer-ysyx <tracer@ysyx.org>
2024-12-08 04:23:13 +0800

    > sim YSYXSOC ysyx_23060198 林锦晖 Linux WIN-H8ULN2I4VKL 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  04:23:13 up  4:53,  4 users,  load average: 0.39, 0.69, 0.52

--- npc/platform/Elaborate.scala
+++ npc/platform/Elaborate.scala
@@ -18,10 +18,10 @@ object Elaborateysyxsoc extends App {
   Config.setDPIC(false)
   Config.setSimulate(true)
   Config.setIcacheParam(2, 4, 19, "ha0000000")
-  Config.setDiffMisMap(AddressSet.misaligned(0x10000000, 0x1000) ++
-                            AddressSet.misaligned(0x10002000, 0x10) ++
-                            AddressSet.misaligned(0x10011000, 0x8) ++
-                            AddressSet.misaligned(0x02000000L, 0x10000))
+  Config.setDiffMisMap( AddressSet.misaligned(0x10000000, 0x1000) ++
+                        AddressSet.misaligned(0x10002000, 0x10) ++
+                        AddressSet.misaligned(0x10011000, 0x8) ++
+                        AddressSet.misaligned(0x02000000L, 0x10000))
 
   circt.stage.ChiselStage.emitSystemVerilogFile(gen = new riscv_cpu.ysyx_23060198(), args = args, firtoolOpts  = firtoolOptions)
 }
@@ -43,6 +43,7 @@ object Elaboratenpc extends App {
   Config.setDPIC(false)
   Config.setSimulate(true)
   Config.setIcacheParam(2, 4, 19, "h80000000")
+  Config.setDiffMisMap(AddressSet.misaligned(0x10000000, 0x1000))
 
   circt.stage.ChiselStage.emitSystemVerilogFile(new riscv_cpu.top(), args, firtoolOptions)
 }

e9e4fecdd17ffd53e90cf640d51b2386f4c424bf
tracer-ysyx <tracer@ysyx.org>
2024-12-08 02:38:02 +0800

    > sim npc ysyx_23060198 林锦晖 Linux WIN-H8ULN2I4VKL 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  02:38:02 up  3:04,  3 users,  load average: 0.46, 0.22, 0.27

--- npc/src/csrc/dpic.cpp
+++ npc/src/csrc/dpic.cpp
@@ -76,13 +76,10 @@ extern "C" {
 #endif
 
     extern void IFU_catch(uint32_t inst){
-        Log("IFU catch");
         emulator->IFU_catch(inst);
     }
 
     extern void IDU_catch(uint32_t type){
-        Log("IDU catch");
-
         performence::Inst_Type inst_type;
         switch(type){
             case 0: inst_type = performence::Inst_Type::Cal; break;
@@ -94,14 +91,10 @@ extern "C" {
     }
 
     extern void ALU_catch(){
-        Log("ALU catch");
-
         emulator->ALU_catch();
     }
 
     extern void LSU_catch(uint32_t diff_skip){
-        Log("LSU catch");
-
         emulator->LSU_catch(diff_skip);
     }
 
@@ -109,7 +102,6 @@ extern "C" {
     uint32_t gpr_waddr, uint32_t gpr_wdata, \
     uint32_t csr_wena, uint32_t csr_waddra, uint32_t csr_wdataa, \
     uint32_t csr_wenb, uint32_t csr_waddrb, uint32_t csr_wdatab){
-        Log("WBU catch");
         emulator->WBU_catch(next_pc, gpr_waddr, gpr_wdata, csr_wena, csr_waddra, csr_wdataa, csr_wenb, csr_waddrb, csr_wdatab);
         sdb->inst_comp();
     }

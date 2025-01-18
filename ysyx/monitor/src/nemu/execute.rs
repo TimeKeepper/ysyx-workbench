4b81fd149f7830b9bf4f4435eae9927eac763083
tracer-ysyx <tracer@ysyx.org>
2025-01-18 11:52:10 +0800

    > compile NEMU ysyx_23060198 林锦晖 Linux wenjiulaptop 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:52:10 up  1:27,  1 user,  load average: 0.12, 0.15, 0.11

--- ysyx/monitor/src/nemu/execute.rs
+++ ysyx/monitor/src/nemu/execute.rs
@@ -1,3 +1,5 @@
+use crate::mmu::Mask;
+
 use super::{ExecuteInst, Simulator};
 
 use super::super::simErr;
@@ -8,33 +10,45 @@ impl Simulator {
         let mut npc = self.cpu_state.pc.value + 4;
         let gpr = &mut self.cpu_state.gpr;
         let pc = &mut self.cpu_state.pc;
+
+        let rd = inst.rd as usize;
+        let rs1 = inst.rs1 as usize;
+        let rs2 = inst.rs2 as usize;
+        let imm = inst.imm;
         
         match name {
             "lui" => {
-                gpr[inst.rd as usize].value = inst.imm;
+                gpr[rd].value = inst.imm;
             },
             "auipc" => {
-                gpr[inst.rd as usize].value = pc.value + inst.imm;
+                gpr[rd].value = pc.value + inst.imm;
             },
 
             "jal" => {
-                gpr[inst.rd as usize].value = npc;
+                gpr[rd].value = npc;
                 npc = pc.value.wrapping_add(inst.imm);
             },
             "jalr" => {
-                gpr[inst.rd as usize].value = pc.value;
-                npc = gpr[inst.rs1 as usize].value.wrapping_add(inst.imm) & !1;
+                gpr[rd].value = pc.value;
+                npc = gpr[rs1].value.wrapping_add(inst.imm) & !1;
             },
 
             "sb" => {
+                self.mmu.write(gpr[rs1].value + imm, gpr[rs2].value, Mask::Byte)?
             },
+            "sh" => {
+                self.mmu.write(gpr[rs1].value + imm, gpr[rs2].value, Mask::Half)?
+            }
+            "sw" => {
+                self.mmu.write(gpr[rs1].value + imm, gpr[rs2].value, Mask::Word)?
+            }
 
             "addi" => {
-                gpr[inst.rd as usize].value = gpr[inst.rs1 as usize].value.wrapping_add(inst.imm);
+                gpr[rd].value = gpr[rs1].value.wrapping_add(inst.imm);
             },
             _ => return Err(simErr::UnknownInstruction { name: name.to_string() }),
         }
-        
+
         pc.value = npc;
 
         if self.inst_trace_buffer.0 {

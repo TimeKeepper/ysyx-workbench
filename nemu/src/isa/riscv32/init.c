1468fc042e6e67843bcbb43b311374af77c47363
tracer-ysyx <tracer@ysyx.org>
2025-01-24 03:06:22 +0800

    > compile NEMU ysyx_23060198 林锦晖 Linux wenjiulaptop 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  03:06:22 up 1 day,  7:23,  1 user,  load average: 0.37, 0.23, 0.12

--- nemu/src/isa/riscv32/init.c
+++ nemu/src/isa/riscv32/init.c
@@ -28,7 +28,7 @@ static const uint32_t img [] = {
 
 static void restart() {
   /* Set the initial program counter. */
-  cpu.pc = 0x80000000;
+  cpu.pc = RESET_VECTOR;
 
   /* Set the initial status register. */
   cpu.sr[ADDR_MSTATUS] = 0x1800;

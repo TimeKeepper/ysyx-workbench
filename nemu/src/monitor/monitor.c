c2dcd75341b07b30de6f347e176ebebebf76849c
tracer-ysyx <tracer@ysyx.org>
2025-01-25 22:54:08 +0800

    > compile NEMU ysyx_23060198 林锦晖 Linux wenjiulaptop 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:54:07 up 1 day, 16:07,  1 user,  load average: 0.09, 0.25, 0.44

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -230,13 +230,13 @@ void init_monitor(int argc, char *argv[]) {
   /* Initialize devices. */
   IFDEF(CONFIG_DEVICE, init_device());
 
+  Log("debug");
   /* Perform ISA dependent initialization. */
   init_isa();
 
   /* Load the image to memory. This will overwrite the built-in image. */
   long img_size = load_img();
 
-  Log("debug");
   /* Load the ELF file. */
   load_elf();
 

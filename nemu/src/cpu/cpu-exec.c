2937614cec29f5d3bd08a3bec76e8511e7973973
tracer-ysyx <tracer@ysyx.org>
2025-01-26 16:15:48 +0800

    > compile NEMU ysyx_23060198 林锦晖 Linux wenjiulaptop 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:15:48 up  1:38,  1 user,  load average: 1.02, 0.65, 0.43

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -130,9 +130,9 @@ static void exec_once(Decode *s, vaddr_t pc) {
 static void execute(uint64_t n) {
   Decode s;
   for (;n > 0; n --) {
-    printf("nemu: executing %ld instructions in for\n", n);
     inst_counter++;
     exec_once(&s, cpu.pc);
+    printf("nemu: executing %ld instructions after exec\n", n);
     g_nr_guest_inst ++;
     trace_and_difftest(&s, cpu.pc);
     if (nemu_state.state != NEMU_RUNNING) break;

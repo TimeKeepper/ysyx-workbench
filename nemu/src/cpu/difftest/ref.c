78c6afd22e252881bf055861dda28f078dfb3190
tracer-ysyx <tracer@ysyx.org>
2025-01-26 16:23:07 +0800

    > compile NEMU ysyx_23060198 林锦晖 Linux wenjiulaptop 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:23:07 up  1:46,  1 user,  load average: 0.25, 0.48, 0.45

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -36,9 +36,9 @@ __EXPORT void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction)
 // `direction`为`DIFFTEST_TO_REF`时, 设置REF的寄存器状态为`dut`;
 __EXPORT void difftest_regcpy(void *dut, bool direction) {
   if(direction == DIFFTEST_TO_DUT) {
-    printf("nemu to dut");
     memcpy(dut, &cpu, sizeof(CPU_state));
   } else {
+    printf("nemu to ref");
     memcpy(&cpu, dut, sizeof(CPU_state));
   }
 }

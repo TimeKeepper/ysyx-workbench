e6bfd2ac3c3059486fc386b578063924ec68a57d
tracer-ysyx <tracer@ysyx.org>
2025-01-26 16:19:18 +0800

    > compile NEMU ysyx_23060198 林锦晖 Linux wenjiulaptop 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:19:18 up  1:42,  1 user,  load average: 0.64, 0.62, 0.46

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -171,7 +171,6 @@ void cpu_exec(uint64_t n) {
 
   uint64_t timer_end = get_time();
   g_timer += timer_end - timer_start;
-  printf("nemu: executing %ld instructions after get_time\n", n);
 
   switch (nemu_state.state) {
     case NEMU_RUNNING: nemu_state.state = NEMU_STOP; break;

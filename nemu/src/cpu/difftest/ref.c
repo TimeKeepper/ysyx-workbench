5f89e3e4cd11efc0c495856707a77bdc6a994afb
tracer-ysyx <tracer@ysyx.org>
2025-01-26 16:25:44 +0800

    > compile NEMU ysyx_23060198 林锦晖 Linux wenjiulaptop 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:25:44 up  1:48,  1 user,  load average: 1.06, 0.69, 0.53

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -26,9 +26,9 @@
 __EXPORT void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction) {
   if(direction == DIFFTEST_TO_REF) {
     // memcpy(Guest_2_host_CODE(addr), buf, n);
+    printf("nemu to ref");
     memcpy(guest_to_host(addr), buf, n);
   } else {
-    printf("nemu to dut");
     memcpy(buf, guest_to_host(addr), n);
   }
 }

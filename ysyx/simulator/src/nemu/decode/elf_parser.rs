b1b13ed357fd8fd2f0d617fb4ade9f5d00801a5c
wenjiu <2784307979@qq.com>
2025-01-31 00:54:39 +0800

    feat: dlopen2

--- ysyx/simulator/src/nemu/decode/elf_parser.rs
+++ ysyx/simulator/src/nemu/decode/elf_parser.rs
@@ -1,5 +1,5 @@
 use winnow::token::take_while;
-use winnow::{PResult, Parser};
+use winnow::{ModalResult, Parser};
 
 pub struct ElfParser;
 
@@ -8,27 +8,27 @@ impl ElfParser {
         Self
     }
 
-    fn parse_addr<'s>(input: &mut &'s str) -> PResult<&'s str> {
+    fn parse_addr<'s>(input: &mut &'s str) -> ModalResult<&'s str> {
         take_while(1.., ('0'..='9', 'a'..='f', ' ')).parse_next(input)
     }
 
-    fn parse_midfix<'s>(input: &mut &'s str) -> PResult<&'s str> {
+    fn parse_midfix<'s>(input: &mut &'s str) -> ModalResult<&'s str> {
         ":	".parse_next(input)
     }
 
-    fn parse_data<'s>(input: &mut &'s str) -> PResult<&'s str> {
+    fn parse_data<'s>(input: &mut &'s str) -> ModalResult<&'s str> {
         take_while(1.., ('0'..='9', 'a'..='f')).parse_next(input)
     }
 
-    fn parse_finalfix<'s>(input: &mut &'s str) -> PResult<&'s str> {
+    fn parse_finalfix<'s>(input: &mut &'s str) -> ModalResult<&'s str> {
         "          	".parse_next(input)
     }
 
-    fn parse_inst<'s>(input: &mut &'s str) -> PResult<&'s str> {
+    fn parse_inst<'s>(input: &mut &'s str) -> ModalResult<&'s str> {
         take_while(1.., ('0'..='9', 'a'..='z', 'A'..='Z', '\t', '+', '-', ',')).parse_next(input)
     }
 
-    pub fn parse<'s>(&mut self, input: &mut &'s str) -> PResult<(&'s str, &'s str, &'s str)> {
+    pub fn parse<'s>(&mut self, input: &mut &'s str) -> ModalResult<(&'s str, &'s str, &'s str)> {
         let addr = ElfParser::parse_addr(input)?;
         let _ = ElfParser::parse_midfix(input)?;
         let data = ElfParser::parse_data(input)?;
@@ -38,7 +38,7 @@ impl ElfParser {
         Ok((addr, data, inst))
     }
 
-    pub fn parser_inst_name<'s>(input: &mut &'s str) -> PResult<&'s str> {
+    pub fn parser_inst_name<'s>(input: &mut &'s str) -> ModalResult<&'s str> {
         take_while(1.., 'a'..='z').parse_next(input)
     }
 }

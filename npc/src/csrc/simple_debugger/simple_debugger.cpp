340f021b8cc65b8a0ef7a2627d901a3339c1a96c
wenjiu <2784307979@qq.com>
2025-02-12 06:14:58 +0800

    simple cache response

--- npc/src/csrc/simple_debugger/simple_debugger.cpp
+++ npc/src/csrc/simple_debugger/simple_debugger.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <numeric>
 #include <simple_debugger.hpp>
 #include <expr.hpp>
@@ -391,6 +392,12 @@ void simple_debugger::LSU_catch(uint32_t diff_skip){
 void simple_debugger::WBU_catch(void) {
     #ifdef CONFIG_DIFFTEST
     this->difftest->difftest_step(this->emulator->cpu.pc);
+    
+    auto msg = this->emulator->icache_msg_transmiter.front();
+    if(msg.first != 0 && msg.second != 0){
+        std::cout << "len " << this->emulator->icache_msg_transmiter.size() << ", map: " << msg.first << ", cache: " << msg.second << std::endl;
+    }
+    this->emulator->icache_msg_transmiter.pop();
     #endif
 
     if(this->wpm->check_watch_points() || this->wpm->check_break_points()){

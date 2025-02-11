594deadb92f931475db6fb2a1ef8208d7387834d
tracer-ysyx <tracer@ysyx.org>
2025-02-12 06:39:00 +0800

    > compile NEMU ysyx_23060198 林锦晖 Linux wenjiupc 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  06:39:00 up  1:54,  2 users,  load average: 0.07, 0.07, 0.08

--- nemu/include/cpu/ifetch.h
+++ nemu/include/cpu/ifetch.h
@@ -17,13 +17,8 @@
 
 #include <memory/vaddr.h>
 
-static inline uint32_t icache_fetch(vaddr_t *pc, int len) {
-  uint32_t inst = vaddr_ifetch(*pc, len);
-  return inst;
-}
-
 static inline uint32_t inst_fetch(vaddr_t *pc, int len) {
-  uint32_t inst = icache_fetch(pc, len);
+  uint32_t inst = vaddr_ifetch(*pc, len);
   (*pc) += len;
   return inst;
 }

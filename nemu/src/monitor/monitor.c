e4b04a094a7408a81184561c5aa883794a859aaf
tracer-ysyx <tracer@ysyx.org>
2025-02-12 07:43:40 +0800

    > compile NEMU ysyx_23060198 林锦晖 Linux wenjiupc 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  07:43:40 up  3:00,  4 users,  load average: 0.22, 0.26, 0.18

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -239,6 +239,8 @@ void init_monitor(int argc, char *argv[]) {
   /* Load the ELF file. */
   load_elf();
 
+  Pin;
+
   /* Initialize differential testing. */
   init_difftest(diff_so_file, img_size, difftest_port);
 

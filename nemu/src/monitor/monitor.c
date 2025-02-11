1e3d98aa9af9f6abde8b6c1ac076b60609da2a62
tracer-ysyx <tracer@ysyx.org>
2025-02-12 07:44:55 +0800

    > compile NEMU ysyx_23060198 林锦晖 Linux wenjiupc 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  07:44:55 up  3:01,  4 users,  load average: 0.15, 0.24, 0.18

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -30,12 +30,12 @@ void init_sdb();
 void init_disasm(const char *triple);
 
 static void welcome() {
-
-  Pin;
-
-
   Log("Trace: %s", MUXDEF(CONFIG_TRACE, ANSI_FMT("ON", ANSI_FG_GREEN),
                           ANSI_FMT("OFF", ANSI_FG_RED)));
+
+                          Pin;
+                        
+                        
   IFDEF(CONFIG_TRACE,
         Log("If trace is enabled, a log file will be generated "
             "to record the trace. This may lead to a large log file. "

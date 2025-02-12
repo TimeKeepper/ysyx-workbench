e09183513458272b61d71d838f2c9d6203570407
tracer-ysyx <tracer@ysyx.org>
2025-02-12 12:36:17 +0800

    > compile NEMU ysyx_23060198 林锦晖 Linux wenjiupc 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  12:36:17 up  2:11,  5 users,  load average: 0.61, 0.95, 0.51

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -112,6 +112,8 @@ void mem_random_set(void){
   memset(sdram, rand(), CONFIG_SDRAM_SIZE);
 }
 
+void Icache_init(paddr_t begin, paddr_t end, int way, int set);
+
 void init_mem() {
 #if   defined(CONFIG_PMEM_MALLOC)
   psram = malloc(CONFIG_PSRAM_SIZE);
@@ -125,6 +127,8 @@ void init_mem() {
   Log("FLASH memory area \t [" FMT_PADDR ", " FMT_PADDR "]", FLASH_LEFT, FLASH_RIGHT);
   Log("PSRAM memory area \t [" FMT_PADDR ", " FMT_PADDR "]", PSRAM_LEFT, PSRAM_RIGHT);
   Log("SDRAM memory area \t [" FMT_PADDR ", " FMT_PADDR "]", SDRAM_LEFT, SDRAM_RIGHT);
+
+  Icache_init(PSRAM_LEFT, PSRAM_RIGHT, 2, 4);
 }
 
 word_t paddr_read(paddr_t addr, int len) {

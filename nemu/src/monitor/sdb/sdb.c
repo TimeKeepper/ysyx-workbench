2c063f5ecb8099c9c0d9fb3b9d3349a98d8044ea
tracer-ysyx <tracer@ysyx.org>
2025-02-12 16:50:10 +0800

    > compile NEMU ysyx_23060198 林锦晖 Linux wenjiupc 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:50:10 up  6:35,  5 users,  load average: 0.43, 0.26, 0.20

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -221,7 +221,7 @@ extern uint64_t map_hit_counter;
 extern uint64_t cache_hit_counter;
 
 static int cmd_t(char *args) {
-  printf(ANSI_FMT("inst_num: \t\t\t", ANSI_FG_BLUE) "%lu\n", inst_counter);
+  printf(ANSI_FMT("inst_num: \t\t\t%lu\n", ANSI_FG_BLUE), inst_counter);
   printf(ANSI_FMT("Execution-Based cache hit rate: %lf\n", ANSI_FG_BLUE), (double)cache_hit_counter / inst_counter);
   printf(ANSI_FMT("Access-Based cache hit rate: \t%lf\n", ANSI_FG_BLUE), (double)cache_hit_counter / map_hit_counter);
   return 0;

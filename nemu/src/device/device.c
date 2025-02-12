22cc7dcd5faa4ccd9bc4b63ff39405eb53761dbd
tracer-ysyx <tracer@ysyx.org>
2025-02-12 08:17:58 +0800

    > compile NEMU ysyx_23060198 林锦晖 Linux wenjiupc 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  08:17:58 up  3:35,  6 users,  load average: 0.22, 0.26, 0.18

--- nemu/src/device/device.c
+++ nemu/src/device/device.c
@@ -77,8 +77,6 @@ void init_device() {
   IFDEF(CONFIG_TARGET_AM, ioe_init());
   init_map();
 
-  Pin;
-
   IFDEF(CONFIG_HAS_SERIAL, init_serial());
   IFDEF(CONFIG_HAS_TIMER, init_timer());
   IFDEF(CONFIG_HAS_VGA, init_vga());
@@ -86,6 +84,8 @@ void init_device() {
   IFDEF(CONFIG_HAS_AUDIO, init_audio());
   IFDEF(CONFIG_HAS_DISK, init_disk());
   IFDEF(CONFIG_HAS_SDCARD, init_sdcard());
+
+  Pin;
 
   IFNDEF(CONFIG_TARGET_AM, init_alarm());
 }

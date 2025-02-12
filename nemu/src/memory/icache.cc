073fdf0c8ed0579ee5bb19608678093a2a759b7d
tracer-ysyx <tracer@ysyx.org>
2025-02-12 14:29:05 +0800

    > compile NEMU ysyx_23060198 林锦晖 Linux wenjiupc 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:29:05 up  4:08,  5 users,  load average: 0.39, 0.25, 0.21

--- nemu/src/memory/icache.cc
+++ nemu/src/memory/icache.cc
@@ -104,7 +104,7 @@ public:
                 std::cout << ANSI_FG_CYAN"valid " << (cache[i][j].valid ? ANSI_FG_GREEN"true" : ANSI_FG_RED"false") << '\t'
                     << ANSI_FG_CYAN"tag " <<  std::hex << ANSI_FG_BLUE"0x" << cache[i][j].tag << '\t'
                     << ANSI_FG_CYAN"data " << std::hex << ANSI_FG_BLUE"0x" << cache[i][j].inst << '\t'
-                    << ANSI_NONE;
+                    << ANSI_NONE << std::endl;
             }
             std::cout << std::endl;
         }

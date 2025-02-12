0ede223cbe740b83d24b5a851f5d8bfcdbee4e08
tracer-ysyx <tracer@ysyx.org>
2025-02-12 18:33:53 +0800

    > sim npc ysyx_23060198 林锦晖 Linux wenjiupc 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:33:53 up  8:23,  5 users,  load average: 0.21, 0.16, 0.23

--- npc/src/csrc/emulator/performence.cpp
+++ npc/src/csrc/emulator/performence.cpp
@@ -90,7 +90,7 @@ void performence::print_perf() {
     std::cout << ANSI_FG_CYAN << "Instruction Counters:" << ANSI_NONE << std::endl;
     for(auto &i : this->inst_cntrs) {
         std::cout << ANSI_FG_BLUE << inst_type_to_string(i.first) << ANSI_NONE 
-            << ":\t" << "Clk" << i.second.first << " " << "Inst" << i.second.second << std::endl;
+            << ":\t" << "Clk:\t" << i.second.first << " Inst:\t" << i.second.second << std::endl;
     }
     // for(auto &i : this->inst_cntrs){
     //     std::cout << inst_type_to_string(i.first) << ":\t" << i.second.first << " " << i.second.second << std::endl;

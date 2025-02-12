8d72c5d234118359a3791ecf000458e0e6d6c20d
tracer-ysyx <tracer@ysyx.org>
2025-02-12 16:17:33 +0800

    > compile NEMU ysyx_23060198 林锦晖 Linux wenjiupc 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:17:33 up  6:01,  5 users,  load average: 0.21, 0.22, 0.42

--- nemu/src/memory/icache.cc
+++ nemu/src/memory/icache.cc
@@ -114,7 +114,7 @@ public:
             std::cout << ANSI_FG_BLUE << "Set " << i << ": " << std::endl;
             for(int j = 0; j < way; ++j) {
                 std::cout << ANSI_FG_CYAN"valid " << (cache[i][j].valid ? ANSI_FG_GREEN"true" : ANSI_FG_RED"false") << '\t'
-                    << ANSI_FG_CYAN"tag[" << this->tag_bits << "] " << this->tag_bits <<  std::hex << ANSI_FG_BLUE"0x" << cache[i][j].tag << '\t'
+                    << ANSI_FG_CYAN"tag[" << tag_bits << "] " <<  std::hex << ANSI_FG_BLUE"0x" << cache[i][j].tag << '\t'
                     << ANSI_FG_CYAN"data " << std::hex << ANSI_FG_BLUE << "0x" << std::setw(8) << std::setfill('0') << cache[i][j].inst << '\t'
                     << ANSI_NONE << std::endl;
                 printf("tagbits: %d\n", tag_bits);

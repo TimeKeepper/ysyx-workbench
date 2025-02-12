d5428e84ac8169671242e3d2a7cd4de3c56787ad
tracer-ysyx <tracer@ysyx.org>
2025-02-12 15:43:28 +0800

    > compile NEMU ysyx_23060198 林锦晖 Linux wenjiupc 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:43:28 up  5:25,  5 users,  load average: 0.36, 0.25, 0.19

--- nemu/src/memory/icache.cc
+++ nemu/src/memory/icache.cc
@@ -48,7 +48,7 @@ public:
         this->tag_bits = valid_bits - offset_bits - set_bits;
 
         printf("offset_bits: %d, set_bits: %d, tag_bits: %d\n", offset_bits, set_bits, tag_bits);
-        printf("size: %d", end - begin);
+        printf("size: %08x", end - begin);
 
         cache.resize(set, std::vector<CacheLine>(way));
         lru.resize(set, std::list<int>());

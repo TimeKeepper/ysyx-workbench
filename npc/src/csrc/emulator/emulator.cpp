62cdfe2b7da1f2ef9da62785ca2e19b5205f72e5
tracer-ysyx <tracer@ysyx.org>
2025-02-13 09:25:02 +0800

    > sim npc ysyx_23060198 林锦晖 Linux wenjiupc 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  09:25:02 up 23:52,  6 users,  load average: 1.04, 0.61, 0.52

--- npc/src/csrc/emulator/emulator.cpp
+++ npc/src/csrc/emulator/emulator.cpp
@@ -148,6 +148,10 @@ void Emulator::init_rand() {
 }
 
 void Emulator::init_mem() {
+    #ifdef CONFIG_waveForm_fst
+    std::cout << "fst" << std::endl;
+    #endif
+
     #ifdef CONFIG_PLATFORM_YSYXSOC
     memorys.emplace("psram", std::make_unique<Memory>(CONFIG_PSRAM_BASE, CONFIG_PSRAM_SIZE));
     memorys.emplace("sdram", std::make_unique<Memory>(CONFIG_SDRAM_BASE, CONFIG_SDRAM_SIZE));

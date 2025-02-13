1b26a564972774ab7a7e1009373a5dba0e3aa6b9
tracer-ysyx <tracer@ysyx.org>
2025-02-13 08:40:29 +0800

    > compile NEMU ysyx_23060198 林锦晖 Linux wenjiupc 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  08:40:29 up 23:07,  5 users,  load average: 0.20, 0.20, 0.13

--- nemu/src/memory/icache.cc
+++ nemu/src/memory/icache.cc
@@ -76,7 +76,7 @@ public:
 
         // uint32_t offset = addr & ((1 << offset_bits) - 1);
         uint32_t set_idx = (addr >> offset_bits) & ((1 << set_bits) - 1);
-        uint32_t tag = (addr >> (offset_bits + set_bits)) & ((1 << tag_bits) - 1);
+        uint32_t tag = (addr >> (offset_bits + set_bits + index_bits)) & ((1 << tag_bits) - 1);
         std::cout << "addr: " << addr << " bias: " << (offset_bits + set_bits) << " tag: " << tag << std::endl;
         // 检查命中
         for(uint32_t w = 0; w < way; ++w) {

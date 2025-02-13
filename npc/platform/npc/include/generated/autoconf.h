e3cd230c63e71805818b8a3d271b57e3ae44784f
tracer-ysyx <tracer@ysyx.org>
2025-02-13 09:38:54 +0800

    > sim npc ysyx_23060198 林锦晖 Linux wenjiupc 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  09:38:54 up 1 day, 6 min,  6 users,  load average: 0.39, 0.31, 0.37

--- npc/platform/npc/include/generated/autoconf.h
+++ npc/platform/npc/include/generated/autoconf.h
@@ -5,10 +5,9 @@
  *
  */
 #define CONFIG_PLATFORM "npc"
-#define CONFIG_DIFFTEST 1
 #define CONFIG_PLATFORM_NPC 1
 #define CONFIG_RESET_VECTOR 0x80000000
+#define CONFIG_waveForm_vcd 1
 #define CONFIG_LOAD_MEMORY_SIZE 0x8000000
-#define CONFIG_WAVEFORM "fst"
+#define CONFIG_WAVEFORM "vcd"
 #define CONFIG_LOAD_MEMORY_BASE 0x80000000
-#define CONFIG_waveForm_fst 1

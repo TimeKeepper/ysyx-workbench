f848676a40ece580c535059497f4f3385e06ab39
tracer-ysyx <tracer@ysyx.org>
2025-02-14 19:58:41 +0800

    > sim npc ysyx_23060198 林锦晖 Linux wenjiupc 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:58:41 up 2 days, 10:49,  7 users,  load average: 1.93, 0.95, 0.60

--- npc/src/csrc/simple_debugger/differtest.cpp
+++ npc/src/csrc/simple_debugger/differtest.cpp
@@ -115,15 +115,15 @@ void Differtest::checkcache(Icache_return icache_state){
     auto ref_cache_state = ref_difftest_cache_state().first;
     for (uint32_t i = 0; i < CONFIG_ICache_Set; ++i) {
         for (uint32_t j = 0; j < CONFIG_ICache_Way; ++j) {
-            if (ref_cache_state[i][j].tag != emulator->cache[i][j].tag) {
-                printf(ANSI_FG_RED "diffter test has detect an error!\n" ANSI_NONE);
+            // if (ref_cache_state[i][j].tag != emulator->cache[i][j].tag || ref_cache_state[i][j].inst != emulator->cache[i][j].inst || ref_cache_state[i][j].valid != emulator->cache[i][j].valid) {
+            //     printf(ANSI_FG_RED "diffter test has detect an error!\n" ANSI_NONE);
                 // printf(ANSI_FG_RED "Set %d, Way %d\n" ANSI_NONE, i, j);
                 // printf("cache: ref_tag:" ANSI_FG_YELLOW "0x%08x" ANSI_NONE ", dut_tag:" ANSI_FG_YELLOW "0x%08x" ANSI_NONE "\n", ref_cache_state[i][j].tag, emulator->cache[i][j].tag);
                 // printf("cache: ref_inst:" ANSI_FG_YELLOW "0x%08x" ANSI_NONE ", dut_inst:" ANSI_FG_YELLOW "0x%08x" ANSI_NONE "\n", ref_cache_state[i][j].inst, emulator->cache[i][j].inst);
                 // printf("cache: ref_valid:" ANSI_FG_YELLOW "%d" ANSI_NONE ", dut_valid:" ANSI_FG_YELLOW "%d" ANSI_NONE "\n", ref_cache_state[i][j].valid, emulator->cache[i][j].valid);
                 // npc_state->state = NPC_ABORT;
                 // npc_state->halt_pc = dut_r->pc;
-            }
+            // }
         }
     }
     Pin;

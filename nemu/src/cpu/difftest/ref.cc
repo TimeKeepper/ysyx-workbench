2b379281433b9ebe6f375ad6ff39a61250742a6b
tracer-ysyx <tracer@ysyx.org>
2025-02-14 18:29:50 +0800

    > compile NEMU ysyx_23060198 林锦晖 Linux wenjiupc 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:29:50 up 2 days,  9:20,  7 users,  load average: 0.93, 0.93, 0.72

--- nemu/src/cpu/difftest/ref.cc
+++ nemu/src/cpu/difftest/ref.cc
@@ -13,6 +13,8 @@
 * See the Mulan PSL v2 for more details.
 ***************************************************************************************/
 
+extern "C"{
+  
 #include <isa.h>
 #include <cpu/cpu.h>
 #include <difftest-def.h>
@@ -24,8 +26,7 @@
 
 // 在DUT host memory的`buf`和REF guest memory的`addr`之间拷贝`n`字节,
 // `direction`指定拷贝的方向, `DIFFTEST_TO_DUT`表示往DUT拷贝, `DIFFTEST_TO_REF`表示往REF拷贝
-extern "C"{
-  
+
 __EXPORT void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction) {
   if(direction == DIFFTEST_TO_REF) {
     // memcpy(Guest_2_host_CODE(addr), buf, n);

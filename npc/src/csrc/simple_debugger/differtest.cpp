e03c16d36bc800d860d779f8c57e94931f1580f6
tracer-ysyx <tracer@ysyx.org>
2025-02-15 01:43:48 +0800

    > gdb YSYXSOC ysyx_23060198 林锦晖 Linux wenjiupc 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  01:43:48 up 2 days, 16:36,  7 users,  load average: 0.87, 1.17, 0.95

--- npc/src/csrc/simple_debugger/differtest.cpp
+++ npc/src/csrc/simple_debugger/differtest.cpp
@@ -134,6 +134,8 @@ void Differtest::checkcache(Icache_return icache_state){
             }
         }
     }
+
+    std::cout << this->emulator->cache[0][0].valid << std::endl;
 }
 
 void Differtest::difftest_step(vaddr_t pc, Icache_return icache_state){

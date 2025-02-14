c6d5cceea882d669f3e7e7c8fbd4ec3a7e088d7f
tracer-ysyx <tracer@ysyx.org>
2025-02-14 23:07:21 +0800

    > compile NEMU ysyx_23060198 林锦晖 Linux wenjiupc 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  23:07:21 up 2 days, 13:58,  7 users,  load average: 3.77, 1.71, 0.85

--- nemu/src/cpu/difftest/ref.cc
+++ nemu/src/cpu/difftest/ref.cc
@@ -61,8 +61,10 @@ __EXPORT void difftest_cache_init(paddr_t begin, paddr_t end, uint32_t way, uint
 __EXPORT void difftest_cache_state(void *dut, bool direction) {
   auto src = reinterpret_cast<std::vector<std::vector<CacheLine>>*>(dut);
   if(direction == DIFFTEST_TO_DUT) {
+    Pin;
     assert(src != nullptr);
     *src = icache.cache;
+    Pin;
   } else {
     icache.cache = *src;
   }

bc6bb4b348550e418043f112b0a6697090715a7f
tracer-ysyx <tracer@ysyx.org>
2025-02-14 23:11:27 +0800

    > sim npc ysyx_23060198 林锦晖 Linux wenjiupc 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  23:11:27 up 2 days, 14:02,  7 users,  load average: 1.21, 1.69, 1.06

--- npc/src/csrc/simple_debugger/differtest.cpp
+++ npc/src/csrc/simple_debugger/differtest.cpp
@@ -121,12 +121,10 @@ void Differtest::checkcache(Icache_return icache_state){
         npc_state->halt_pc = dut_r->pc;
     }
 
+    std::vector<std::vector<CacheLine>> ref_cache_state;
+    ref_cache_state.resize(CONFIG_ICache_Set, std::vector<CacheLine>(CONFIG_ICache_Way));
     Pin;
-    std::vector<std::vector<CacheLine>>* ref_cache_state;
-    Pin;
-    ref_cache_state->resize(CONFIG_ICache_Set, std::vector<CacheLine>(CONFIG_ICache_Way));
-    Pin;
-    ref_difftest_cache_state(ref_cache_state, DIFFTEST_TO_DUT);
+    ref_difftest_cache_state(&ref_cache_state, DIFFTEST_TO_DUT);
     Pin;
 
     // for(uint32_t i = 0; i < ref_cache_state.size(); i++){

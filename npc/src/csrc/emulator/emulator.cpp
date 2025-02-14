01375e5de59148f5a0e9dcacebb8a3f81ba303da
tracer-ysyx <tracer@ysyx.org>
2025-02-15 01:31:40 +0800

    > sim YSYXSOC ysyx_23060198 林锦晖 Linux wenjiupc 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  01:31:40 up 2 days, 16:23,  7 users,  load average: 0.73, 0.66, 0.45

--- npc/src/csrc/emulator/emulator.cpp
+++ npc/src/csrc/emulator/emulator.cpp
@@ -138,6 +138,7 @@ void Emulator::init_mem() {
     memorys.emplace("sram", std::make_unique<Memory>(CONFIG_LOAD_MEMORY_BASE, CONFIG_LOAD_MEMORY_SIZE));
     #endif
 
+    std::cout << "Cache Initialized" << std::endl;
     cache.resize(CONFIG_ICache_Set, std::vector<CacheLine>(CONFIG_ICache_Way));
 }
 

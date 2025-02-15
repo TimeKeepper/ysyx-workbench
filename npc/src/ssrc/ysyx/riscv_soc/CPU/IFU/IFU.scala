0792a3fa2e0e7542aff077044cef37f5f304d593
tracer-ysyx <tracer@ysyx.org>
2025-02-15 11:55:04 +0800

    > sim npc ysyx_23060198 林锦晖 Linux wenjiupc 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:55:04 up 3 days,  3:12, 10 users,  load average: 3.21, 1.35, 0.93

--- npc/src/ssrc/ysyx/riscv_soc/CPU/IFU/IFU.scala
+++ npc/src/ssrc/ysyx/riscv_soc/CPU/IFU/IFU.scala
@@ -160,13 +160,19 @@ class Icache(address: Seq[AddressSet], way: Int, set: Int, block_size: Int) exte
     io.cache_hit := tag_match
 
     // TODO: have to implement LRU Algorithm
+    val replacement = ReplacementPolicy.fromString("plru", way)
+
     val replace_set_index = io.replace_addr(set_width + offset_width - 1, offset_width) // input addr maybe change after input shake hands
+    val replace_way = replacement.way
     val replace_tag = io.replace_addr(valid_width - 1, set_width + offset_width)
     val replace_cache = io.replace_data.bits
 
     when(io.replace_data.valid){
-        meta(replace_set_index)(match_way) := Cat(true.B, replace_tag)
-        data(replace_set_index)(match_way) := replace_cache
+        meta(replace_set_index)(replace_way) := Cat(true.B, replace_tag)
+        data(replace_set_index)(replace_way) := replace_cache
+        replacement.access(replace_way)
+    }.elsewhen(tag_match){
+        replacement.access(match_way)
     }
 
     if(Config.Simulate){

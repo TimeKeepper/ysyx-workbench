d0cde2f2eff503d3fa02aad2495fff3b14fe882f
tracer-ysyx <tracer@ysyx.org>
2025-02-21 20:32:01 +0800

    > compile NEMU ysyx_23060198 林锦晖 Linux wenjiupc 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:32:01 up 37 min,  3 users,  load average: 0.45, 0.24, 0.19

--- nemu/src/memory/icache.cc
+++ nemu/src/memory/icache.cc
@@ -1,3 +1,4 @@
+#include <cstdint>
 #include <iomanip>
 #include <stdio.h>
 
@@ -13,25 +14,22 @@ extern "C" {
 #include <iostream>
 #include <memory/icache.hpp>
 
-void Icache_LRU::init(uint32_t begin, uint32_t end, uint32_t way, uint32_t set, uint32_t line_size) {
+void Icache_LRU::init(uint32_t begin, uint32_t end, uint32_t way, uint32_t set, uint32_t block_size) {
         this->begin = begin;
         this->end = end;
         this->way = way;
         this->set = set;
-        this->line_size = line_size;
-        this->offset_bits = ceil(std::log2((double)line_size));
+        this->block_size = block_size;
+        this->offset_bits = ceil(std::log2((double)block_size));
         this->set_bits = ceil(log2((double)set));
 
         uint32_t valid_bits = ceil(std::log2((double)(end - begin)));
         this->tag_bits = valid_bits - offset_bits - set_bits;
 
-        cache.resize(set, std::vector<CacheLine>(way));
+        cache.resize(set, std::vector<CacheLine>(way, CacheLine(block_size)));
         lru.resize(set, std::list<uint32_t>());
         for(uint32_t i = 0; i < set; ++i) {
             for(uint32_t j = 0; j < way; ++j) {
-                cache[i][j].valid = false;
-                cache[i][j].tag = 0;
-                cache[i][j].inst = 0;
                 lru[i].push_front(j);
             }
         }
@@ -49,7 +47,7 @@ Icache_return Icache_LRU::fetch(vaddr_t addr, uint32_t len) {
         return result;
     }
 
-    // uint32_t offset = addr & ((1 << offset_bits) - 1);
+    uint32_t offset = addr & ((1 << offset_bits) - 1);
     uint32_t set_idx = (addr >> offset_bits) & ((1 << set_bits) - 1);
     uint32_t tag = (addr >> (offset_bits + set_bits)) & ((1 << tag_bits) - 1);
     
@@ -59,22 +57,32 @@ Icache_return Icache_LRU::fetch(vaddr_t addr, uint32_t len) {
             // 更新LRU列表
             lru[set_idx].remove(w);
             lru[set_idx].push_back(w);
-            result.inst = cache[set_idx][w].inst;
+            result.inst = cache[set_idx][w].inst[0];
             result.cache_hit = true;
             return result;
         }
     }
 
     // 缓存未命中，从内存中加载指令
-    result.inst = paddr_read(addr, len);
-
     // 替换策略：替换LRU列表末尾的缓存行
     int replace_way = lru[set_idx].front();
     lru[set_idx].pop_front();
     lru[set_idx].push_back(replace_way);
 
+    // Calculate base address aligned to block size
+    vaddr_t block_addr = addr & ~((1 << offset_bits) - 1);
+    
+    // Load entire cache block at once
+    auto& block_data = cache[set_idx][replace_way].inst;
+    for (uint32_t i = 0; i < block_size / 4; ++i) {
+        block_data[i] = paddr_read(block_addr + i * 4, 4);
+    }
+    
+    // Get the requested instruction
+    result.inst = block_data[offset / 4];
+
     cache[set_idx][replace_way].tag = tag;
-    cache[set_idx][replace_way].inst = result.inst;
+    // cache[set_idx][replace_way].inst = replace_data
     cache[set_idx][replace_way].valid = true;
 
     return result;
@@ -86,7 +94,14 @@ void Icache_LRU::print_cache() {
         for(uint32_t j = 0; j < way; ++j) {
             std::cout << ANSI_FG_CYAN"valid " << (cache[i][j].valid ? ANSI_FG_GREEN"true" : ANSI_FG_RED"false") << '\t'
                 << ANSI_FG_CYAN"tag[" << tag_bits << "] " <<  std::hex << ANSI_FG_BLUE"0x" << cache[i][j].tag << '\t'
-                << ANSI_FG_CYAN"data " << ANSI_FG_BLUE << "0x" << std::setw(8) << std::setfill('0') << cache[i][j].inst << std::dec << '\t'
+                << ANSI_FG_CYAN"data " << ANSI_FG_BLUE;
+
+            for(uint32_t k : cache[i][j].inst) {
+                std::cout << std::hex << std::setw(8) << std::setfill('0') << k << " ";
+            }
+
+            // std::cout << cache[i][j].inst;
+            std::cout << std::dec << '\t'
                 << ANSI_NONE << std::endl;
         }
         std::cout << std::endl;

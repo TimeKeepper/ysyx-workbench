c8b716cd3c1f893fe84257bf1265acd508e6b68b
tracer-ysyx <tracer@ysyx.org>
2025-02-23 19:40:54 +0800

    > sim npc ysyx_23060198 林锦晖 Linux wenjiupc 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:40:54 up 2 days,  2:38,  4 users,  load average: 0.48, 0.25, 0.27

--- npc/src/ssrc/ysyx/riscv_soc/CPU/IFU/IFU.scala
+++ npc/src/ssrc/ysyx/riscv_soc/CPU/IFU/IFU.scala
@@ -164,11 +164,6 @@ class Icache(address: Seq[AddressSet], way: Int, set: Int, block_size: Int) exte
     val replace_set_index = io.replace_addr(set_width + offset_width - 1, offset_width) // input addr maybe change after input shake hands
     
     val replacement = ReplacementPolicy.fromString("setlru", way, set)
-    // val replacements = Seq(ReplacementPolicy.fromString("lru", way))
-    // val replacement_idx = Wire(UInt(log2Ceil(set).W))
-    // replacement_idx := MuxCase(0.U, (0 until set).map { i =>
-    //     (replace_set_index === i.U) -> i.U
-    // })
     
     val replace_way = replacement.way(replace_set_index)
     val replace_way_mask = UIntToOH(replace_way, way)
@@ -182,8 +177,6 @@ class Icache(address: Seq[AddressSet], way: Int, set: Int, block_size: Int) exte
     when(io.replace_data.valid){
         meta.write(replace_set_index, replace_tag_v, replace_way_mask.asBools)
         data.write(replace_set_index, replace_cache_v, replace_way_mask.asBools)
-        // meta(replace_set_index)(replace_way) := Cat(true.B, replace_tag)
-        // data(replace_set_index)(replace_way) := replace_cache
         replacement.access(replace_set_index, replace_way)
     }.elsewhen(RegNext(tag_match && io.addr.valid)){
         replacement.access(replace_set_index, match_way)

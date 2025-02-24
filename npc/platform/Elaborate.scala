9f77c9a31500daa68c3a7ab97250e2633f94a5bc
tracer-ysyx <tracer@ysyx.org>
2025-02-24 11:44:56 +0800

    > sim npc ysyx_23060198 林锦晖 Linux wenjiupc 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:44:56 up 2 days, 19:16,  4 users,  load average: 0.64, 0.48, 0.52

--- npc/platform/Elaborate.scala
+++ npc/platform/Elaborate.scala
@@ -40,7 +40,7 @@ object Elaboratenpc extends App {
   
   Config.Reset_Vector = "h80000000".U(32.W)
   Config.setSimulate(true)
-  Config.setIcacheParam(AddressSet.misaligned(0x80000000L, 0x8000000), 2, 8, 4)
+  Config.setIcacheParam(AddressSet.misaligned(0x80000000L, 0x8000000), 2, 4, 8)
   Config.setDiffMisMap( AddressSet.misaligned(0x10000000, 0x1000) ++
                         AddressSet.misaligned(0xa0000048L, 0x10))
 

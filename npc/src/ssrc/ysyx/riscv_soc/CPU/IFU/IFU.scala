d49d1e385fea4189bb410c379a0583d25ad7d820
tracer-ysyx <tracer@ysyx.org>
2025-02-24 13:32:27 +0800

    > sim npc ysyx_23060198 林锦晖 Linux wenjiupc 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  13:32:27 up 2 days, 21:08,  4 users,  load average: 2.26, 0.77, 0.41

--- npc/src/ssrc/ysyx/riscv_soc/CPU/IFU/IFU.scala
+++ npc/src/ssrc/ysyx/riscv_soc/CPU/IFU/IFU.scala
@@ -239,7 +239,7 @@ class IFU(idBits: Int)(implicit p: Parameters) extends LazyModule {
         io.IFU_2_IDU.bits.PC := addr_cache
 
         master.ar.valid := state === s_busy
-        master.ar.bits.addr := addr_cache + (Multi_transfer_counter << 2.U) // so we can use it here
+        master.ar.bits.addr := (addr_cache & ~((Config.Icache_Param.block_size - 1).U)) + (Multi_transfer_counter << 2.U) // so we can use it here
         Icache.io.replace_addr := addr_cache
         
         val map_hit = Config.Icache_Param.address.map(_.contains(addr_cache)).reduce(_ || _)

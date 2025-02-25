458ec1f5c5e3fac5cf0ba7ba8815db1ad46dc099
tracer-ysyx <tracer@ysyx.org>
2025-02-26 03:47:51 +0800

    > sim npc ysyx_23060198 林锦晖 Linux wenjiupc 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  03:47:51 up 4 days, 12:46,  5 users,  load average: 1.73, 0.75, 0.49

--- npc/src/ssrc/ysyx/riscv_soc/CPU/IFU/IFU.scala
+++ npc/src/ssrc/ysyx/riscv_soc/CPU/IFU/IFU.scala
@@ -262,7 +262,7 @@ class IFU(idBits: Int)(implicit p: Parameters) extends LazyModule {
         master.r.ready := Mux(state === IFU_state.s_get, io.IFU_2_IDU.ready, state === IFU_state.s_replacement_get)
 
         Icache.io.replace_data.bits := Multi_transfer.asTypeOf(UInt((Config.Icache_Param.block_size * 8).W))
-        Icache.io.replace_data.valid := (state === IFU_state.s_replacement_get) && (Multi_transfer_counter === 0.U)
+        Icache.io.replace_data.valid := (state === IFU_state.s_wait_ready) && (Multi_transfer_counter === 0.U)
 
         // val inst_cache = Multi_transfer(blcok_index)
         val inst_cache = Mux(state === IFU_state.s_get, master.r.bits.data, Multi_transfer(blcok_index))

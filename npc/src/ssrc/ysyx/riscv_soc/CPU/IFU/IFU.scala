912c6641ed86c4f0be55647724df0daad3952988
tracer-ysyx <tracer@ysyx.org>
2025-02-26 03:22:48 +0800

    > sim YSYXSOC ysyx_23060198 林锦晖 Linux wenjiupc 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  03:22:48 up 4 days, 12:20,  5 users,  load average: 1.94, 0.66, 0.36

--- npc/src/ssrc/ysyx/riscv_soc/CPU/IFU/IFU.scala
+++ npc/src/ssrc/ysyx/riscv_soc/CPU/IFU/IFU.scala
@@ -253,7 +253,7 @@ class IFU(idBits: Int)(implicit p: Parameters) extends LazyModule {
         io.IFU_2_IDU.bits.PC := addr_cache
 
         master.ar.valid := (state === IFU_state.s_replacement_send_addr) || (state === IFU_state.s_send_addr)
-        master.ar.bits.addr := Mux(state === IFU_state.s_get, addr_cache, 
+        master.ar.bits.addr := Mux(state === IFU_state.s_send_addr, addr_cache, 
             (addr_cache & ~((Config.Icache_Param.block_size - 1).U(32.W))) + (Multi_transfer_counter << 2.U)) // so we can use it here
         Icache.io.replace_addr := addr_cache
         

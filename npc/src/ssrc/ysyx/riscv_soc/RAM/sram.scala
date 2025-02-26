77959e903ea6dfec81849ad0ebd2c6c1f7351dbf
tracer-ysyx <tracer@ysyx.org>
2025-02-26 10:33:01 +0800

    > sim npc ysyx_23060198 林锦晖 Linux wenjiulaptop 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  10:33:01 up  1:46,  1 user,  load average: 10.61, 5.78, 3.91

--- npc/src/ssrc/ysyx/riscv_soc/RAM/sram.scala
+++ npc/src/ssrc/ysyx/riscv_soc/RAM/sram.scala
@@ -78,7 +78,6 @@ class SRAM(address: Seq[AddressSet])(implicit p: Parameters) extends LazyModule 
         val write_burst_counter = RegEnable(AXI.aw.bits.len, AXI.aw.fire)
 
         val read_addr = RegEnable(AXI.ar.bits.addr + 4.U, AXI.ar.fire)
-        val write_addr = RegEnable(AXI.aw.bits.addr + 4.U, AXI.aw.fire)
 
         when(AXI.r.fire) {
             read_burst_counter := read_burst_counter - 1.U
@@ -120,7 +119,7 @@ class SRAM(address: Seq[AddressSet])(implicit p: Parameters) extends LazyModule 
         AXI.r.bits.resp := "b0".U
 
         bridge.io.write := state_w === s_burst
-        bridge.io.w_addr  := Mux(AXI.aw.fire, AXI.aw.bits.addr, write_addr)
+        bridge.io.w_addr  := RegEnable(AXI.aw.bits.addr, AXI.aw.fire)
         bridge.io.w_data  := AXI.w.bits.data
         bridge.io.w_strb  := AXI.w.bits.strb
         AXI.b.bits.resp := "b0".U

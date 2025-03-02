1e7124568e81fb808cf51e2f8a770c7963e28324
tracer-ysyx <tracer@ysyx.org>
2025-03-02 19:42:46 +0800

    > sim npc ysyx_23060198 林锦晖 Linux wenjiupc 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:42:46 up 9 days,  8:53,  5 users,  load average: 1.12, 1.01, 0.86

--- npc/src/ssrc/ysyx/riscv_soc/CPU/IFU/IFU.scala
+++ npc/src/ssrc/ysyx/riscv_soc/CPU/IFU/IFU.scala
@@ -280,10 +280,10 @@ class IFU(idBits: Int)(implicit p: Parameters) extends LazyModule {
             }
         }
         
-        when(io.WBU_2_IFU.fire){
-            Multi_transfer(block_index_pre) := Icache.io.data.asTypeOf(Vec(Config.Icache_Param.block_size / 4, UInt(32.W)))(block_index_pre)
-        }
-        
+        // when(io.WBU_2_IFU.fire){
+        //     Multi_transfer(block_index_pre) := Icache.io.data.asTypeOf(Vec(Config.Icache_Param.block_size / 4, UInt(32.W)))(block_index_pre)
+        // }
+
         io.WBU_2_IFU.ready := state === IFU_state.s_wait_valid
 
         io.IFU_2_IDU.valid := state === IFU_state.s_wait_ready
@@ -301,7 +301,7 @@ class IFU(idBits: Int)(implicit p: Parameters) extends LazyModule {
         Icache.io.replace_data.bits := Multi_transfer.asTypeOf(UInt((Config.Icache_Param.block_size * 8).W))
         Icache.io.replace_data.valid := (state === IFU_state.s_wait_ready) && (RegNext(state === IFU_state.s_replacement_get))
 
-        val inst_cache = Mux(state === IFU_state.s_get, master.r.bits.data, Multi_transfer(blcok_index))
+        val inst_cache = Mux(state === IFU_state.s_get, Multi_transfer(block_num - 1), RegEnable(Icache.io.data.asTypeOf(Vec(Config.Icache_Param.block_size / 4, UInt(32.W)))(block_index_pre), io.WBU_2_IFU.fire))
             
         io.IFU_2_IDU.bits.data := inst_cache
         io.IFU_2_REG.GPR_Aaddr := inst_cache(19, 15)

8d4adfff0a4d68bc70f4baafa2a019e78e6801e6
tracer-ysyx <tracer@ysyx.org>
2025-03-02 20:14:50 +0800

    > sim npc ysyx_23060198 林锦晖 Linux wenjiupc 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:14:50 up 9 days,  9:25,  5 users,  load average: 2.74, 2.15, 1.32

--- npc/src/ssrc/ysyx/riscv_soc/CPU/IFU/IFU.scala
+++ npc/src/ssrc/ysyx/riscv_soc/CPU/IFU/IFU.scala
@@ -267,16 +267,20 @@ class IFU(idBits: Int)(implicit p: Parameters) extends LazyModule {
         val Multi_transfer = RegInit(VecInit(Seq.fill(block_num)(0.U(32.W))))
         // val Multi_transfer_counter = RegInit((block_num - 1).U)
         val Multi_transfer_counter = RegInit(0.U(log2Ceil(block_num).W))
-        when (master.r.fire && state === IFU_state.s_replacement_get) {
-            when(Multi_transfer_counter === (block_num - 1).U){
-                Multi_transfer_counter := 0.U
-            }.otherwise{
-                Multi_transfer_counter := Multi_transfer_counter + 1.U
-            }
+        when (master.r.fire) {
+            when(state === IFU_state.s_replacement_get){
+                when(Multi_transfer_counter === (block_num - 1).U){
+                    Multi_transfer_counter := 0.U
+                }.otherwise{
+                    Multi_transfer_counter := Multi_transfer_counter + 1.U
+                }
 
-            Multi_transfer(block_num - 1) := master.r.bits.data
-            for(i <- 0 until (block_num - 1)){
-                Multi_transfer(i) := Multi_transfer(i + 1)
+                Multi_transfer(block_num - 1) := master.r.bits.data
+                for(i <- 0 until (block_num - 1)){
+                    Multi_transfer(i) := Multi_transfer(i + 1)
+                }
+            }.otherwise {
+                Multi_transfer(block_index_pre) := master.r.bits.data
             }
         }.elsewhen(io.WBU_2_IFU.fire){
             Multi_transfer(block_index_pre) := Icache.io.data.asTypeOf(Vec(Config.Icache_Param.block_size / 4, UInt(32.W)))(block_index_pre)
@@ -298,7 +302,7 @@ class IFU(idBits: Int)(implicit p: Parameters) extends LazyModule {
         Icache.io.replace_data.bits := Multi_transfer.asTypeOf(UInt((Config.Icache_Param.block_size * 8).W))
         Icache.io.replace_data.valid := (state === IFU_state.s_wait_ready) && (RegNext(state === IFU_state.s_replacement_get))
 
-        val inst_cache = Mux(state === IFU_state.s_get, master.r.bits.data, Multi_transfer(blcok_index))
+        val inst_cache = Multi_transfer(blcok_index)
             
         io.IFU_2_IDU.bits.data := inst_cache
         io.IFU_2_REG.GPR_Aaddr := inst_cache(19, 15)

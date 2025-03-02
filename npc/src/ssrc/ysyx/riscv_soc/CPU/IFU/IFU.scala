e746908929b30639c7720b1f58c6db165ec398b8
tracer-ysyx <tracer@ysyx.org>
2025-03-02 19:34:20 +0800

    > sim npc ysyx_23060198 林锦晖 Linux wenjiupc 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:34:20 up 9 days,  8:44,  5 users,  load average: 1.49, 0.85, 0.73

--- npc/src/ssrc/ysyx/riscv_soc/CPU/IFU/IFU.scala
+++ npc/src/ssrc/ysyx/riscv_soc/CPU/IFU/IFU.scala
@@ -200,7 +200,7 @@ class Icache(address: Seq[AddressSet], way: Int, set: Int, block_size: Int) exte
         Icache_state.io.write_tag := replace_tag
         Icache_state.io.write_data := replace_cache
 
-        Icache_state.io.flush := false.B
+        Icache_state.io.flush := io.flush
     }
 }
 
@@ -283,7 +283,7 @@ class IFU(idBits: Int)(implicit p: Parameters) extends LazyModule {
         }
         io.WBU_2_IFU.ready := state === IFU_state.s_wait_valid
 
-        io.IFU_2_IDU.valid := Mux(state === IFU_state.s_get, master.r.valid, state === IFU_state.s_wait_ready)
+        io.IFU_2_IDU.valid := state === IFU_state.s_wait_ready
         io.IFU_2_IDU.bits.PC := addr_cache
 
         master.ar.valid := (state === IFU_state.s_replacement_send_addr) || (state === IFU_state.s_send_addr)
@@ -338,7 +338,7 @@ class IFU(idBits: Int)(implicit p: Parameters) extends LazyModule {
                 ),
 
                 IFU_state.s_replacement_get -> Mux(master.r.fire && (Multi_transfer_counter === (block_num - 1).U), 
-                    IFU_state.s_wait_ready, 
+                    IFU_state.s_wait_valid, 
                     // Mux((Multi_transfer_counter === (block_num - 1).U), 
                     //     IFU_state.s_wait_ready, 
                     //     IFU_state.s_replacement_send_addr

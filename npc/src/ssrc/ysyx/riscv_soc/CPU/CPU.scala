ae71e2a5e2d391ee1a3939ce0fab9c822ebabdc5
tracer-ysyx <tracer@ysyx.org>
2025-03-03 16:56:51 +0800

    > sim npc ysyx_23060198 林锦晖 Linux wenjiupc 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:56:51 up 10 days,  6:32,  5 users,  load average: 2.23, 1.21, 0.80

--- npc/src/ssrc/ysyx/riscv_soc/CPU/CPU.scala
+++ npc/src/ssrc/ysyx/riscv_soc/CPU/CPU.scala
@@ -152,11 +152,11 @@ class npc(idBits: Int)(implicit p: Parameters) extends LazyModule {
 
   ElaborationArtefacts.add("graphml", graphML)
   val LazyIFU = LazyModule(new IFU(idBits = idBits))
-  // val LazyEXU = LazyModule(new EXU(idBits = idBits))
+  val LazyEXU = LazyModule(new EXU(idBits = idBits))
 
   val xbar = AXI4Xbar(maxFlightPerId = 1, awQueueDepth = 1)
   xbar := LazyIFU.masterNode
-  // xbar := LazyEXU.masterNode
+  xbar := LazyEXU.masterNode
 
   val luart = LazyModule(new UART(AddressSet.misaligned(0x10000000, 0x1000)))
   val lclint = LazyModule(new CLINT(AddressSet.misaligned(0xa0000048L, 0x10), 985.U))
@@ -170,7 +170,7 @@ class npc(idBits: Int)(implicit p: Parameters) extends LazyModule {
     
     val IFU             = LazyIFU.module
     val IDU             = Module(new IDU)
-    // val EXU             = LazyEXU.module
+    val EXU             = LazyEXU.module
     // val WBU             = Module(new WBU)
     val REG             = Module(new REG) 
 
@@ -183,25 +183,18 @@ class npc(idBits: Int)(implicit p: Parameters) extends LazyModule {
     IFU.io.Pipeline_ctrl := Ctrl
     // IFU.io.IFU_2_IDU     <> IDU.io.IFU_2_IDU
     pipelineConnect(IFU.io.IFU_2_IDU, IDU.io.IFU_2_IDU, IDU.io.IDU_2_EXU, Ctrl)
+    pipelineConnect(IDU.io.IDU_2_EXU, EXU.io.IDU_2_EXU, EXU.io.EXU_2_WBU, Ctrl)
 
     // should delete
-    IDU.io.IDU_2_EXU.ready := true.B
-    REG.io.IDU_2_REG <> DontCare
-    REG.io.REG_2_EXU <> DontCare
+    EXU.io.EXU_2_WBU.ready := true.B
     REG.io.WBU_2_REG <> DontCare
 
     // bus IFU -> REG -> IDU without delay
     REG.io.REG_2_IDU     <> IDU.io.REG_2_IDU
 
-    // // bus IDU -> EXU
-    // IDU.io.IDU_2_EXU     <> EXU.io.IDU_2_EXU    
-
     // // bus IDU -> REG -> EXU without delay
-    // IDU.io.IDU_2_REG     <> REG.io.IDU_2_REG
-    // REG.io.REG_2_EXU     <> EXU.io.REG_2_EXU   
-
-    // // bus EXU -> WBU
-    // EXU.io.EXU_2_WBU     <> WBU.io.EXU_2_WBU   
+    IDU.io.IDU_2_REG     <> REG.io.IDU_2_REG
+    REG.io.REG_2_EXU     <> EXU.io.REG_2_EXU   
 
     // // bus WBU -> IFU
     // WBU.io.WBU_2_IFU     <> IFU.io.WBU_2_IFU
--- npc/src/ssrc/ysyx/riscv_soc/CPU/EXU/EXU.scala
+++ npc/src/ssrc/ysyx/riscv_soc/CPU/EXU/EXU.scala
@@ -33,8 +33,6 @@ class EXU(idBits: Int)(implicit p: Parameters) extends LazyModule {
         val (master, _) = masterNode.out(0)
         master <> AXI
 
-        val save = RegEnable(io.IDU_2_EXU.bits, io.IDU_2_EXU.fire)
-
         val alu = Module(new ALU)
         val lsu = Module(new LSU)
 
@@ -56,24 +54,24 @@ class EXU(idBits: Int)(implicit p: Parameters) extends LazyModule {
             io.EXU_2_WBU.valid <> alu.io.out.valid
         }
 
-        alu.io.IDU_2_EXU.bits := save
+        alu.io.IDU_2_EXU.bits := io.IDU_2_EXU.bits
 
-        lsu.io.IDU_2_EXU.bits := save
+        lsu.io.IDU_2_EXU.bits := io.IDU_2_EXU.bits
         lsu.io.AXI <> AXI
         
-        val Jmp_Pc = MuxLookup(save.Branch, save.PC + save.Imm)(Seq(
-            Bran_TypeEnum.Bran_Jmpr -> (save.EXU_A + save.Imm),
-            Bran_TypeEnum.Bran_Jcsr -> (save.EXU_B)
+        val Jmp_Pc = MuxLookup(io.IDU_2_EXU.bits.Branch, io.IDU_2_EXU.bits.PC + io.IDU_2_EXU.bits.Imm)(Seq(
+            Bran_TypeEnum.Bran_Jmpr -> (io.IDU_2_EXU.bits.EXU_A + io.IDU_2_EXU.bits.Imm),
+            Bran_TypeEnum.Bran_Jcsr -> (io.IDU_2_EXU.bits.EXU_B)
         ))
 
-        io.EXU_2_WBU.bits.Branch        := save.Branch      
+        io.EXU_2_WBU.bits.Branch        := io.IDU_2_EXU.bits.Branch      
         io.EXU_2_WBU.bits.Jmp_Pc        := Jmp_Pc                        
-        io.EXU_2_WBU.bits.MemtoReg      := save.EXUctr  === EXUctr_TypeEnum.EXUctr_LD 
-        io.EXU_2_WBU.bits.csr_ctr       := save.csr_ctr     
-        io.EXU_2_WBU.bits.CSR_waddr     := save.Imm(11, 0)  
-        io.EXU_2_WBU.bits.GPR_waddr     := save.GPR_waddr   
-        io.EXU_2_WBU.bits.PC            := save.PC          
-        io.EXU_2_WBU.bits.CSR_rdata     := save.EXU_B   
+        io.EXU_2_WBU.bits.MemtoReg      := io.IDU_2_EXU.bits.EXUctr  === EXUctr_TypeEnum.EXUctr_LD 
+        io.EXU_2_WBU.bits.csr_ctr       := io.IDU_2_EXU.bits.csr_ctr     
+        io.EXU_2_WBU.bits.CSR_waddr     := io.IDU_2_EXU.bits.Imm(11, 0)  
+        io.EXU_2_WBU.bits.GPR_waddr     := io.IDU_2_EXU.bits.GPR_waddr   
+        io.EXU_2_WBU.bits.PC            := io.IDU_2_EXU.bits.PC          
+        io.EXU_2_WBU.bits.CSR_rdata     := io.IDU_2_EXU.bits.EXU_B   
         io.EXU_2_WBU.bits.Result        := alu.io.out.bits.Result
         io.EXU_2_WBU.bits.Mem_rdata     := lsu.io.out.bits.Mem_rdata
     }

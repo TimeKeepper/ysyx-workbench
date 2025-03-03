31a7696449b875498770d016edf0fa545c4733ad
tracer-ysyx <tracer@ysyx.org>
2025-03-03 13:11:57 +0800

    > sim npc ysyx_23060198 林锦晖 Linux wenjiupc 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  13:11:57 up 10 days,  2:44,  5 users,  load average: 1.22, 1.29, 1.09

--- npc/src/ssrc/ysyx/riscv_soc/CPU/IFU/IFU.scala
+++ npc/src/ssrc/ysyx/riscv_soc/CPU/IFU/IFU.scala
@@ -317,7 +317,7 @@ class IFU(idBits: Int)(implicit p: Parameters) extends LazyModule {
                     IFU_state.s_get_data, IFU_state.s_send_addr),
 
                 IFU_state.s_get_data -> Mux(master.r.fire,
-                    IFU_state.s_wait_valid, IFU_state.s_get_data),
+                    IFU_state.s_try_fetch, IFU_state.s_get_data),
 
                 IFU_state.s_replace_send_addr -> Mux(master.ar.fire,
                     IFU_state.s_replace_get_data, IFU_state.s_replace_send_addr),

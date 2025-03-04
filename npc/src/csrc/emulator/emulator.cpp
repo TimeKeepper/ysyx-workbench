f67411a4a3b905ab4ad58b575432f7b3ec8aaa5a
tracer-ysyx <tracer@ysyx.org>
2025-03-04 08:51:41 +0800

    > sim npc ysyx_23060198 林锦晖 Linux wenjiulaptop 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  08:51:41 up  6:25,  1 user,  load average: 5.47, 1.97, 1.29

--- npc/src/csrc/emulator/emulator.cpp
+++ npc/src/csrc/emulator/emulator.cpp
@@ -379,5 +379,5 @@ void Emulator::WBU_catch(uint32_t next_pc, \
     this->perf->inst_cont();
     
     std::cout << this->disasm(cpu.pc, Inst_quene.back()) << std::endl;
-    Inst_quene.pop();
+    // Inst_quene.pop();
 }

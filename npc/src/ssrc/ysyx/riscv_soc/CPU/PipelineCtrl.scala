b7caaf492d2ae9616a163be1b93181f68ebd909b
tracer-ysyx <tracer@ysyx.org>
2025-03-05 11:26:57 +0800

    > sim npc ysyx_23060198 林锦晖 Linux wenjiulaptop 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:26:57 up  2:03,  1 user,  load average: 6.63, 3.28, 2.44

--- npc/src/ssrc/ysyx/riscv_soc/CPU/PipelineCtrl.scala
+++ npc/src/ssrc/ysyx/riscv_soc/CPU/PipelineCtrl.scala
@@ -5,14 +5,19 @@ import chisel3.util._
 
 import signal_value._
 import config._
+import freechips.rocketchip.tilelink.TLMessages.d
 
 class PipelineCtrl extends Module {
     val io = IO(new Bundle {
-        val IDU_msg = Flipped(ValidIO((new BUS_IDU_2_REG)))
+        val GPR_read = Flipped(ValidIO((new BUS_IDU_2_REG)))
+
+        val IDU_msg = Flipped(ValidIO((new BUS_IFU_2_IDU)))
         val EXU_msg = Flipped(ValidIO((new BUS_IDU_2_EXU)))
         val WBU_msg = Flipped(ValidIO((new BUS_EXU_2_WBU)))
+        val Branch_msg = Flipped(ValidIO((new BUS_WBU_2_IFU)))
 
         val IDUCtrl = new Pipeline_ctrl
+        val EXUCtrl = new Pipeline_ctrl
     })
 
     def conflict(rs: UInt, rd: UInt) = (rs === rd)
@@ -22,10 +27,20 @@ class PipelineCtrl extends Module {
         (conflict_gpr(rs, io.EXU_msg.bits.GPR_waddr) & io.EXU_msg.valid) ||
         (conflict_gpr(rs, io.WBU_msg.bits.GPR_waddr) & io.WBU_msg.valid)
 
-    def is_gpr_RAW = io.IDU_msg.valid && 
-                     (conflict_gpr_valid(io.IDU_msg.bits.GPR_Aaddr) ||
-                     conflict_gpr_valid(io.IDU_msg.bits.GPR_Baddr))
+    def is_gpr_RAW = io.GPR_read.valid && 
+                     (conflict_gpr_valid(io.GPR_read.bits.GPR_Aaddr) ||
+                     conflict_gpr_valid(io.GPR_read.bits.GPR_Baddr))
 
-    io.IDUCtrl.flush := false.B
+    def conflict_pc(target: UInt, valid: Bool) =
+        valid && io.Branch_msg.valid && (target =/= io.Branch_msg.bits.Next_PC)
+        
+    def is_bp_error = conflict_pc(io.IDU_msg.bits.PC, io.IDU_msg.valid) ||
+                      conflict_pc(io.EXU_msg.bits.PC, io.EXU_msg.valid) ||
+                      conflict_pc(io.WBU_msg.bits.PC, io.WBU_msg.valid)
+
+    io.IDUCtrl.flush := is_bp_error
     io.IDUCtrl.stall := is_gpr_RAW
+
+    io.EXUCtrl.flush := is_bp_error
+    io.EXUCtrl.stall := false.B
 }

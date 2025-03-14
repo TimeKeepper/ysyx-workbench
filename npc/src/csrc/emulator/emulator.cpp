9bb24a4333ec24bc6b7e7f714202bfa906586d43
tracer-ysyx <tracer@ysyx.org>
2025-03-14 18:36:00 +0800

    > sim npc ysyx_23060198 林锦晖 Linux wenjiupc 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:36:00 up 21 days, 14:43,  2 users,  load average: 0.13, 0.25, 0.38

--- npc/src/csrc/emulator/emulator.cpp
+++ npc/src/csrc/emulator/emulator.cpp
@@ -371,9 +371,9 @@ void Emulator::WBU_catch(uint32_t next_pc, \
     this->perf->inst_cont();
 
     switch(Inst_quene.front().second){
-        case 0x00000000: this->Emulator_trap(1);   break; // ecall
-        case 0xffffffff: this->Emulator_trap(1);   break; // bad trap
-        case 0x00100073: this->Emulator_trap(cpu.gpr[10]);   break; // ebreak
+        case 0x00000000: Log("00000000"); this->Emulator_trap(1);   break; // ecall
+        case 0xffffffff: Log("ffffffff"); this->Emulator_trap(1);   break; // bad trap
+        case 0x00100073: Log("00100073"); this->Emulator_trap(cpu.gpr[10]);   break; // ebreak
         default: break;
     }
 
